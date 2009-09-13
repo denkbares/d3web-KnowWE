df8bb3be5032a32ea94258aa7bca84caec079ef2
Alex Legler <alex.legler@denkbares.com>
2009-09-13 09:28:08 +0000

    d3web-Distributed MIGRATION
    	* pom.xml cleanup
    	* Adding license header
    	* Organizing imports

--- d3web-KnowWE/d3web-Distributed/src/main/java/de/d3web/kernel/psMethods/delegate/PSMethodDelegate.java
+++ d3web-KnowWE/d3web-Distributed/src/main/java/de/d3web/kernel/psMethods/delegate/PSMethodDelegate.java
@@ -1,12 +1,33 @@
+/*
+ * Copyright (C) 2009 Chair of Artificial Intelligence and Applied Informatics
+ *                    Computer Science VI, University of Wuerzburg
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
 package de.d3web.kernel.psMethods.delegate;
 
+import java.util.Collection;
 import java.util.List;
 
 import de.d3web.kernel.XPSCase;
 import de.d3web.kernel.domainModel.KnowledgeSlice;
-import de.d3web.kernel.domainModel.NamedObject;
 import de.d3web.kernel.domainModel.RuleComplex;
 import de.d3web.kernel.psMethods.PSMethodAdapter;
+import de.d3web.kernel.psMethods.PropagationEntry;
 
 public class PSMethodDelegate extends PSMethodAdapter {
 	private static PSMethodDelegate instance = new PSMethodDelegate();
@@ -16,11 +37,19 @@ public class PSMethodDelegate extends PSMethodAdapter {
 	}
 
 
-	public void propagate(XPSCase theCase, NamedObject nob, Object[] newValue) {
-		List<? extends KnowledgeSlice> slices = nob.getKnowledge(this.getClass());
-		if(slices == null) return;
-		for (KnowledgeSlice slice : slices) {
-			((RuleComplex) slice).check(theCase);
+	@Override
+	public void propagate(XPSCase theCase, Collection<PropagationEntry> changes) {
+		
+		for (PropagationEntry propagationEntry : changes) {
+			
+			List<? extends KnowledgeSlice> slices = propagationEntry.getObject().getKnowledge(this.getClass());
+			
+			if(slices == null) 
+				return;
+			
+			for (KnowledgeSlice slice : slices) {
+				((RuleComplex) slice).check(theCase);
+			}
 		}
 	}
 }
--- d3web-KnowWE/d3web-Distributed/src/main/java/de/d3web/we/core/knowledgeService/D3webKnowledgeServiceSession.java
+++ d3web-KnowWE/d3web-Distributed/src/main/java/de/d3web/we/core/knowledgeService/D3webKnowledgeServiceSession.java
@@ -1,8 +1,27 @@
+/*
+ * Copyright (C) 2009 Chair of Artificial Intelligence and Applied Informatics
+ *                    Computer Science VI, University of Wuerzburg
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
 package de.d3web.we.core.knowledgeService;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -32,7 +51,6 @@ import de.d3web.kernel.domainModel.qasets.Question;
 import de.d3web.kernel.domainModel.qasets.QuestionChoice;
 import de.d3web.kernel.domainModel.qasets.QuestionNum;
 import de.d3web.kernel.domainModel.qasets.QuestionOC;
-import de.d3web.kernel.psMethods.MethodKind;
 import de.d3web.kernel.psMethods.PSMethodInit;
 import de.d3web.kernel.psMethods.compareCase.CompareCaseException;
 import de.d3web.kernel.psMethods.compareCase.comparators.CompareMode;
@@ -49,7 +67,6 @@ import de.d3web.we.basic.Information;
 import de.d3web.we.basic.InformationType;
 import de.d3web.we.basic.SolutionState;
 import de.d3web.we.basic.TerminologyType;
-import de.d3web.we.core.blackboard.InferenceConverterUtils;
 import de.d3web.we.core.broker.Broker;
 import de.d3web.we.core.utils.ConverterUtils;
 
