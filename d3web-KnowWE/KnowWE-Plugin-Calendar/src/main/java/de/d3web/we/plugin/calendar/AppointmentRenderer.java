b29726e4dfcecfee60085c07c810b06fe27e1b7f
Alex Legler <alex.legler@denkbares.com>
2009-10-02 08:04:42 +0000

    Adding the Calendar plugin

---
+++ d3web-KnowWE/KnowWE-Plugin-Calendar/src/main/java/de/d3web/we/plugin/calendar/AppointmentRenderer.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2009 Chair of Artificial Intelligence and Applied Informatics
+ *                    Computer Science VI, University of Wuerzburg
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package de.d3web.we.plugin.calendar;
+
+import java.util.Map;
+
+import de.d3web.we.core.KnowWEEnvironment;
+import de.d3web.we.kdom.Section;
+import de.d3web.we.kdom.rendering.KnowWEDomRenderer;
+import de.d3web.we.wikiConnector.KnowWEUserContext;
+
+public class AppointmentRenderer extends KnowWEDomRenderer{
+
+	@Override
+	public void render(Section sec, KnowWEUserContext user, StringBuilder string) {
+		
+		Map<String, String> persons = CalendarModule.getPersons();
+		
+		Section dateSec = sec.findChildOfType(AppointmentStartSymbol.class).findChildOfType(AppointmentDate.class);
+		Section timeSec = sec.findChildOfType(AppointmentStartSymbol.class).findChildOfType(AppointmentTime.class);
+		Section textSec = sec.findChildOfType(AppointmentText.class);
+		Section nameSec = sec.findChildOfType(AppointmentAuthor.class);
+		
+		String name = "";
+		String text = textSec.getOriginalText();
+		
+		//TODO Error messages for dateSec, timeSec  = null ??
+		
+		if(nameSec == null) {
+			
+			name = "{person not found}";
+			
+		} else {
+			
+			name = persons.get(nameSec.getOriginalText());
+		}
+		
+		StringBuilder b = new StringBuilder();
+		b.append("<table class=wikitable width=95% border=0>\n");
+		b.append("<tr><th align=left width=80>" + dateSec.getOriginalText() + "</th><th align=left>" + timeSec.getOriginalText() + "</th><th align=right width=250><i>" + name + "</i></th></tr>\n");
+		string.append("<tr><td colspan=3>" + text + "</td></tr>\n</table>\n");
+		
+		string.append(KnowWEEnvironment.maskHTML(b.toString()));
+	}
+}
---
+++ d3web-KnowWE/KnowWE-Plugin-Calendar/src/main/webapp/scripts/CalendarPlugin.js
@@ -0,0 +1,100 @@
+
+/**
+ *  goto date.
+ */
+function gotoDate() {
+	
+	var date = document.date.input.value;
+    
+	var params = {
+    		action : 'CalendarAction',
+    		CalendarGoTo : date
+    	}
+	
+	var options = {
+    		url : getURL( params ),
+    		response : {
+    			action : 'none',
+    			ids : []
+    		}
+	}
+	
+	new KnowWEAjax( options ).send();
+	
+}
+
+/**
+*  go back to today.
+*/
+function gotoToday() {
+	
+	var date = document.date.today.value;
+   
+	var params = {
+   		action : 'CalendarAction',
+   		CalendarGoTo : date
+   	}
+	
+	var options = {
+   		url : getURL( params ),
+   		response : {
+   			action : 'none',
+   			ids : []
+   		}
+	}
+	
+	new KnowWEAjax( options ).send();
+	
+}
+
+function loadAction( today ) {
+
+	var params = {
+   		action : 'CalendarAction',
+   		CalendarGoTo : today,
+   	}
+	
+	var options = {
+   		url : getURL( params ),
+   		response : {
+   			action : 'none',
+   			ids : []
+   		}
+	}
+	
+	new KnowWEAjax( options ).send();
+
+}
+
+/**
+ * link
+ */
+function link(topic, week) {
+	
+	var url = document.URL;
+	
+ 	if(url.charAt(url.length - 1) == '/') {
+ 	    
+ 	    url = url + "Wiki.jsp?page=" + topic;
+ 	    
+ 	} else {
+	
+    	var urlSplit = url.split('&');
+    	
+    	var path = urlSplit[0].split("#");
+    	
+    	url = path[0];
+    
+    	for(var i = 1; i < urlSplit.length; i++) {
+    	    parameterSplit = urlSplit[i].split('=');
+    	    if(parameterSplit[0] == 'week') break;
+    	    if(parameterSplit[0].contains('manually')) break;
+    	    url = url + "&" + urlSplit[i];
+    	}
+ 	}
+	
+	url = url + '&week=' + week + "#Calendar";
+	
+	location.replace(url);
+	
+}
