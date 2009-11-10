710916b60a80fda6f0a31f888f4ea12ef936001e
Florian Lemmerich <lemmerich@d3web.svn.invalid>
2009-11-10 11:25:35 +0000

    * added Hermes.js to KnowWEScriptLoader

--- d3web-KnowWE/KnowWE-Plugin-HermesModule/src/main/java/de/d3web/we/hermes/HermesPlugin.java
+++ d3web-KnowWE/KnowWE-Plugin-HermesModule/src/main/java/de/d3web/we/hermes/HermesPlugin.java
@@ -28,6 +28,7 @@ import javax.servlet.ServletContext;
 
 import de.d3web.we.action.KnowWEAction;
 import de.d3web.we.core.KnowWEEnvironment;
+import de.d3web.we.core.KnowWEScriptLoader;
 import de.d3web.we.hermes.action.InsertRelationAction;
 import de.d3web.we.hermes.action.SearchTimeEventsAction;
 import de.d3web.we.hermes.action.SetFilterLevelAction;
@@ -45,74 +46,76 @@ import de.d3web.we.taghandler.TagHandler;
 
 public class HermesPlugin extends AbstractDefaultKnowWEModule {
 
-	private static HermesPlugin instance;
+    private static HermesPlugin instance;
 
-	public static HermesPlugin getInstance() {
-		if (instance == null) {
-			instance = new HermesPlugin();
-		}
-		return instance;
+    public static HermesPlugin getInstance() {
+	if (instance == null) {
+	    instance = new HermesPlugin();
+	}
+	return instance;
+    }
+
+    @Override
+    public void initModule(ServletContext context) {
+
+	// KDOM-types hack to having TimeEventType have higher priority than
+	// standard SemanticAnnotation
+	// to allow for SemanticAnnotation inside TimeEvents (without destroying
+	// those)
+	List<KnowWEObjectType> rootTypes = KnowWEEnvironment.getInstance()
+		.getRootTypes();
+	int index = -1;
+	for (KnowWEObjectType knowWEObjectType : rootTypes) {
+	    if (knowWEObjectType instanceof SemanticAnnotation) {
+		index = rootTypes.indexOf(knowWEObjectType);
+	    }
+	}
+	if (index != -1) {
+	    rootTypes.add(index - 1, new TimeEventType());
 	}
 
-	@Override
-	public void initModule(ServletContext context) {
+	KnowWEScriptLoader.getInstance().add("Hermes.js", false);
 
-		// KDOM-types hack to having TimeEventType have higher priority than
-		// standard SemanticAnnotation
-		// to allow for SemanticAnnotation inside TimeEvents (without destroying
-		// those)
-		List<KnowWEObjectType> rootTypes = KnowWEEnvironment.getInstance()
-				.getRootTypes();
-		int index = -1;
-		for (KnowWEObjectType knowWEObjectType : rootTypes) {
-			if (knowWEObjectType instanceof SemanticAnnotation) {
-				index = rootTypes.indexOf(knowWEObjectType);
-			}
-		}
-		if (index != -1) {
-			rootTypes.add(index - 1, new TimeEventType());
-		}
+    }
 
-	}
+    /**
+     * @see de.d3web.we.module.AbstractDefaultKnowWEModule#getRootTypes() The
+     *      Type 'DemoSectionType' is registered to the KnowWE-type system. This
+     *      method is called once at initialization of KnowWE(-Modules)
+     */
+    @Override
+    public List<KnowWEObjectType> getRootTypes() {
+	List<KnowWEObjectType> rootTypes = new ArrayList<KnowWEObjectType>();
+	// not really necessary anymore because type TimeEventType is already
+	// registered
+	// in initModule() by a hack to gather higher priority
+	rootTypes.add(new TimeEventType());
+	rootTypes.add(new MapType());
+	return rootTypes;
+    }
 
-	/**
-	 * @see de.d3web.we.module.AbstractDefaultKnowWEModule#getRootTypes() The
-	 *      Type 'DemoSectionType' is registered to the KnowWE-type system. This
-	 *      method is called once at initialization of KnowWE(-Modules)
-	 */
-	@Override
-	public List<KnowWEObjectType> getRootTypes() {
-		List<KnowWEObjectType> rootTypes = new ArrayList<KnowWEObjectType>();
-		// not really necessary anymore because type TimeEventType is already
-		// registered
-		// in initModule() by a hack to gather higher priority
-		rootTypes.add(new TimeEventType());
-		rootTypes.add(new MapType());
-		return rootTypes;
-	}
+    @Override
+    public List<TagHandler> getTagHandlers() {
+	List<TagHandler> list = new ArrayList<TagHandler>();
+	list.add(new TimeLineHandler());
+	list.add(new LocalTimeEventsHandler());
+	list.add(new TimeEventSearchHandler());
+	list.add(new ShowMapHandler());
+	list.add(new SetTimeEventFilterLevelHandler());
+	return list;
+    }
 
-	@Override
-	public List<TagHandler> getTagHandlers() {
-		List<TagHandler> list = new ArrayList<TagHandler>();
-		list.add(new TimeLineHandler());
-		list.add(new LocalTimeEventsHandler());
-		list.add(new TimeEventSearchHandler());
-		list.add(new ShowMapHandler());
-		list.add(new SetTimeEventFilterLevelHandler());
-		return list;
-	}
+    @Override
+    public List<PageAppendHandler> getPageAppendHandlers() {
+	List<PageAppendHandler> handlers = new ArrayList<PageAppendHandler>();
+	handlers.add(new AppendTagEditHandler());
+	return handlers;
+    }
 
-	@Override
-	public List<PageAppendHandler> getPageAppendHandlers() {
-		List<PageAppendHandler> handlers = new ArrayList<PageAppendHandler>();
-		handlers.add(new AppendTagEditHandler());
-		return handlers;
-	}
-
-	@Override
-	public void addAction(Map<Class<? extends KnowWEAction>, KnowWEAction> map) {
-		map.put(SearchTimeEventsAction.class, new SearchTimeEventsAction());
-		map.put(SetFilterLevelAction.class, new SetFilterLevelAction());
-		map.put(InsertRelationAction.class, new InsertRelationAction());
-	}
+    @Override
+    public void addAction(Map<Class<? extends KnowWEAction>, KnowWEAction> map) {
+	map.put(SearchTimeEventsAction.class, new SearchTimeEventsAction());
+	map.put(SetFilterLevelAction.class, new SetFilterLevelAction());
+	map.put(InsertRelationAction.class, new InsertRelationAction());
+    }
 }
