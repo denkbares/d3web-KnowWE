0e1b8303b36aa07c6012b9c94cd15f488b9c2985
Jochen Reutelshöfer <jochen.reutelshoefer@denkbares.com>
2009-11-25 16:03:54 +0000

    fixed conservation of rule error messages

--- d3web-KnowWE/KnowWE-Plugin-d3web/src/main/java/de/d3web/we/kdom/rules/Rule.java
+++ d3web-KnowWE/KnowWE-Plugin-d3web/src/main/java/de/d3web/we/kdom/rules/Rule.java
@@ -33,6 +33,7 @@ import de.d3web.KnOfficeParser.rule.D3ruleBuilder;
 import de.d3web.kernel.domainModel.KnowledgeBaseManagement;
 import de.d3web.report.Message;
 import de.d3web.report.Report;
+import de.d3web.we.core.KnowWEEnvironment;
 import de.d3web.we.core.KnowWEParseResult;
 import de.d3web.we.kdom.DefaultAbstractKnowWEObjectType;
 import de.d3web.we.kdom.KnowWEArticle;
@@ -50,11 +51,12 @@ import de.d3web.we.utils.KnowWEObjectTypeUtils;
 import de.d3web.we.utils.KnowWEUtils;
 import de.d3web.we.wikiConnector.KnowWEUserContext;
 
-public class Rule extends DefaultAbstractKnowWEObjectType implements KnowledgeRecyclingObjectType {
-	
+public class Rule extends DefaultAbstractKnowWEObjectType implements
+		KnowledgeRecyclingObjectType {
+
 	public static final String KBID_KEY = "kbid";
 	public static final String KDOMID_KEY = "kdomid";
-	
+
 	@Override
 	protected void init() {
 		subtreeHandler.add(new RuleSubTreeHandler());
@@ -62,94 +64,123 @@ public class Rule extends DefaultAbstractKnowWEObjectType implements KnowledgeRe
 		setCustomRenderer(new RuleRenderer());
 		this.childrenTypes.add(new RuleActionLine());
 		this.childrenTypes.add(new RuleCondLine());
-		
+
 	}
-	
 
 	@Override
-	public void cleanKnowledge(KnowWEArticle article, Section s, KnowledgeBaseManagement kbm) {
+	public void cleanKnowledge(KnowWEArticle article, Section s,
+			KnowledgeBaseManagement kbm) {
 		OldRulesEraser.deleteRules(article, s, kbm);
 	}
-	
-	
-	private class RuleRenderer extends KnowWEDomRenderer{
-		
+
+	private class RuleRenderer extends KnowWEDomRenderer {
+
 		@Override
-		public void render(Section sec, KnowWEUserContext user, StringBuilder string) {
-			
-			List<Message> messages = ((Rule) sec.getObjectType()).getMessages(sec);
-			
-			string.append(KnowWEUtils.maskHTML("<pre><span id='"+sec.getId()
-					+"' class = 'XCLRelationInList'><span id=\"\">"));
-			
+		public void render(Section sec, KnowWEUserContext user,
+				StringBuilder string) {
+
+			// List<Message> messages = ((Rule)
+			// sec.getObjectType()).getMessages(sec);
+			// quick fix by Jochen : only show first/single error
+			Message error = getErrorMessage(sec);
+
+			string.append(KnowWEUtils.maskHTML("<pre><span id='" + sec.getId()
+					+ "' class = 'XCLRelationInList'><span id=\"\">"));
+
 			boolean empty = true;
-			if (messages != null) {
-				for (Message m : messages) {
-					if (m.getMessageType().equals(Message.ERROR)
-							|| m.getMessageType().equals(Message.WARNING)) {  // hack showing only errors, this rendering needs a complete redesign
-						empty = false;
-						string.append(KnowWEUtils.maskHTML("<span style='color:red'>"));
-						string.append(m.getMessageType() + ": " + m.getMessageText() 
-								+ " Line: " + m.getLineNo());
-						string.append(KnowWEUtils.maskHTML("</span>"));
-						string.append(KnowWEUtils.maskHTML("\n"));
-					}
+			if (error != null) {
+				if (error.getMessageType().equals(Message.ERROR)
+						|| error.getMessageType().equals(Message.WARNING)) { // hack
+																				// showing
+																				// only
+																				// errors,
+																				// this
+																				// rendering
+																				// needs
+																				// a
+																				// complete
+																				// redesign
+					empty = false;
+					string.append(KnowWEUtils
+							.maskHTML("<span style='color:red'>"));
+					string
+							.append(error.getMessageType()
+									+ ": "
+									+ error.getMessageText()
+									+ (error.getMessageType().equals(
+											Message.NOTE) ? " "
+											+ error.getCount() : " Line: "
+											+ error.getLineNo()));
+					
+					string.append(KnowWEUtils.maskHTML("</span>"));
+					string.append(KnowWEUtils.maskHTML("\n"));
+
 				}
 				if (!empty)
 					string.append(KnowWEUtils.maskHTML("\n"));
 			}
-			
+
 			StringBuilder b = new StringBuilder();
 			if (!empty) {
 				DefaultTextRenderer.getInstance().render(sec, user, b);
 			} else {
 				DelegateRenderer.getInstance().render(sec, user, b);
 			}
-			string.append(b.toString() + KnowWEUtils.maskHTML("</pre></span></span>\n"));
+			string.append(b.toString()
+					+ KnowWEUtils.maskHTML("</pre></span></span>\n"));
 		}
+
 	}
-	
-	public class RuleSubTreeHandler extends D3webReviseSubTreeHandler {
+
+	class RuleSubTreeHandler extends D3webReviseSubTreeHandler {
 
 		@Override
 		public void reviseSubtree(KnowWEArticle article, Section s) {
-			
-			boolean lazy = false ;
-			Section xml = KnowWEObjectTypeUtils.getAncestorOfType(s, AbstractXMLObjectType.class);
-			Map<String,String> attributes = AbstractXMLObjectType.getAttributeMapFor(xml);
-			if(attributes != null && attributes.containsKey("lazy")) {
+
+			boolean lazy = false;
+			Section xml = KnowWEObjectTypeUtils.getAncestorOfType(s,
+					AbstractXMLObjectType.class);
+			Map<String, String> attributes = AbstractXMLObjectType
+					.getAttributeMapFor(xml);
+			if (attributes != null && attributes.containsKey("lazy")) {
 				String l = attributes.get("lazy");
-				if(l != null) {
-					if(l.equals("1") | l.equals("on") | l.equals("true") | l.equals("an") ) {
+				if (l != null) {
+					if (l.equals("1") | l.equals("on") | l.equals("true")
+							| l.equals("an")) {
 						lazy = true;
 					}
 				}
 			}
-			
+
 			KnowledgeBaseManagement kbm = getKBM(article, s);
-			
+
 			if (kbm != null) {
-				
-				D3ruleBuilder builder = 
-					new D3ruleBuilder(s.getId(), lazy,
-							new SingleKBMIDObjectManager(kbm));
-				
+
+				D3ruleBuilder builder = new D3ruleBuilder(s.getId(), lazy,
+						new SingleKBMIDObjectManager(kbm));
+
 				if (s != null) {
-					String text = 
-						TextInclude.removeTextIncludeTags(s.getOriginalText());
+					String text = TextInclude.removeTextIncludeTags(s
+							.getOriginalText());
 					Reader r = new StringReader(text);
-					
-					List<Message> bm = 
-						builder.addKnowledge(r, new SingleKBMIDObjectManager(kbm), null);
-					
+
+					List<Message> bm = builder.addKnowledge(r,
+							new SingleKBMIDObjectManager(kbm), null);
+
 					if (builder.getRuleIDs().size() == 1) {
-						KnowWEUtils.storeSectionInfo(
-								article.getWeb(), article.getTitle(), s.getId(),
-								KBID_KEY, builder.getRuleIDs().get(0));
+						KnowWEUtils.storeSectionInfo(article.getWeb(), article
+								.getTitle(), s.getId(), KBID_KEY, builder
+								.getRuleIDs().get(0));
 					}
-					
+
 					((Rule) s.getObjectType()).storeMessages(s, bm);
-					
+					for (Message message : bm) {
+						if (message.getMessageType().equals(Message.ERROR)) {
+							storeErrorMessage(s, message);
+							break;
+						}
+					}
+
 					Report ruleRep = new Report();
 					for (Message messageKnOffice : bm) {
 						ruleRep.add(messageKnOffice);
@@ -158,29 +189,59 @@ public class Rule extends DefaultAbstractKnowWEObjectType implements KnowledgeRe
 							.getTitle(), s.getOriginalText());
 					s.getArticle().getReport().addReport(result);
 				}
-			}			
+			} else {
+				// store empty message to prevent surviving of old errors due to
+				// update-inconstistencies
+				storeErrorMessage(s, null);
+			}
 		}
 	}
-	
+
+	public static final String RULE_ERROR_MESSAGE_STORE_KEY = "Rule-error-message";
+
+	/**
+	 * Stores a message under to rule-error-store-key
+	 * 
+	 * @param s
+	 * @param message
+	 */
+	public static void storeErrorMessage(Section s, Message message) {
+		KnowWEUtils.storeSectionInfo(KnowWEEnvironment.DEFAULT_WEB, s
+				.getTitle(), s.getId(), RULE_ERROR_MESSAGE_STORE_KEY, message);
+	}
+
+	/**
+	 * Stores a message under to rule-error-store-key
+	 * 
+	 * @param s
+	 * @param message
+	 */
+	public static Message getErrorMessage(Section s) {
+		return (Message) KnowWEUtils.getStoredObject(
+				KnowWEEnvironment.DEFAULT_WEB, s.getTitle(), s.getId(),
+				RULE_ERROR_MESSAGE_STORE_KEY);
+	}
+
 	public class RuleSectionFinder extends SectionFinder {
 
 		@Override
-		public List<SectionFinderResult> lookForSections(String text, Section father) {
+		public List<SectionFinderResult> lookForSections(String text,
+				Section father) {
 
-			ArrayList<SectionFinderResult> result =
-						new ArrayList<SectionFinderResult>();
+			ArrayList<SectionFinderResult> result = new ArrayList<SectionFinderResult>();
 
-			Pattern p = Pattern.compile 
-				("(IF|WENN).*?(?=(\\s*?(?m)^\\s*?$\\s*|\\s*IF|\\s*WENN|\\s*" + TextInclude.PATTERN_BOTH + "|\\s*\\z))", Pattern.DOTALL);
+			Pattern p = Pattern.compile(
+					"(IF|WENN).*?(?=(\\s*?(?m)^\\s*?$\\s*|\\s*IF|\\s*WENN|\\s*"
+							+ TextInclude.PATTERN_BOTH + "|\\s*\\z))",
+					Pattern.DOTALL);
 			Matcher m = p.matcher(text);
-			
-			while(m.find()) {
+
+			while (m.find()) {
 				result.add(new SectionFinderResult(m.start(), m.end()));
 			}
-		
+
 			return result;
 		}
 	}
-
 
 }
