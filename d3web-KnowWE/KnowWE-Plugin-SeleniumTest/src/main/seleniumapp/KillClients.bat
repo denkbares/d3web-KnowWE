caf5a79cd09be027d8ebc97133c0484ae9282a2f
Alex Legler <alex.legler@denkbares.com>
2009-12-23 20:41:14 +0000

    Remove old copy of SeleniumTest

--- d3web-KnowWE/KnowWE-Plugin-SeleniumTest/src/main/seleniumapp/KillClients.bat
+++
@@ -1,3 +0,0 @@
-cd /D I:\Programme\Eclipse\D3WebKnowWE\d3web-KnowWE\KnowWE-Plugin-SeleniumTest\src\main\seleniumapp
-start pskill.exe java
-start pskill.exe cmd
--- d3web-KnowWE/KnowWE-Plugin-SeleniumTest/src/main/seleniumapp/StartClients.bat
+++
@@ -1,6 +0,0 @@
-cd /D I:\Programme\Selenium-Grid
-start ant launch-hub
-start ant -Dport=5554 launch-remote-control
-start ant -Dport=5555 launch-remote-control
-start ant -Dport=5556 launch-remote-control
-start ant -Dport=5557 launch-remote-control
