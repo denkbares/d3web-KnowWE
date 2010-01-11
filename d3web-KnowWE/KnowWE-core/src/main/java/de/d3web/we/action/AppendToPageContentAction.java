20674ff9024ef0bf7679533c9208d1e5b3ca65b0
Jochen Reutelshöfer <jochen.reutelshoefer@denkbares.com>
2010-01-11 08:37:50 +0000

    added AppendToPageContentAction allowing for appending some text to a page

---
+++ d3web-KnowWE/KnowWE-core/src/main/java/de/d3web/we/action/AppendToPageContentAction.java
@@ -0,0 +1,34 @@
+package de.d3web.we.action;
+
+import de.d3web.we.core.KnowWEArticleManager;
+import de.d3web.we.core.KnowWEAttributes;
+import de.d3web.we.core.KnowWEEnvironment;
+import de.d3web.we.core.KnowWEParameterMap;
+import de.d3web.we.kdom.KnowWEArticle;
+
+/**
+ * @author Jochen
+ * 
+ * This action allows to append any String to the article source of an article
+ *
+ */
+public class AppendToPageContentAction implements KnowWEAction {
+
+	@Override
+	public boolean isAdminAction() {
+		return false;
+	}
+
+	@Override
+	public String perform(KnowWEParameterMap parameterMap) {
+		String web = parameterMap.getWeb();
+		String name = parameterMap.getTopic();
+		String appendText = parameterMap.get(KnowWEAttributes.TEXT);
+		KnowWEArticleManager mgr = KnowWEEnvironment.getInstance().getArticleManager(web);
+		KnowWEArticle art = mgr.getArticle("name");
+		
+		// replaces old article content with old article content + text to append 
+		return mgr.replaceKDOMNode(parameterMap, name, art.getSection().getId(), art.getSection().getOriginalText()+appendText);
+	}
+
+}
