da43f0d9747faaf825efad235e13456e7fea59b7
Markus Friedrich <markus.friedrich@denkbares.com>
2010-02-06 17:50:24 +0000

    Commit file svn has ignored...

---
+++ d3web-KnowWE/KnowWE-core/src/main/java/de/d3web/we/kdom/tagging/TagsContent.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2009 Chair of Artificial Intelligence and Applied Informatics
+ *                    Computer Science VI, University of Wuerzburg
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package de.d3web.we.kdom.tagging;
+
+import org.openrdf.model.URI;
+
+import de.d3web.we.kdom.Section;
+import de.d3web.we.kdom.renderer.NothingRenderer;
+import de.d3web.we.kdom.xml.XMLContent;
+import de.d3web.we.module.semantic.owl.IntermediateOwlObject;
+import de.d3web.we.module.semantic.owl.UpperOntology;
+import de.d3web.we.module.semantic.owl.helpers.OwlHelper;
+
+public class TagsContent extends XMLContent {
+
+	@Override
+	protected void init() {
+		this.setCustomRenderer(NothingRenderer.getInstance());
+	}
+
+	@Override
+	public IntermediateOwlObject getOwl(Section s) {
+		String text = s.getOriginalText();
+		IntermediateOwlObject io = new IntermediateOwlObject();
+		for (String cur : text.split(" |,")) {
+			if (cur.trim().length() > 0) {
+				UpperOntology uo = UpperOntology.getInstance();
+				URI suri = uo.getHelper().createlocalURI(s.getTitle());
+				URI puri = uo.getHelper().createURI("hasTag");
+				URI ouri = uo.getHelper().createlocalURI(cur.trim());				
+				io.merge(uo.getHelper().createProperty(suri, puri, ouri, s));
+			}
+		}
+		return io;
+	}
+
+}
