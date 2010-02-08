b7f0cbe967fb656b3007cd1ff15a65a435b24336
Jochen Reutelshöfer <jochen.reutelshoefer@denkbares.com>
2010-02-08 13:20:36 +0000

    Added ObjectCreationError

---
+++ d3web-KnowWE/KnowWE-core/src/main/java/de/d3web/we/kdom/report/ObjectCreationError.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2009 Chair of Artificial Intelligence and Applied Informatics
+ *                    Computer Science VI, University of Wuerzburg
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package de.d3web.we.kdom.report;
+
+import de.d3web.we.kdom.ReviseSubTreeHandler;
+import de.d3web.we.kdom.report.KDOMError;
+import de.d3web.we.wikiConnector.KnowWEUserContext;
+
+public class ObjectCreationError extends KDOMError{
+
+	private String s;
+	private Class<? extends ReviseSubTreeHandler> c;
+	
+	public ObjectCreationError(String s, Class<? extends ReviseSubTreeHandler> c) {
+		this.s = s;
+		this.c = c;
+	}
+	
+	@Override
+	public String getVerbalization(KnowWEUserContext usercontext) {
+		return "Could not create Object: "+s+" - Unknown Error in: "+c.getSimpleName();
+	}
+
+}
