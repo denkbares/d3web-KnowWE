14207d04f7c708d448fc84cc65191afd7d54b058
Franz Schwab <schwab@d3web.svn.invalid>
2010-02-08 15:03:43 +0000

    * restoring old folder...

--- d3web-KnowWE/KnowWE-Plugin-Refactoring/src/main/resources/webapp/KnowWEExtension/scripts/CodeMirror.js
+++
@@ -1,9 +0,0 @@
-var editor = CodeMirror.fromTextArea("default-edit-area",{
-	parserfile: ["tokenizegroovy.js", "parsegroovy.js"],
-	path: "KnowWEExtension/scripts/CodeMirror-0.65/js/",
-	stylesheet: "KnowWEExtension/scripts/CodeMirror-0.65/css/groovycolors.css",
-	continuousScanning: 500,
-	lineNumbers: true,
-	textWrapping: false,
-	tabMode: "spaces"
-});
--- d3web-KnowWE/KnowWE-Plugin-Refactoring/src/main/resources/webapp/KnowWEExtension/scripts/SyntaxHighlighter.js
+++
@@ -1,3 +0,0 @@
-SyntaxHighlighter.all();
-SyntaxHighlighter.defaults['toolbar'] = false;
-KNOWWE.helper.observer.subscribe('quick-edit', SyntaxHighlighter.highlight);
