69abdfff3cdbb1f8d94607ca07533ccde6fb8db1
Jochen Reutelshöfer <jochen.reutelshoefer@denkbares.com>
2010-02-08 09:45:53 +0000

    commit test...

--- d3web-KnowWE/KnowWE-core/src/main/java/de/d3web/we/kdom/sectionFinder/StringEnumChecker.java
+++ d3web-KnowWE/KnowWE-core/src/main/java/de/d3web/we/kdom/sectionFinder/StringEnumChecker.java
@@ -1,9 +1,30 @@
+/*
+ * Copyright (C) 2009 Chair of Artificial Intelligence and Applied Informatics
+ *                    Computer Science VI, University of Wuerzburg
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
 package de.d3web.we.kdom.sectionFinder;
 
 import de.d3web.we.kdom.KnowWEArticle;
 import de.d3web.we.kdom.ReviseSubTreeHandler;
 import de.d3web.we.kdom.Section;
-import de.d3web.we.kdom.error.KDOMError;
+import de.d3web.we.kdom.report.KDOMError;
+import de.d3web.we.kdom.report.KDOMReportMessage;
 
 public class StringEnumChecker implements ReviseSubTreeHandler{
 
@@ -16,7 +37,7 @@ public class StringEnumChecker implements ReviseSubTreeHandler{
 	}
 	
 	@Override
-	public void reviseSubtree(KnowWEArticle article, Section s) {
+	public KDOMReportMessage reviseSubtree(KnowWEArticle article, Section s) {
 		boolean found = false;
 		for (String string : values) {
 			if(s.getOriginalText().contains(string)) {
@@ -25,8 +46,10 @@ public class StringEnumChecker implements ReviseSubTreeHandler{
 		}
 		
 		if(!found) {
-			KDOMError.storeError(s, error);
+			return error;
 		}
+		return null;
+		
 		
 	}
 
