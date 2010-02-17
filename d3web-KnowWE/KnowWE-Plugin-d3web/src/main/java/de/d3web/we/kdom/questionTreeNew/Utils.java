f39fd30a684c507b5ee75197eaf7445e173b463b
Jochen Reutelshöfer <jochen.reutelshoefer@denkbares.com>
2010-02-17 16:04:15 +0000

    small fix: checking if answer != null before creating a new CondEqual

--- d3web-KnowWE/KnowWE-Plugin-d3web/src/main/java/de/d3web/we/kdom/questionTreeNew/Utils.java
+++ d3web-KnowWE/KnowWE-Plugin-d3web/src/main/java/de/d3web/we/kdom/questionTreeNew/Utils.java
@@ -20,25 +20,26 @@ public class Utils {
 
 	/**
 	 * Creates a condition from a List of DashTreeElements. The List is supposed
-	 * to start with a Section containing a QuestionTreeAnswerID, followed by 
-	 * a Section containing a QuestionID. After that it starts again with a
-	 * QuestionTreeAnswerID and so forth.  If thats not the case, <tt>null</tt> 
-	 * will be returned.
-	 * Such a List of DashTreeElements can be created with 
-	 * <tt>DashTreeElement.getDashTreeAncestors(Section s)</tt>, if <tt>s</tt> is
-	 * the child Section of an answer in a valid DashTree.
+	 * to start with a Section containing a QuestionTreeAnswerID, followed by a
+	 * Section containing a QuestionID. After that it starts again with a
+	 * QuestionTreeAnswerID and so forth. If thats not the case, <tt>null</tt>
+	 * will be returned. Such a List of DashTreeElements can be created with
+	 * <tt>DashTreeElement.getDashTreeAncestors(Section s)</tt>, if <tt>s</tt>
+	 * is the child Section of an answer in a valid DashTree.
 	 */
-	public static AbstractCondition createCondition(List<Section<? extends DashTreeElement>> ancestors) {
-		
+	public static AbstractCondition createCondition(
+			List<Section<? extends DashTreeElement>> ancestors) {
+
 		List<AbstractCondition> simpleConds = new ArrayList<AbstractCondition>();
-		
+
 		for (int i = 0; i + 2 < ancestors.size(); i += 2) {
-			AbstractCondition simpleCond = createSimpleCondition(ancestors.get(i), ancestors.get(i + 1));
+			AbstractCondition simpleCond = createSimpleCondition(ancestors
+					.get(i), ancestors.get(i + 1));
 			if (simpleCond != null) {
 				simpleConds.add(simpleCond);
 			}
 		}
-		
+
 		if (simpleConds.isEmpty()) {
 			return null;
 		} else if (simpleConds.size() == 1) {
@@ -46,40 +47,46 @@ public class Utils {
 		} else {
 			return new CondAnd(simpleConds);
 		}
-		
+
 	}
-	
+
 	/**
 	 * Creates a condition from the two DashTreeElements father an grandfather.
 	 * Father is supposed to contain a QuestionTreeAnswerID, the grandfather a
 	 * QuestionID. If thats not the case, <tt>null</tt> will be returned.
 	 */
-	public static AbstractCondition createSimpleCondition(Section<? extends DashTreeElement> father, 
+	public static AbstractCondition createSimpleCondition(
+			Section<? extends DashTreeElement> father,
 			Section<? extends DashTreeElement> grandFather) {
-		
-		Section<QuestionTreeAnswerID> answerSec = father.findSuccessor(new QuestionTreeAnswerID());		
+
+		Section<QuestionTreeAnswerID> answerSec = father
+				.findSuccessor(new QuestionTreeAnswerID());
 		Section<QuestionID> qSec = grandFather.findSuccessor(new QuestionID());
 
 		Question q = qSec.get().getObject(qSec);
 
 		if (answerSec != null && q instanceof QuestionChoice) {
 			Answer a = answerSec.get().getObject(answerSec);
-			CondEqual c = new CondEqual((QuestionChoice) q, a);
-			return c;
+			if (a != null) {
+				CondEqual c = new CondEqual((QuestionChoice) q, a);
+				return c;
+			}
 		}
-		
-		Section<NumericCondLine> numCondSec = father
-			.findSuccessor(new NumericCondLine());
 
-		if(numCondSec != null && q instanceof QuestionNum) {
+		Section<NumericCondLine> numCondSec = father
+				.findSuccessor(new NumericCondLine());
+
+		if (numCondSec != null && q instanceof QuestionNum) {
 			Double d = NumericCondLine.getValue(numCondSec);
 			String comp = NumericCondLine.getComparator(numCondSec);
-			AbstractCondition condNum = FindingToConditionBuilder.createCondNum(father.getArticle(), numCondSec, comp, d, (QuestionNum)q);
+			AbstractCondition condNum = FindingToConditionBuilder
+					.createCondNum(father.getArticle(), numCondSec, comp, d,
+							(QuestionNum) q);
 			return condNum;
 		}
-		
+
 		return null;
-		
+
 	}
 
 }
