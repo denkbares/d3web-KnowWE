1d43991d637e8a1b378f8e0ec7eb92e83af0b077
Markus Friedrich <markus.friedrich@denkbares.com>
2010-02-19 15:10:23 +0000

    Added comments

--- d3web-KnowWE/KnowWE/src/test/java/de/knowwe/plugin/test/PluginTest.java
+++ d3web-KnowWE/KnowWE/src/test/java/de/knowwe/plugin/test/PluginTest.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2009 denkbares GmbH
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package de.knowwe.plugin.test;
 
 import java.io.IOException;
@@ -6,8 +24,19 @@ import de.d3web.plugin.test.InitPluginManager;
 import de.knowwe.plugin.Plugins;
 import junit.framework.TestCase;
 
-public class PluginTest extends TestCase{
-	
+/**
+ * Simple JUnit test for plugins
+ * 
+ * @author Markus Friedrich (denkbares GmbH)
+ */
+public class PluginTest extends TestCase {
+
+	/**
+	 * Tries to load all extensions. Doesn't check anything, if a class is
+	 * missing, a ClassNotFoundException will be thrown
+	 * 
+	 * @throws IOException
+	 */
 	public void testPlugins() throws IOException {
 		InitPluginManager.init();
 		Plugins.getGlobalTypes();
