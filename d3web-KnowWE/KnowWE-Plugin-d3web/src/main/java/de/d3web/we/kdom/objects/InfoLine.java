6d0191dbb6bda6febfcacaf4b9dda2a4fb4e9996
Markus Friedrich <markus.friedrich@denkbares.com>
2010-03-04 18:44:03 +0000

    Added InfoLine

---
+++ d3web-KnowWE/KnowWE-Plugin-d3web/src/main/java/de/d3web/we/kdom/objects/InfoLine.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2010 denkbares GmbH
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package de.d3web.we.kdom.objects;
+
+import de.d3web.we.kdom.objects.D3webID;
+import de.d3web.we.kdom.sectionFinder.AllTextFinderTrimmed;
+/**
+ * KnowWEObjectType representing text containing information
+ *
+ * @author Markus Friedrich (denkbares GmbH)
+ */
+public class InfoLine extends D3webID<String> {
+
+	public InfoLine() {
+		super("info");
+		setSectionFinder(AllTextFinderTrimmed.getInstance());
+	}
+
+}
