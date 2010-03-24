517488b30a4361558ca8120e2d2698f95da1b7de
Jochen Reutelshöfer <jochen.reutelshoefer@denkbares.com>
2010-03-24 14:18:04 +0000

    CompositeCondition: * allowing for endline-comment after ')' * added some comments

--- d3web-KnowWE/KnowWE-Plugin-d3web/src/main/java/de/d3web/we/kdom/rulesNew/CompositeCondition.java
+++ d3web-KnowWE/KnowWE-Plugin-d3web/src/main/java/de/d3web/we/kdom/rulesNew/CompositeCondition.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2009 Chair of Artificial Intelligence and Applied Informatics
+ *                    Computer Science VI, University of Wuerzburg
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
 package de.d3web.we.kdom.rulesNew;
 
 import java.util.ArrayList;
@@ -8,6 +28,7 @@ import de.d3web.we.kdom.DefaultAbstractKnowWEObjectType;
 import de.d3web.we.kdom.Section;
 import de.d3web.we.kdom.basic.AnonymousType;
 import de.d3web.we.kdom.constraint.ExclusiveType;
+import de.d3web.we.kdom.dashTree.LineEndComment;
 import de.d3web.we.kdom.renderer.FontColorRenderer;
 import de.d3web.we.kdom.sectionFinder.AllTextFinderTrimmed;
 import de.d3web.we.kdom.sectionFinder.OneOfStringEnumFinder;
@@ -15,6 +36,15 @@ import de.d3web.we.kdom.sectionFinder.SectionFinder;
 import de.d3web.we.kdom.sectionFinder.SectionFinderResult;
 import de.d3web.we.utils.SplitUtility;
 
+/**
+ * This class defines a KDOM-Schema to parse composite conditions as known from
+ * proposition logics, using 'AND', 'OR', 'NOT' as keywords and brackets '(' and
+ * ')' for to express association boundaries
+ *
+ *
+ * @author Jochen
+ *
+ */
 public class CompositeCondition extends DefaultAbstractKnowWEObjectType {
 
 	public static char BRACE_OPEN = '(';
@@ -22,25 +52,50 @@ public class CompositeCondition extends DefaultAbstractKnowWEObjectType {
 
 	@Override
 	protected void init() {
+		// this composite takes everything it gets => needs suitable wrapper
+		// type as father
 		this.sectionFinder = new AllTextFinderTrimmed();
 		this.setCustomRenderer(new de.d3web.we.kdom.renderer.KDOMDepthFontSizeRenderer());
 
-		BracedCondition braced = new BracedCondition();
-		this.childrenTypes.add(braced);
-		braced.addChildType(this);
 
+		// a composite condition may either be a BracedCondition,...
+		BracedCondition braced = new BracedCondition(); // contains the brackets
+														// and the
+														// endline-comments
+		this.childrenTypes.add(braced);
+		BracedConditionContent bracedContent = new BracedConditionContent(); // without
+																				// brackets
+																				// and
+																				// comments
+		braced.addChildType(bracedContent);
+		braced.addChildType(new LineEndComment()); // explicit nodes for the
+													// endline-comments
+		bracedContent.addChildType(this);
+
+
+		// ... a negated expression,...
 		NegatedExpression negatedExpression = new NegatedExpression();
 		this.childrenTypes.add(negatedExpression);
-		negatedExpression.addChildType(this);
+		negatedExpression.addChildType(this); // a NegatedExpression again
+												// allows for a
+												// CompositeCondition
 
+
+		// ...a conjuctive expression,...
 		Conjunct conj = new Conjunct();
 		this.addChildType(conj);
-		conj.addChildType(this);
+		conj.addChildType(this); // Conjuncts again allow for a
+									// CompositeCondition
 
+
+		// ... a disjuctive expression,...
 		Disjunct disj = new Disjunct();
 		this.addChildType(disj);
-		disj.addChildType(this);
+		disj.addChildType(this); // Disjuncts again allow for a
+									// CompositeCondition
 
+
+		// ... or finally a TerminalCondition which stops the recursive descent
 		this.addChildType(new TerminalCondition());
 	}
 }
@@ -56,7 +111,8 @@ class TerminalCondition extends DefaultAbstractKnowWEObjectType {
 class Disjunct extends DefaultAbstractKnowWEObjectType {
 	@Override
 	protected void init() {
-		this.sectionFinder = new ConjunctionSectionFinder(new String[] { "OR" });
+		// TODO make it work for "OR", "ODER"
+		this.sectionFinder = new ConjunctSectionFinder(new String[] { "OR" });
 	}
 }
 
@@ -67,7 +123,7 @@ class Conjunct extends DefaultAbstractKnowWEObjectType {
 
 	@Override
 	protected void init() {
-		this.setSectionFinder(new ConjunctionSectionFinder(CONJ_SIGNS));
+		this.setSectionFinder(new ConjunctSectionFinder(CONJ_SIGNS));
 	}
 
 }
@@ -101,11 +157,11 @@ class NegatedExpression extends DefaultAbstractKnowWEObjectType {
 
 }
 
-class ConjunctionSectionFinder extends SectionFinder {
+class ConjunctSectionFinder extends SectionFinder {
 
 	private final String[] signs;
 
-	public ConjunctionSectionFinder(String[] signs) {
+	public ConjunctSectionFinder(String[] signs) {
 		this.signs = signs;
 		this.addConstraint(ExclusiveType.getInstance());
 	}
@@ -124,7 +180,8 @@ class ConjunctionSectionFinder extends SectionFinder {
 
 		Collections.sort(allFoundOps);
 		int lastBeginIndex = 0;
-		// TODO: caution works only for OP signs with same length!!
+		// TODO: caution works only for OP signs with same length!! (e.g., not
+		// with 'OR' and 'ODER')
 		for (Integer integer : allFoundOps) {
 			results.add(new SectionFinderResult(lastBeginIndex, integer));
 			lastBeginIndex = integer + signs[0].length();
@@ -137,14 +194,31 @@ class ConjunctionSectionFinder extends SectionFinder {
 
 }
 
-// class BracedConditionContent extends DefaultAbstractKnowWEObjectType {
-// @Override
-// protected void init() {
-// // TODO Auto-generated method stub
-// super.init();
-// }
-//
-// }
+class BracedConditionContent extends DefaultAbstractKnowWEObjectType {
+	@Override
+	protected void init() {
+		this.sectionFinder = new BracedConditionContentFinder();
+	}
+
+	class BracedConditionContentFinder extends SectionFinder {
+
+		@Override
+		public List<SectionFinderResult> lookForSections(String text, Section father) {
+			String trimmed = text.trim();
+			int leadingSpaces = text.indexOf(trimmed);
+			if (trimmed.startsWith(Character.toString(CompositeCondition.BRACE_OPEN))) {
+				int closingBracket = SplitUtility.findIndexOfClosingBracket(trimmed, 0,
+						CompositeCondition.BRACE_OPEN, CompositeCondition.BRACE_CLOSED);
+
+				return SectionFinderResult.createSingleItemList(new SectionFinderResult(
+						leadingSpaces + 1, closingBracket));
+
+			}
+			return null;
+		}
+
+	}
+}
 
 class BracedCondition extends DefaultAbstractKnowWEObjectType {
 
@@ -172,19 +246,27 @@ class BracedCondition extends DefaultAbstractKnowWEObjectType {
 			int closingBracket = SplitUtility.findIndexOfClosingBracket(trimmed, 0,
 					CompositeCondition.BRACE_OPEN, CompositeCondition.BRACE_CLOSED);
 
+			// an embracedExpression needs to to start and end with '(' and ')'
 			if (trimmed.startsWith(Character.toString(CompositeCondition.BRACE_OPEN))
 					&& trimmed.endsWith(Character.toString(CompositeCondition.BRACE_CLOSED))) {
+				// and the ending ')' needs to clse the opening
 				if (closingBracket == trimmed.length() - 1) {
 					return SectionFinderResult.createSingleItemList(new SectionFinderResult(
-							leadingSpaces + 1, text.length() - followingSpaces - 1));
+							leadingSpaces, text.length() - followingSpaces));
 				}
 			}
 
+			// OR an embracedExpression can be concluded with a lineEnd-comment
 			int lastEndLineCommentSymbol = SplitUtility.lastIndexOfUnquoted(text, "//");
-			if (lastEndLineCommentSymbol > -1) {
+			// so has to start with '(' and have a lineend-comment-sign after
+			// the closing bracket but nothing in between!
+			if (trimmed.startsWith(Character.toString(CompositeCondition.BRACE_OPEN))
+					&& lastEndLineCommentSymbol > -1) {
+				// TODO fix: < 3 is inaccurate
+				// better check that there is no other expression in between
 				if (lastEndLineCommentSymbol - closingBracket < 3) {
 					return SectionFinderResult.createSingleItemList(new SectionFinderResult(
-							leadingSpaces + 1, lastEndLineCommentSymbol - 1));
+							leadingSpaces, text.length()));
 				}
 			}
 
