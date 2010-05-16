901c97b961f7503a04e343e0ebd1a5f5196ec244
Sebastian Furth <sebastian.furth@denkbares.com>
2010-05-16 07:59:13 +0000

    * changes due to changes in FindingSetEvent

--- d3web-KnowWE/KnowWE-Plugin-WISEC-jpf-plugin/src/main/java/de/d3web/we/wisec/event/WISECFindingSetEventListener.java
+++ d3web-KnowWE/KnowWE-Plugin-WISEC-jpf-plugin/src/main/java/de/d3web/we/wisec/event/WISECFindingSetEventListener.java
@@ -1,5 +1,12 @@
 package de.d3web.we.wisec.event;
 
+import org.openrdf.query.MalformedQueryException;
+import org.openrdf.query.Query;
+import org.openrdf.query.QueryLanguage;
+import org.openrdf.repository.RepositoryConnection;
+import org.openrdf.repository.RepositoryException;
+
+import de.d3web.we.core.SemanticCore;
 import de.d3web.we.event.Event;
 import de.d3web.we.event.EventListener;
 import de.d3web.we.event.FindingSetEvent;
@@ -18,10 +25,27 @@ public class WISECFindingSetEventListener implements EventListener<FindingSetEve
 			FindingSetEvent event) {
 		
 		event.getQuestion();
-		event.getAnswer();
+		event.getValue();
 		
-		// We have to do something with that.
+		// TODO: Build the SPARQL Query
+		String queryString = "";
 		
+		// TODO: Send the Query to the Semantic Core {@Link SparqlDelegateRenderer}
+		SemanticCore sc = SemanticCore.getInstance();
+		RepositoryConnection con = sc.getUpper().getConnection();
+		
+		try {
+			Query query = con.prepareQuery(QueryLanguage.SPARQL, queryString);
+		} catch (RepositoryException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MalformedQueryException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// TODO: Do something with the result (Set NumValues in the session)
+	
 	}
 
 }
