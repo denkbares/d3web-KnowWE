2577315e05672f5313f479cb186d9c5304497084
Jochen Reutelshöfer <jochen.reutelshoefer@denkbares.com>
2010-05-18 10:38:12 +0000

    Some small modification of QuestionDashTreeOnlyAnswersRenderer: also shows description popup for descriptions attached to (Answer-)Choice objects

--- d3web-KnowWE/KnowWE-Plugin-d3web/src/main/java/de/d3web/we/kdom/questionTreeNew/dialog/QuestionDashTreeOnlyAnswersRenderer.java
+++ d3web-KnowWE/KnowWE-Plugin-d3web/src/main/java/de/d3web/we/kdom/questionTreeNew/dialog/QuestionDashTreeOnlyAnswersRenderer.java
@@ -31,6 +31,7 @@ import de.d3web.core.knowledge.terminology.info.MMInfoStorage;
 import de.d3web.core.knowledge.terminology.info.Property;
 import de.d3web.core.manage.KnowledgeBaseManagement;
 import de.d3web.core.session.Session;
+import de.d3web.core.session.values.Choice;
 import de.d3web.we.d3webModule.D3webModule;
 import de.d3web.we.kdom.KnowWEArticle;
 import de.d3web.we.kdom.KnowWEObjectType;
@@ -38,6 +39,7 @@ import de.d3web.we.kdom.Section;
 import de.d3web.we.kdom.basic.PlainText;
 import de.d3web.we.kdom.dashTree.DashTreeElement;
 import de.d3web.we.kdom.dashTree.SubTree;
+import de.d3web.we.kdom.objects.AnswerDef;
 import de.d3web.we.kdom.questionTreeNew.QuestionDashTreeElementContent;
 import de.d3web.we.kdom.rendering.CustomRenderer;
 import de.d3web.we.kdom.rendering.DelegateRenderer;
@@ -51,9 +53,9 @@ import de.d3web.we.wikiConnector.KnowWEUserContext;
  * This renderer renders a collapsible/expandable decision tree. This tree
  * is used to guide the user when answering the questions of the
  * decision tree. This renderer only renders the answers of the decision tree.
- * If the question should be present, please use the 
+ * If the question should be present, please use the
  * {@link QuestionDashTreeRenderer} which provides also the questions.
- * 
+ *
  * @author smark
  * @since 2010/03/25
  * @see KnowWEDomRenderer
@@ -63,21 +65,21 @@ public class QuestionDashTreeOnlyAnswersRenderer extends CustomRenderer {
 	@Override
 	public boolean doesApply(String user, String topic, RenderingMode type) {
 		return true;
-	}		
-	
+	}
+
 	@Override
 	public void render(KnowWEArticle article, Section sec,
 			KnowWEUserContext user, StringBuilder string) {
 
-		List<Section<? extends KnowWEObjectType>> children = sec.getChildren(); //get qclass lines 
-		
+		List<Section<? extends KnowWEObjectType>> children = sec.getChildren(); //get qclass lines
+
 		for (Section<? extends KnowWEObjectType> section : children) {
 			parseSubtree(article, section, user, string, true);
-		}		
+		}
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @param article
 	 * @param section
 	 * @param user
@@ -89,20 +91,20 @@ public class QuestionDashTreeOnlyAnswersRenderer extends CustomRenderer {
 			KnowWEUserContext user,
 			StringBuilder string,
 			boolean dte){
-		
+
 		string.append(KnowWEUtils.maskHTML("<ul>"));
-	
+
 		for (Section<? extends KnowWEObjectType> s : section.getChildren()) {
-			
+
 			if( s.getObjectType() instanceof PlainText ) continue;
-			
+
             if( s.getObjectType() instanceof SubTree ) {
 				//string.append(KnowWEUtils.maskHTML("<li class=\"qline pointer\"><p>")); //body class
-				
+
 				List<Section<SubTree>> children = s.findChildrenOfType(SubTree.class);
 				if(children.size() < 1) {
 					string.append(KnowWEUtils.maskHTML("<li class=\"qanswer\"><p>"));
-					
+
 					renderLine( article, s.findChildOfType( DashTreeElement.class ), user, string);
 					//DelegateRenderer.getInstance().render(article, s.findChildOfType( DashTreeElement.class ), user, string);
 				} else {
@@ -112,11 +114,11 @@ public class QuestionDashTreeOnlyAnswersRenderer extends CustomRenderer {
 				string.append(KnowWEUtils.maskHTML("</p></li>"));
 			}
 		}
-		string.append(KnowWEUtils.maskHTML("</ul>"));	
+		string.append(KnowWEUtils.maskHTML("</ul>"));
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @param article
 	 * @param children
 	 * @param user
@@ -127,14 +129,14 @@ public class QuestionDashTreeOnlyAnswersRenderer extends CustomRenderer {
 			List<Section<SubTree>> children,
 			KnowWEUserContext user,
 			StringBuilder string){
-		
+
 		string.append(KnowWEUtils.maskHTML("<ul>"));
-		
-		for (Section<? extends KnowWEObjectType> s : children) {		
-			
+
+		for (Section<? extends KnowWEObjectType> s : children) {
+
 			if( s.getChildren().size() > 1 ){
 				string.append(KnowWEUtils.maskHTML("<li class=\"\"><p>")); //head class
-				
+
 				renderLine( article, s.findChildOfType( DashTreeElement.class ), user, string);
 				//DelegateRenderer.getInstance().render(article, s.findChildOfType( DashTreeElement.class ), user, string);
 				parseSubtree(article, s, user, string, false);
@@ -153,14 +155,14 @@ public class QuestionDashTreeOnlyAnswersRenderer extends CustomRenderer {
 	/**
 	 * Renders the collapsible tree and adds if found some additional information that
 	 * is stored within an {@link MMInfoStorage} object.
-	 * 
+	 *
 	 * @param section
 	 * @return
 	 */
 	private void renderLine(KnowWEArticle article, Section<? extends KnowWEObjectType> section, KnowWEUserContext user, StringBuilder string){
 		//render the current section
-		DelegateRenderer.getInstance().render(article, section, user, string); 
-		
+		DelegateRenderer.getInstance().render(article, section, user, string);
+
 		//render the additional information icon
 		Session theCase = D3webUtils.getSession(article.getTitle(), user, article.getWeb());
 		if (theCase != null) {
@@ -168,16 +170,29 @@ public class QuestionDashTreeOnlyAnswersRenderer extends CustomRenderer {
 			String name = child.getOriginalText();
 
 			KnowledgeBaseManagement kbm = D3webModule.getKnowledgeRepresentationHandler(article.getWeb()).getKBM(article, null, section);
-			
-			NamedObject o = kbm.findQuestion( name );
 
+			NamedObject o = kbm.findQuestion(name);
+
+			Section<AnswerDef> answerSec = section.findSuccessor(AnswerDef.class);
+			Choice c = null;
+
+			 MMInfoStorage mminfo = null;
+
+			if (answerSec != null) {
+				c = answerSec.get().getObject(answerSec);
+				if (c != null) {
+					mminfo = (MMInfoStorage) c.getProperties().getProperty(Property.MMINFO);;
+				}
+			}
 			if( o != null ){
-			    MMInfoStorage mminfo = (MMInfoStorage) o.getProperties().getProperty(Property.MMINFO);
+				mminfo = (MMInfoStorage) o.getProperties().getProperty(Property.MMINFO);
+
+			}
 			    if( mminfo != null) {
 				    DCMarkup markup = new DCMarkup();
 				    markup.setContent(DCElement.TITLE, "description");
 	    	        Set<MMInfoObject> result = mminfo.getMMInfo(markup);
-	    	        
+
 	    	        String attr = "";
 			        for (MMInfoObject infoObject : result) {
 			        	attr = infoObject.getContent();
@@ -185,7 +200,6 @@ public class QuestionDashTreeOnlyAnswersRenderer extends CustomRenderer {
 			        String imgAlt = D3webModule.getKwikiBundle_d3web(user).getString("KnowWE.QuestionDashTree.image.altdescription");
 			        string.append(KnowWEUtils.maskHTML("<img class=\"collapsible-info\" rel=\"{info : '"+attr+"'}\" src=\"KnowWEExtension/images/question.gif\" height=\"16\" width=\"16\" title=\""+imgAlt+"\"/>"));
 			    }
-			}
-		}          
-	}	
+		}
+	}
 }
