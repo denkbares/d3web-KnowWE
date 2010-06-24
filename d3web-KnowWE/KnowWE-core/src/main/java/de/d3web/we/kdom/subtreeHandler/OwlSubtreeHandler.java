3de9f6b966fd1089916ec237947b525768ca2a59
Jochen Reutelshöfer <jochen.reutelshoefer@denkbares.com>
2010-06-24 07:39:20 +0000

    trying to catch exception in owlim which is always killing wmswiki (cannot reproduce problem locally)

--- d3web-KnowWE/KnowWE-core/src/main/java/de/d3web/we/kdom/subtreeHandler/OwlSubtreeHandler.java
+++ d3web-KnowWE/KnowWE-core/src/main/java/de/d3web/we/kdom/subtreeHandler/OwlSubtreeHandler.java
@@ -1,7 +1,10 @@
 /**
- * 
+ *
  */
 package de.d3web.we.kdom.subtreeHandler;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import de.d3web.we.core.SemanticCore;
 import de.d3web.we.kdom.KnowWEArticle;
@@ -16,7 +19,7 @@ import de.d3web.we.kdom.Section;
  *            owl-generating (i.e. those that call
  *            SemanticCore.addstatemnts(...)) SubtreeHandlers to facilitate the
  *            incremental build of Articles
- * 
+ *
  */
 public abstract class OwlSubtreeHandler<T extends KnowWEObjectType> extends
 		SubtreeHandler<T> {
@@ -24,7 +27,36 @@ public abstract class OwlSubtreeHandler<T extends KnowWEObjectType> extends
 	@Override
 	public void destroy(KnowWEArticle article, Section s) {
 		super.destroy(article, s);
-		SemanticCore.getInstance().clearContext(s);
+		try {
+			SemanticCore.getInstance().clearContext(s);
+		}
+		catch (Exception e) {
+			// TODO find some fix for the exception in owlim (see underneath)
+
+			// trying to avoid the wiki crashing because of the Exception thrown
+			// in owlim-lib
+
+			Logger.getLogger(this.getClass().getName()).log(Level.WARNING,
+			"exception in owlim catched: \n" +
+							" java.lang.ArrayIndexOutOfBoundsException2" +
+							"com.ontotext.trree.owlim_ext.c.do(Unknown Source)" +
+							"com.ontotext.trree.owlim_ext.c$2.remove(Unknown Source)");
+
+
+
+			// java.lang.ArrayIndexOutOfBoundsException2
+			// com.ontotext.trree.owlim_ext.c.do(Unknown Source)
+			// com.ontotext.trree.owlim_ext.c$2.remove(Unknown Source)
+			// com.ontotext.trree.owlim_ext.Repository.removeStatements(Unknown
+			// Source)
+			// com.ontotext.trree.owlim_ext.SailConnectionImpl.commit(SailConnectionImpl.java:222)
+			// org.openrdf.repository.sail.SailRepositoryConnection.commit(SailRepositoryConnection.java:81)
+			// org.openrdf.repository.base.RepositoryConnectionBase.setAutoCommit(RepositoryConnectionBase.java:168)
+			// org.openrdf.repository.base.RepositoryConnectionBase.remove(RepositoryConnectionBase.java:485)
+			// de.d3web.we.core.SemanticCore.clearContext(SemanticCore.java:491)
+			// de.d3web.we.kdom.subtreeHandler.OwlSubtreeHandler.destroy(OwlSubtreeHandler.java:27)
+
+		}
 	}
 
 }
