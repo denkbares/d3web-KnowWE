b34c64e4997e86b7f9570090b7d4f573201f2854
Christian Grotheer <grotheer@d3web.svn.invalid>
2010-07-17 19:53:26 +0000

    Added Page-access-logger to HermesModule. * logs every page access to KnowWEExtensionPath/tmp/pagelogger.log (Timestamp Username Pagetitle) * by default deactivated in plugin.xml

---
+++ d3web-KnowWE/KnowWE-Plugin-HermesModule-jpf-plugin/src/main/java/de/d3web/we/hermes/logger/PageLoggerHandler.java
@@ -0,0 +1,37 @@
+package de.d3web.we.hermes.logger;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
+import de.d3web.we.core.KnowWEEnvironment;
+import de.d3web.we.module.PageAppendHandler;
+import de.d3web.we.wikiConnector.KnowWEUserContext;
+
+public class PageLoggerHandler implements PageAppendHandler {
+	public boolean log = true;
+	
+	@Override
+	public String getDataToAppend(String topic, String web,
+			KnowWEUserContext user) {
+		if (log) {
+			try {
+				BufferedWriter buffy = new BufferedWriter(new FileWriter(KnowWEEnvironment.getInstance().getKnowWEExtensionPath()+"/tmp/Pagelogger.log",true));
+				SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+				String uhrzeit = sdf.format(new Date(System.currentTimeMillis()));
+				buffy.append(uhrzeit+";"+user.getUsername()+";"+user.getPage()+"\n");
+				buffy.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public boolean isPre() {
+		return false;
+	}
+}
