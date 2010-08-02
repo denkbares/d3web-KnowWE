9209c26632d8ba52a15c7936a991cc61c778ed87
Max Diez <MaxDiez@d3web.svn.invalid>
2010-08-02 13:48:26 +0000

    temporary fix for encoded QuickEdit content

--- d3web-KnowWE/KnowWE-core/src/main/java/de/d3web/we/core/KnowWEArticleManager.java
+++ d3web-KnowWE/KnowWE-core/src/main/java/de/d3web/we/core/KnowWEArticleManager.java
@@ -21,6 +21,7 @@
 package de.d3web.we.core;
 
 import java.io.File;
+import java.net.URLDecoder;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -208,7 +209,7 @@ public class KnowWEArticleManager {
 	private void appendTextReplaceNode(Section sec,
 			Map<String, String> nodesMap, StringBuffer newText) {
 		if (nodesMap.containsKey(sec.getID())) {
-			newText.append(nodesMap.get(sec.getID()));
+			newText.append(URLDecoder.decode(nodesMap.get(sec.getID())));
 			return;
 		}
 		List<Section> children = sec.getChildren();
