3f9cf5479744a231a5450f01997163890f942539
Martina Freiberg <mfreiberg@d3web.svn.invalid>
2010-08-03 11:42:35 +0000

    Adapted the SPAQL to also return the right FAQs in case of more complex query cases. :-)

--- d3web-KnowWE/KnowWE-Plugin-FAQ-jpf-plugin/src/main/java/de/d3web/we/faq/FAQTagHandler.java
+++ d3web-KnowWE/KnowWE-Plugin-FAQ-jpf-plugin/src/main/java/de/d3web/we/faq/FAQTagHandler.java
@@ -102,34 +102,38 @@ public class FAQTagHandler extends AbstractTagHandler {
 		// check, whether all faqs should be fetched ("all") or if there are
 		// further specifications‚
 		String faqVal = values.get("faq");
-		if (faqVal != null && faqVal.equals("all")) {
-			querystring = SPARQL_BASE;
-		}
-		else { // further specifications‚
+
+		// in case faq-attribute is not "all", the SPARQL needs adaption
+		if (faqVal != null && !faqVal.equals("all")) {
+
 			String major = "";
 			String status = "";
 
-			// check for all annotations whether they are provided. In case they
-			// are, replace the variables with the given annotation. Then, the
-			// first variable needs to be reset to SPARQL variable style
-			// also, the String with the variable needs to be inserted again to
-			// get the value nevertheless
+			// check for all annotations if provided. In case they are, replace
+			// the variables with the given annotation.
+
+			// check for major, first
 			if (values.get("major") != null) {
+
 				major = values.get("major");
-				// String[] majors = major.split(" ");
-				// for (String m : majors) {
+
 				try {
-					querystring = querystring.replace("}", ". ?t lns:hasmajor \"" + major + "\" }");
-					querystring = querystring.replace("}", ". FILTER (regex(?m, \"" + major
-							+ "\"))}");
+					querystring =
+							querystring.replace("WHERE {", "WHERE { " +
+									"FILTER (regex(?m, \"" + major + "\")) .");
+					querystring = querystring.replace("}", ". ?t lns:hasmajor \"" + major + "\" .}");
 				}
 
+				// SELECT ?title
+				// WHERE {
+				// _:book :title ?title .
 				// FILTER (regex(?title, "SPARQL")) .
+				// }
+
 				catch (Exception e) {
 					return "Illegal query String: " + querystring + "<br />"
 								+ " no valid parameter for: " + "MAJOR";
 				}
-				// }
 
 			}
 			if (values.get("status") != null) {
@@ -147,7 +151,7 @@ public class FAQTagHandler extends AbstractTagHandler {
 				// }
 			}
 		}
-		// System.out.println(querystring);
+		System.out.println(querystring);
 
 		/**
 		 * establish connection and send query
@@ -222,7 +226,7 @@ public class FAQTagHandler extends AbstractTagHandler {
 
 			resultVals = sortedFAQs.get(i).split("----");
 
-			// build the HTML
+			// build the HTML represnting one faq entry
 			string.append(KnowWEUtils.maskHTML("<div class='faq_question'> Q: "));
 			string.append(KnowWEUtils.maskHTML(resultVals[0]));
 			string.append(KnowWEUtils.maskHTML("</div>"));
@@ -235,12 +239,19 @@ public class FAQTagHandler extends AbstractTagHandler {
 			string.append(KnowWEUtils.maskHTML(resultVals[3]));
 			string.append(KnowWEUtils.maskHTML("</div>"));
 			string.append(KnowWEUtils.maskHTML("</div>"));
-
-			System.out.println(string.toString());
 		}
 		return string.toString();
 	}
 
+	/**
+	 * Takes a TupleQueryResult from the SPARQL query and creates a list with
+	 * all FAQ entries, the entry vals each separated by "----", that is sorted
+	 * alphabetically
+	 * 
+	 * @created 03.08.2010
+	 * @param result the TupleQueryResult from the SPARQL query
+	 * @return the sorted list of FAQ entries in String representation
+	 */
 	private List<String> sortAlphabetically(TupleQueryResult result) {
 
 		List<String> faqList = new ArrayList<String>();
@@ -290,10 +301,10 @@ public class FAQTagHandler extends AbstractTagHandler {
 			}
 		}
 		catch (QueryEvaluationException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
+		// Sorts the FAQ-Strings correctly regarding the alphabet
 		Collator coll = Collator.getInstance(Locale.GERMAN);
 		coll.setStrength(Collator.SECONDARY);
 		Collections.sort(faqList, coll);
@@ -301,22 +312,45 @@ public class FAQTagHandler extends AbstractTagHandler {
 		return faqList;
 	}
 
+	/**
+	 * replaces the link-markup within a FAQ answer text "-L- -/L-" with correct
+	 * HTML linking markup
+	 * 
+	 * @created 03.08.2010
+	 * @param a the String potentially containing the link
+	 * @return the correctly HTML-marked-up answer string
+	 */
 	private String createLinks(String a) {
-		a = a.replaceAll("-L-", "<a href='");
-		a = a.replaceAll("-/L-", "' ><b>" + "link" + "</b></a>");
-
+		if (a.isEmpty() || a.equals(" ") || !a.contains("-L-")) {
+			return a;
+		}
+		String link = "";
+		while (a.contains("-L-")) {
+			link = a.substring(a.indexOf("-L-") + 3, a.indexOf("-/L-"));
+			a = a.replaceFirst("-L-", "<b><a href='" + link + "'>");
+			a = a.replaceFirst("-/L-", "</a></b>");
+		}
 		return a;
 	}
 
+	/**
+	 * replaces the attachment-markup within a FAQ answer text "-A- -/A-" with
+	 * correct HTML linking markup for attached files, e.g. PDFs
+	 * 
+	 * @created 03.08.2010
+	 * @param a the String potentially containing the attachment-link
+	 * @return the correctly HTML-marked-up answer string
+	 */
 	private String resolvePDFs(String a) {
 		if (a.isEmpty() || a.equals(" ") || !a.contains("-A-")) {
 			return a;
 		}
-		String name = a.substring(a.indexOf("-A-") + 3, a.indexOf("-/A-"));
-		System.out.println(name);
-		a = a.replaceAll("-A-", "<b><a href='/KnowWE/attach/FAQ%20Entry/" + name + "'>");
-		a = a.replaceAll("-/A-", "</a></b>");
-		System.out.println(a);
+		String name = "";
+		while (a.contains("-A-")) {
+			name = a.substring(a.indexOf("-A-") + 3, a.indexOf("-/A-"));
+			a = a.replaceAll("-A-", "<b><a href='/KnowWE/attach/FAQ%20Entry/" + name + "'>");
+			a = a.replaceAll("-/A-", "</a></b>");
+		}
 		return a;
 	}
 
