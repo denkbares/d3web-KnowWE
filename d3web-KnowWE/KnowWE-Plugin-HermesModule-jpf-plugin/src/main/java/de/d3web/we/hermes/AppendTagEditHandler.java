2075b95a8652acf115c5527d5a3321715635855e
Jochen Reutelshöfer <jochen.reutelshoefer@denkbares.com>
2010-08-11 16:05:41 +0000

    moved Tagging out of KnowWE-Core to Semantic-Core-Plugin

--- d3web-KnowWE/KnowWE-Plugin-HermesModule-jpf-plugin/src/main/java/de/d3web/we/hermes/AppendTagEditHandler.java
+++ d3web-KnowWE/KnowWE-Plugin-HermesModule-jpf-plugin/src/main/java/de/d3web/we/hermes/AppendTagEditHandler.java
@@ -2,19 +2,19 @@ package de.d3web.we.hermes;
 
 import java.util.HashMap;
 
+import de.d3web.we.core.semantic.TagEditPanel;
 import de.d3web.we.module.PageAppendHandler;
-import de.d3web.we.taghandler.TagEditPanel;
 import de.d3web.we.taghandler.TagHandler;
 import de.d3web.we.wikiConnector.KnowWEUserContext;
 
 public class AppendTagEditHandler implements PageAppendHandler {
 
-	
-	private TagHandler tagHandler = new TagEditPanel();
-	
+	private final TagHandler tagHandler = new TagEditPanel();
+
 	@Override
 	public String getDataToAppend(String topic, String web, KnowWEUserContext user) {
-		return "\\\\"+tagHandler.render(topic, user, new HashMap<String,String>(), web);
+		return "\\\\"
+				+ tagHandler.render(topic, user, new HashMap<String, String>(), web);
 	}
 
 	@Override
