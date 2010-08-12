045e62ff37c313fadacb0262f660f13ccbcae554
Martina Freiberg <mfreiberg@d3web.svn.invalid>
2010-08-12 23:10:52 +0000

    Preparing the "infrastructure" of the externalized and plugin-ed solution panel

--- d3web-KnowWE/KnowWE-Plugin-SolutionPanel-jpf-plugin/src/main/java/de/d3web/we/solutionpanel/SolutionPanelTagHandler.java
+++ d3web-KnowWE/KnowWE-Plugin-SolutionPanel-jpf-plugin/src/main/java/de/d3web/we/solutionpanel/SolutionPanelTagHandler.java
@@ -1,6 +1,34 @@
 package de.d3web.we.solutionpanel;
 
+import java.util.Map;
 
-public class SolutionPanelTagHandler {
+import de.d3web.we.core.KnowWERessourceLoader;
+import de.d3web.we.taghandler.AbstractTagHandler;
+import de.d3web.we.wikiConnector.KnowWEUserContext;
 
+public class SolutionPanelTagHandler extends AbstractTagHandler {
+
+	/**
+	 * Create the TagHandler --> "quickInterview" defines the "name" of the tag,
+	 * so the tag is inserted in the wiki page like [KnowWEPlugin
+	 * quickInterview]
+	 */
+	public SolutionPanelTagHandler() {
+		super("solutionPanel");
+		KnowWERessourceLoader.getInstance().add("solPane.css",
+				KnowWERessourceLoader.RESOURCE_STYLESHEET);
+
+	}
+
+	// @Override
+	// public String getDescription(KnowWEUserContext user) {
+	// return
+	// D3webModule.getKwikiBundle_d3web(user).getString("KnowWE.QuickInterview.description");
+	// }
+
+	@Override
+	public String render(String topic, KnowWEUserContext user, Map<String,
+			String> values, String web) {
+		return "here soon the new solution panel will be created and displayed :-)";
+	}
 }
