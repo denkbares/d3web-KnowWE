0a786804f0d91455005d6ac36c2dedf7e4d74753
Marc-Oliver Ochlast <ochlast@d3web.svn.invalid>
2010-08-19 08:44:38 +0000

    * added "Uni WÜ"-license header to all d3web-KnowWE Classes which miss a header. Now every java-class is licensed! *cheers*

--- d3web-KnowWE/d3web-KnOfficeParser/src/main/java/de/d3web/KnOfficeParser/xcl/XCL.java
+++ d3web-KnowWE/d3web-KnOfficeParser/src/main/java/de/d3web/KnOfficeParser/xcl/XCL.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2010 Chair of Artificial Intelligence and Applied Informatics
+ * Computer Science VI, University of Wuerzburg
+ * 
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
+ */
+
 // $ANTLR 3.1.1 D:\\eclipse workspaces\\Uni SVN
 // 64bit\\d3web-KnowWE\\d3web-KnOfficeParser\\src\\main\\antlr\\XCL.g 2010-02-22
 // 09:40:24
