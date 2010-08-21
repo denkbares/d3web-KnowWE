b66b3386865bf9aed0890a61c41525f94bd6d21c
Martina Freiberg <mfreiberg@d3web.svn.invalid>
2010-08-21 02:21:29 +0000

    Adapted the SetSingleFindingAction to work with the new interview. (Code-line for the old interview is only commented out in case it should be needed while changing from old --> new the next weeks...)
    !!People out there, please also start using the new interview [{KnowWEPlugin quickInterview}] if an interview is needed in the wiki for 2 good reasons:
    - I can't (and won't) guarantee for the functionality of the old one any more
    - the more people use the new one, the faster (quite certain) existing bugs and misbehaviors will be detected and resolved :-) 
      (just learned that last lesson while trying to set up a quite specific kb that evening. So much *small* things detected "on the fly" *sigh*)

--- d3web-KnowWE/KnowWE-Plugin-d3web-jpf-plugin/src/main/java/de/d3web/we/action/SetSingleFindingAction.java
+++ d3web-KnowWE/KnowWE-Plugin-d3web-jpf-plugin/src/main/java/de/d3web/we/action/SetSingleFindingAction.java
@@ -1,17 +1,17 @@
 /*
  * Copyright (C) 2009 Chair of Artificial Intelligence and Applied Informatics
  * Computer Science VI, University of Wuerzburg
- * 
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -283,7 +283,12 @@ public class SetSingleFindingAction extends DeprecatedAbstractKnowWEAction {
 		}
 
 		try {
-			KnowWEFacade.getInstance().performAction("RefreshHTMLDialogAction", parameterMap);
+			// TODO WATCH!
+			// if anyone still needs to work with the old interview you need
+			// to activate the commented out line below.
+			// KnowWEFacade.getInstance().performAction("RefreshHTMLDialogAction",
+			// parameterMap);
+			KnowWEFacade.getInstance().performAction("QuickInterviewAction", parameterMap);
 		}
 		catch (IOException e) {
 			Logger.getLogger(this.getClass()).error(
