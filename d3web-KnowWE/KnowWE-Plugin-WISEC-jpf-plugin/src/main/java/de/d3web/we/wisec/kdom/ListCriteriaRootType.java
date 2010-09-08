764befc1348de80a92449fe141ccd368b6dcb57e
Jochen Reutelshöfer <jochen.reutelshoefer@denkbares.com>
2010-09-08 12:06:34 +0000

    added new Renderer for ListCriteriaRootType * not rendering annotations * not rendering header and PRE

--- d3web-KnowWE/KnowWE-Plugin-WISEC-jpf-plugin/src/main/java/de/d3web/we/wisec/kdom/ListCriteriaRootType.java
+++ d3web-KnowWE/KnowWE-Plugin-WISEC-jpf-plugin/src/main/java/de/d3web/we/wisec/kdom/ListCriteriaRootType.java
@@ -20,10 +20,17 @@
 
 package de.d3web.we.wisec.kdom;
 
+import java.util.List;
+
+import de.d3web.we.kdom.KnowWEArticle;
+import de.d3web.we.kdom.Section;
+import de.d3web.we.kdom.basic.PlainText;
+import de.d3web.we.kdom.defaultMarkup.AnnotationType;
 import de.d3web.we.kdom.defaultMarkup.DefaultMarkup;
 import de.d3web.we.kdom.defaultMarkup.DefaultMarkupType;
-import de.d3web.we.kdom.rendering.DelegateRenderer;
 import de.d3web.we.kdom.rendering.KnowWEDomRenderer;
+import de.d3web.we.utils.KnowWEUtils;
+import de.d3web.we.wikiConnector.KnowWEUserContext;
 import de.d3web.we.wisec.kdom.subtreehandler.ListCriteriaOWLSubtreeHandler;
 
 /**
@@ -46,10 +53,82 @@ public class ListCriteriaRootType extends DefaultMarkupType {
 
 	@Override
 	public KnowWEDomRenderer getRenderer() {
-		return DelegateRenderer.getInstance();
+		return new CriteriaListDefaultMarkupRenderer();
 	}
 
 	public ListCriteriaRootType() {
 		super(m);
+	}
+
+	/**
+	 * 
+	 * @author Jochen
+	 * @created 08.09.2010
+	 * 
+	 *          A modified DefaultMarkupRenderer that without PRE, header and
+	 *          annotations
+	 * 
+	 */
+	class CriteriaListDefaultMarkupRenderer extends KnowWEDomRenderer<DefaultMarkupType> {
+
+		private final String iconPath;
+
+		public CriteriaListDefaultMarkupRenderer() {
+			this(null);
+		}
+
+		public CriteriaListDefaultMarkupRenderer(String iconPath) {
+			this.iconPath = iconPath;
+		}
+
+		@Override
+		public void render(KnowWEArticle article, Section<DefaultMarkupType> section, KnowWEUserContext user, StringBuilder string) {
+
+			String id = section.getID();
+			String name = "<span>" + section.getObjectType().getName() + "</span>";
+			String icon = "";
+			if (this.iconPath != null) {
+				icon = "<img class='markupIcon' src='" + this.iconPath + "'></img> ";
+			}
+			string.append(KnowWEUtils.maskHTML("<div id=\"" + id
+					+ "\" class='defaultMarkup'>\n"));
+			// string.append(KnowWEUtils.maskHTML("<div class='markupHeader'>" +
+			// icon + name
+			// + "</div>\n"));
+			// render pre-formatted box
+
+			// add an anchor to enable direct link to the section
+			String anchorName = KnowWEUtils.getAnchor(section);
+			string.append(KnowWEUtils.maskHTML("<a name='" + anchorName + "'></a>"));
+
+			// render messages and content
+			renderContents(article, section, user, string);
+
+			// and close the box
+			string.append(KnowWEUtils.maskHTML("</div>\n"));
+		}
+
+		protected void renderContents(KnowWEArticle article, Section<? extends DefaultMarkupType> section, KnowWEUserContext user, StringBuilder string) {
+			List<Section<?>> subsecs = section.getChildren();
+			Section<?> first = subsecs.get(0);
+			Section<?> last = subsecs.get(subsecs.size() - 1);
+			// bloody hack to not render annotations...
+			boolean annotationPartStarted = false;
+			for (Section<?> subsec : subsecs) {
+				if (subsec == first && subsec.getObjectType() instanceof PlainText)
+					continue;
+				if (subsec == last && subsec.getObjectType() instanceof PlainText)
+					continue;
+				if (subsec.getOriginalText().trim().startsWith("@")) {
+					annotationPartStarted = true;
+				}
+				if (!(subsec.getObjectType() instanceof AnnotationType)
+						&& !annotationPartStarted) {
+					subsec.getObjectType().getRenderer().render(article, subsec, user,
+							string);
+				}
+			}
+		}
+
 	}
 }
