6e42ba60266e3caf7a17f3c57296958d1096573b
Albrecht Striffler <albrecht.striffler@denkbares.com>
2010-09-09 13:34:57 +0000

    Fixed package rendering with includes.

--- d3web-KnowWE/KnowWE-core-Plugins-jpf-plugin/src/main/java/de/d3web/we/kdom/packaging/PackageRenderUtils.java
+++ d3web-KnowWE/KnowWE-core-Plugins-jpf-plugin/src/main/java/de/d3web/we/kdom/packaging/PackageRenderUtils.java
@@ -11,35 +11,39 @@ public class PackageRenderUtils {
 	public static KnowWEArticle checkArticlesCompiling(KnowWEArticle article, Section<?> s, StringBuilder string) {
 		// check and handle, if the section is compiled in other articles
 		KnowWEArticle compilingArticle = article;
-		Set<String> compilingArticles = KnowWEEnvironment.getInstance().getPackageManager(
-				article.getWeb()).getArticlesReferingTo(s);
+		if (article.getTitle().equals(s.getTitle())) {
+			Set<String> compilingArticles = KnowWEEnvironment.getInstance().getPackageManager(
+					article.getWeb()).getArticlesReferingTo(s);
 
-		if (compilingArticles.size() > 1) {
-			string.append("Articles compiling this Section: " + compilingArticles + "\n\n");
-		}
-		else if (compilingArticles.size() == 1) {
-			String title = compilingArticles.iterator().next();
-			if (!title.equals(compilingArticle.getTitle())) {
-				string.append("Compiled and rendered for Article: " + title + "\n\n");
-				compilingArticle = KnowWEEnvironment.getInstance().getArticle(article.getWeb(),
-						title);
+			if (compilingArticles.size() > 1) {
+				string.append("Articles compiling this Section: " + compilingArticles + "\n\n");
 			}
-		}
-		else {
-			string.append("This Section is not compiled in any article!\n\n");
+			else if (compilingArticles.size() == 1) {
+				String title = compilingArticles.iterator().next();
+				if (!title.equals(compilingArticle.getTitle())) {
+					string.append("Compiled and rendered for Article: " + title + "\n\n");
+					compilingArticle = KnowWEEnvironment.getInstance().getArticle(article.getWeb(),
+							title);
+				}
+			}
+			else {
+				string.append("This Section is not compiled in any article!\n\n");
+			}
 		}
 		return compilingArticle;
 	}
 
 	public static KnowWEArticle checkArticlesCompiling(KnowWEArticle article, Section<?> s) {
 		KnowWEArticle compilingArticle = article;
-		Set<String> compilingArticles = KnowWEEnvironment.getInstance().getPackageManager(
-				article.getWeb()).getArticlesReferingTo(s);
-		if (compilingArticles.size() == 1) {
-			String title = compilingArticles.iterator().next();
-			if (!title.equals(compilingArticle.getTitle())) {
-				compilingArticle = KnowWEEnvironment.getInstance().getArticle(article.getWeb(),
-						title);
+		if (article.getTitle().equals(s.getTitle())) {
+			Set<String> compilingArticles = KnowWEEnvironment.getInstance().getPackageManager(
+					article.getWeb()).getArticlesReferingTo(s);
+			if (compilingArticles.size() == 1) {
+				String title = compilingArticles.iterator().next();
+				if (!title.equals(compilingArticle.getTitle())) {
+					compilingArticle = KnowWEEnvironment.getInstance().getArticle(article.getWeb(),
+							title);
+				}
 			}
 		}
 		return compilingArticle;
