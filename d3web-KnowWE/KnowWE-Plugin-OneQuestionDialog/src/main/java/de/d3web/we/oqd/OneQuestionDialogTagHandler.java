411b7cdfe9091187c4fc2f1f6e67e731cda6e20e
Reinhard Hatko <hatko@d3web.svn.invalid>
2010-09-10 12:05:30 +0000

    hotfix for placing OQDialog in LeftMenu

--- d3web-KnowWE/KnowWE-Plugin-OneQuestionDialog/src/main/java/de/d3web/we/oqd/OneQuestionDialogTagHandler.java
+++ d3web-KnowWE/KnowWE-Plugin-OneQuestionDialog/src/main/java/de/d3web/we/oqd/OneQuestionDialogTagHandler.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (C) 2010 University Wuerzburg, Computer Science VI
- * 
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -30,7 +30,7 @@ import de.d3web.we.taghandler.AbstractTagHandler;
 import de.d3web.we.wikiConnector.KnowWEUserContext;
 
 /**
- * 
+ *
  * @author Florian Ziegler
  * @created 16.08.2010
  */
@@ -50,6 +50,13 @@ public class OneQuestionDialogTagHandler extends AbstractTagHandler {
 		// D3webModule.getAD3webKnowledgeServiceInTopic(
 		// web, topic);
 		ResourceBundle rb = D3webModule.getKwikiBundle_d3web(user.getHttpRequest());
+
+		//if the OQDialog is in the LeftMenu, the topic is 'LeftMenu',
+		// but then no KB is found. Set to article.
+		if (topic.equalsIgnoreCase("LeftMenu")) { 
+			topic = user.getUrlParameterMap().get("page");
+		}
+		
 		D3webKnowledgeService knowledgeServiceInTopic = D3webModule.getAD3webKnowledgeServiceInTopic(
 				web, topic);
 		if (knowledgeServiceInTopic == null) return rb.getString("KnowWE.QuickInterview.error");
