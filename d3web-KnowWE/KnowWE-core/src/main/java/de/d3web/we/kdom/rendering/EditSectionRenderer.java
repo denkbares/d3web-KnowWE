66c08a81cebd909458eaad1529a918c2aa1739be
Albrecht Striffler <albrecht.striffler@denkbares.com>
2010-10-22 14:46:52 +0000

    EditSectionRenderer now also allows QuickEdit for Sections, that are rendered for other articles, than the article the Section is defined in.

--- d3web-KnowWE/KnowWE-core/src/main/java/de/d3web/we/kdom/rendering/EditSectionRenderer.java
+++ d3web-KnowWE/KnowWE-core/src/main/java/de/d3web/we/kdom/rendering/EditSectionRenderer.java
@@ -99,7 +99,8 @@ public class EditSectionRenderer extends KnowWEDomRenderer {
 		string.append(KnowWEUtils.maskHTML("<a name=\"" + sec.getID() + "\"></a>"));
 
 		string.append(KnowWEUtils.maskHTML("<div id=\"" + sec.getID() + "\" style=\"width: 100%\">"));
-		if (sec.getArticle().equals(article)) {
+		if (sec.getArticle().equals(article) || KnowWEEnvironment.getInstance().getPackageManager(
+				article.getWeb()).getArticlesReferingTo(sec).contains(article.getTitle())) {
 			string.append(KnowWEUtils.maskHTML(this.generateQuickEdit("Quickedit "
 					+ sec.getObjectType().getName() + " Section", sec.getID(), isEditable, user,
 					isInline)));
