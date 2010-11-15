09e19fa89cdebfe3d329d569ca9797a6f9189d24
Markus Friedrich <markus.friedrich@denkbares.com>
2010-11-15 18:09:25 +0000

    Fixed test in KnowWE-App
    Someday we should find something else than these hacks...

--- KnowWE/KnowWE-core/src/main/java/dummies/KnowWETestWikiConnector.java
+++ KnowWE/KnowWE-core/src/main/java/dummies/KnowWETestWikiConnector.java
@@ -1,17 +1,17 @@
 /*
  * Copyright (C) 2009 Chair of Artificial Intelligence and Applied Informatics
  * Computer Science VI, University of Wuerzburg
- *
+ * 
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- *
+ * 
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -40,26 +40,31 @@ import de.d3web.we.wikiConnector.KnowWEWikiConnector;
 /**
  * Used in the tests. A WikiConnector is needed to initialize the
  * KnowWEEnvironment. The methods are empty.
- *
+ * 
  * @author Johannes Dienst
- *
+ * 
  */
 public class KnowWETestWikiConnector implements KnowWEWikiConnector {
 
 	/**
 	 * This returns a path, that enables the use of this connector in tests of
 	 * projects.
-	 *
+	 * 
 	 * @author Sebastian Furth
 	 * @return relative Path to KnowWEExtensions
 	 */
 	public String getHackedPath() {
 		String hackedPath = System.getProperty("user.dir");
 		hackedPath = hackedPath.replaceAll("Research", "KnowWE");
-		hackedPath += "/../KnowWE-Resources/src/main/webapp/KnowWEExtension/";
+		if (hackedPath.contains("KnowWE-App")) {
+			hackedPath = hackedPath.replaceAll("KnowWE-App", "KnowWE");
+		}
+		else {
+			hackedPath += "/..";
+		}
+		hackedPath += "/KnowWE-Resources/src/main/webapp/KnowWEExtension/";
 		return hackedPath;
 	}
-
 
 	@Override
 	public String appendContentToPage(String topic, String pageContent) {
