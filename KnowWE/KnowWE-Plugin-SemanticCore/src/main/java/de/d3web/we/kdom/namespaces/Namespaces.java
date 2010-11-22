7aa0ecd0d3e59ceccd23e524d8508bc47e906dea
Sebastian Furth <sebastian.furth@denkbares.com>
2010-11-22 16:11:22 +0000

    Namespaces uses DefaultMarkup instead of XML

--- KnowWE/KnowWE-Plugin-SemanticCore/src/main/java/de/d3web/we/kdom/namespaces/Namespaces.java
+++ KnowWE/KnowWE-Plugin-SemanticCore/src/main/java/de/d3web/we/kdom/namespaces/Namespaces.java
@@ -1,17 +1,17 @@
 /*
- * Copyright (C) 2009 Chair of Artificial Intelligence and Applied Informatics
- * Computer Science VI, University of Wuerzburg
- * 
+ * < * Copyright (C) 2009 Chair of Artificial Intelligence and Applied
+ * Informatics Computer Science VI, University of Wuerzburg
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -19,25 +19,28 @@
  */
 package de.d3web.we.kdom.namespaces;
 
-import de.d3web.we.kdom.xml.AbstractXMLObjectType;
+import de.d3web.we.kdom.defaultMarkup.DefaultMarkup;
+import de.d3web.we.kdom.defaultMarkup.DefaultMarkupRenderer;
+import de.d3web.we.kdom.defaultMarkup.DefaultMarkupType;
+import de.d3web.we.kdom.rendering.KnowWEDomRenderer;
 
-/**
- * Created by IntelliJ IDEA. User: s179455 Date: Nov 10, 2009 Time: 3:14:28 PM
- * To change this template use File | Settings | File Templates.
- */
-public class Namespaces extends AbstractXMLObjectType {
+public class Namespaces extends DefaultMarkupType {
+
+	private static DefaultMarkup m = null;
+
+	static {
+		m = new DefaultMarkup("Namespaces");
+		m.addContentType(new NamespacesContent());
+	}
 
 	public Namespaces() {
-		super("namespaces");
+		super(m);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see de.d3web.we.dom.AbstractKnowWEObjectType#init()
-	 */
 	@Override
-	protected void init() {
-		childrenTypes.add(new NamespacesContent());
+	protected KnowWEDomRenderer<?> getDefaultRenderer() {
+		// "This Section is not compiled in any article" won't be displayed
+		return new DefaultMarkupRenderer<DefaultMarkupType>(false);
 	}
+
 }
