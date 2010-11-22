a2193d973608b95060ee61596c72520911ad50d0
Sebastian Furth <sebastian.furth@denkbares.com>
2010-11-22 14:53:40 +0000

    * organized imports

--- KnowWE/KnowWE-Plugin-d3web-MarkupSet/src/main/java/de/d3web/we/kdom/rules/action/SolutionValueAssignment.java
+++ KnowWE/KnowWE-Plugin-d3web-MarkupSet/src/main/java/de/d3web/we/kdom/rules/action/SolutionValueAssignment.java
@@ -1,17 +1,17 @@
 /*
  * Copyright (C) 2009 Chair of Artificial Intelligence and Applied Informatics
  * Computer Science VI, University of Wuerzburg
- * 
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -36,13 +36,13 @@ import de.d3web.we.kdom.sectionFinder.AllTextFinderTrimmed;
 import de.d3web.we.kdom.sectionFinder.OneOfStringEnumUnquotedFinder;
 import de.d3web.we.kdom.sectionFinder.SectionFinder;
 import de.d3web.we.kdom.sectionFinder.SectionFinderResult;
-import de.d3web.we.kdom.util.SplitUtility;
 import de.d3web.we.object.SolutionReference;
 import de.d3web.we.utils.D3webUtils;
+import de.d3web.we.utils.SplitUtility;
 
 /**
  * @author Johannes Dienst
- * 
+ *
  */
 public class SolutionValueAssignment extends D3webRuleAction<SolutionValueAssignment> {
 
