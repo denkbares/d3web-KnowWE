d9849847eeb7acd57c53a592198fe84763230157
Sebastian Furth <sebastian.furth@denkbares.com>
2010-11-22 13:27:22 +0000

    * markusinated dependency to KnowWE-Plugin-d3web (KnOfficeParser)

--- KnowWE/KnowWE-Plugin-TestSuite/src/main/java/de/d3web/we/testsuite/kdom/Finding.java
+++ KnowWE/KnowWE-Plugin-TestSuite/src/main/java/de/d3web/we/testsuite/kdom/Finding.java
@@ -1,17 +1,17 @@
 /*
  * Copyright (C) 2009 Chair of Artificial Intelligence and Applied Informatics
  * Computer Science VI, University of Wuerzburg
- * 
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -26,25 +26,26 @@ import java.util.List;
 import de.d3web.we.kdom.DefaultAbstractKnowWEObjectType;
 import de.d3web.we.kdom.KnowWEObjectType;
 import de.d3web.we.kdom.Section;
-import de.d3web.we.kdom.Annotation.FindingAnswer;
-import de.d3web.we.kdom.Annotation.FindingQuestion;
+import de.d3web.we.kdom.condition.AnswerReferenceImpl;
+import de.d3web.we.kdom.renderer.FontColorRenderer;
+import de.d3web.we.kdom.renderer.ObjectInfoLinkRenderer;
 import de.d3web.we.kdom.sectionFinder.ISectionFinder;
 import de.d3web.we.kdom.sectionFinder.SectionFinderResult;
-import de.d3web.we.utils.SplitUtility;
-import de.knowwe.core.renderer.FontColorRenderer;
-import de.knowwe.core.renderer.ObjectInfoLinkRenderer;
+import de.d3web.we.kdom.util.SplitUtility;
+import de.d3web.we.object.AnswerReference;
+import de.d3web.we.object.QuestionReference;
 
 public class Finding extends DefaultAbstractKnowWEObjectType {
 
 	@Override
 	public void init() {
 
-		FindingQuestion question = new FindingQuestion();
+		QuestionReference question = new QuestionReference();
 		question.setSectionFinder(new QuestionSectionFinder());
 		question.setCustomRenderer(new ObjectInfoLinkRenderer(
 				FontColorRenderer.getRenderer(FontColorRenderer.COLOR6)));
 
-		FindingAnswer answer = new FindingAnswer();
+		AnswerReference answer = new AnswerReferenceImpl();
 		answer.setSectionFinder(new AnswerSectionFinder());
 		answer.setCustomRenderer(new ObjectInfoLinkRenderer(
 				FontColorRenderer.getRenderer(FontColorRenderer.COLOR5)));
--- KnowWE/KnowWE-Plugin-TestSuite/src/main/java/de/d3web/we/testsuite/kdom/TestSuiteType.java
+++ KnowWE/KnowWE-Plugin-TestSuite/src/main/java/de/d3web/we/testsuite/kdom/TestSuiteType.java
@@ -18,19 +18,18 @@
  */
 package de.d3web.we.testsuite.kdom;
 
-import java.io.Reader;
-import java.io.StringReader;
 import java.util.Collection;
-import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
-import de.d3web.KnOfficeParser.SingleKBMIDObjectManager;
-import de.d3web.core.manage.IDObjectManagement;
+import de.d3web.core.knowledge.terminology.Question;
+import de.d3web.core.knowledge.terminology.QuestionNum;
+import de.d3web.core.knowledge.terminology.Solution;
 import de.d3web.core.manage.KnowledgeBaseManagement;
-import de.d3web.report.Message;
-import de.d3web.report.Report;
+import de.d3web.core.session.Value;
+import de.d3web.core.session.values.NumValue;
+import de.d3web.empiricaltesting.TestSuite;
 import de.d3web.we.core.KnowWEEnvironment;
-import de.d3web.we.core.KnowWEParseResult;
 import de.d3web.we.core.packaging.KnowWEPackageManager;
 import de.d3web.we.kdom.KnowWEArticle;
 import de.d3web.we.kdom.Priority;
@@ -38,14 +37,19 @@ import de.d3web.we.kdom.Section;
 import de.d3web.we.kdom.defaultMarkup.DefaultMarkup;
 import de.d3web.we.kdom.defaultMarkup.DefaultMarkupType;
 import de.d3web.we.kdom.report.KDOMReportMessage;
+import de.d3web.we.kdom.report.SimpleMessageError;
+import de.d3web.we.kdom.report.message.NoSuchObjectError;
+import de.d3web.we.kdom.report.message.ObjectCreatedMessage;
+import de.d3web.we.object.AnswerReference;
+import de.d3web.we.object.QuestionReference;
+import de.d3web.we.object.SolutionReference;
 import de.d3web.we.reviseHandler.D3webSubtreeHandler;
-import de.d3web.we.testsuite.TestsuiteBuilder;
 import de.d3web.we.testsuite.renderer.TestSuiteRenderer;
 import de.d3web.we.utils.KnowWEUtils;
 
 /**
  * TestSuiteType for defining test suites in wiki markup.
- * 
+ *
  * @author Sebastian Furth (denkbares GmbH)
  * @created 18/10/2010
  */
@@ -72,12 +76,13 @@ public class TestSuiteType extends DefaultMarkupType {
 
 		@Override
 		public boolean isIgnoringPackageCompile() {
-			return true;
+			return false;
 		}
 
 		@Override
 		public Collection<KDOMReportMessage> create(KnowWEArticle article, Section<TestSuiteType> s) {
 
+			List<KDOMReportMessage> messages = new LinkedList<KDOMReportMessage>();
 			KnowledgeBaseManagement kbm = loadKBM(article, s);
 
 			if (kbm != null) {
@@ -86,37 +91,205 @@ public class TestSuiteType extends DefaultMarkupType {
 
 				if (content != null) {
 
-					Reader r = new StringReader(content.getOriginalText());
-					IDObjectManagement idom = new SingleKBMIDObjectManager(kbm);
-					TestsuiteBuilder builder = new TestsuiteBuilder("", idom);
+					List<de.d3web.empiricaltesting.SequentialTestCase> repository = new LinkedList<de.d3web.empiricaltesting.SequentialTestCase>();
 
-					// Parsing
-					List<de.d3web.report.Message> messages = builder.addKnowledge(r, idom, null);
+					// Get all SequentialTestCase sections
+					List<Section<SequentialTestCase>> stcSections = new LinkedList<Section<SequentialTestCase>>();
+					content.findSuccessorsOfType(SequentialTestCase.class, stcSections);
 
-					// Reporting
-					storeMessages(article, s, this.getClass(), messages);
-					Report testsuiteRep = new Report();
+					// Process each SequentialTestCase section
+					for (Section<SequentialTestCase> stcSection : stcSections) {
 
-					for (Message messageKnOffice : messages) {
-						testsuiteRep.add(messageKnOffice);
+						int index = stcSections.indexOf(stcSection) + 1;
+
+						// Create new STC
+						de.d3web.empiricaltesting.SequentialTestCase stc = new de.d3web.empiricaltesting.SequentialTestCase();
+						createSTCName(stcSection, index, stc, messages);
+						createRTCs(stcSection, index, stc, kbm, messages);
+
+						// Add STC to Repository
+						repository.add(stc);
+
 					}
 
-					KnowWEParseResult result =
-							new KnowWEParseResult(testsuiteRep, s.getTitle(), s.getOriginalText());
-
-					s.getArticle().getReport().addReport(result);
+					// Create the test suite
+					TestSuite testSuite = new TestSuite();
+					testSuite.setKb(kbm.getKnowledgeBase());
+					testSuite.setRepository(repository);
 
 					// Store test suite
-					KnowWEUtils.storeObject(s.getArticle().getWeb(), s.getTitle(), s.getID(),
-							TestSuiteType.TESTSUITEKEY, builder.getTestsuite());
+					if (messages.size() == 0) {
+						KnowWEUtils.storeObject(s.getArticle().getWeb(), s.getTitle(), s.getID(),
+								TestSuiteType.TESTSUITEKEY, testSuite);
+						messages.add(new ObjectCreatedMessage(
+								"Test Suite successfully created with "
+										+ testSuite.getRepository().size() + " cases."));
+					}
+
 				}
 			}
 
-			return Collections.emptyList();
+			return messages;
+		}
+
+		private void createSTCName(Section<SequentialTestCase> stcSection, int index, de.d3web.empiricaltesting.SequentialTestCase stc, List<KDOMReportMessage> messages) {
+			Section<SequentialTestCaseName> stcName = stcSection.findSuccessor(SequentialTestCaseName.class);
+			if (stcName == null) {
+				messages.add(new SimpleMessageError("There is no name for STC" + index));
+			}
+			else {
+				stc.setName(clean(stcName.getOriginalText().trim()));
+			}
+		}
+
+		private void createRTCs(Section<SequentialTestCase> stcSection, int stcIndex, de.d3web.empiricaltesting.SequentialTestCase stc, KnowledgeBaseManagement kbm, List<KDOMReportMessage> messages) {
+
+			// Get all RatedTestCase sections
+			List<Section<RatedTestCase>> rtcSections = new LinkedList<Section<RatedTestCase>>();
+			stcSection.findSuccessorsOfType(RatedTestCase.class, rtcSections);
+
+			// Process each RatedTestCase section
+			for (Section<RatedTestCase> rtcSection : rtcSections) {
+
+				int rtcIndex = rtcSections.indexOf(rtcSection);
+				de.d3web.empiricaltesting.RatedTestCase rtc = new de.d3web.empiricaltesting.RatedTestCase();
+				createFindings(rtcSection, stcIndex, rtcIndex, rtc, kbm, messages);
+				createRatedSolutions(rtcSection, stcIndex, rtcIndex, rtc, kbm, messages);
+				stc.add(rtc);
+			}
+
+		}
+
+		private void createFindings(Section<RatedTestCase> rtcSection, int stcIndex, int rtcIndex, de.d3web.empiricaltesting.RatedTestCase rtc, KnowledgeBaseManagement kbm, List<KDOMReportMessage> messages) {
+
+			// Get all Finding sections
+			List<Section<Finding>> findingSections = new LinkedList<Section<Finding>>();
+			rtcSection.findSuccessorsOfType(Finding.class, findingSections);
+
+			// Process each Finding section
+			for (Section<Finding> findingSection : findingSections) {
+
+				// Get the QuestionReference section
+				Section<QuestionReference> questionSection = findingSection.findSuccessor(QuestionReference.class);
+
+				// Get the real Question
+				if (questionSection != null) {
+					String questionText = clean(questionSection.getOriginalText());
+					Question question = kbm.findQuestion(questionText);
+
+					// Create error message if there is no question with this name in the KB
+					if (question == null) {
+						messages.add(new NoSuchObjectError(
+								clean(questionSection.getOriginalText().trim())));
+					}
+					else {
+
+						// Get the value
+						Section<AnswerReference> valueSection = findingSection.findSuccessor(AnswerReference.class);
+
+						// Create error message if there is no value defined
+						if (valueSection == null) {
+							messages.add(new SimpleMessageError(
+									"There is no Value defined for Question \""
+											+ clean(questionSection.getOriginalText().trim())
+											+ "\""));
+						}
+
+						// Check if the question is a QuestionNum
+						if (question instanceof QuestionNum) {
+
+							try {
+								double value = Double.parseDouble(clean(valueSection.getOriginalText().trim()));
+								rtc.add(new de.d3web.empiricaltesting.Finding(question,
+										new NumValue(value)));
+							}
+							catch (NumberFormatException e) {
+								messages.add(new SimpleMessageError("The Value for Question \""
+										+ clean(questionSection.getOriginalText().trim())
+										+ "\" has to be a number."));
+							}
+						}
+						// If not, it is a QuestionChoice
+						else {
+							Value value = kbm.findValue(question,
+									clean(valueSection.getOriginalText().trim()));
+							if (value == null) {
+								messages.add(new NoSuchObjectError(
+										clean(valueSection.getOriginalText().trim())));
+							}
+							else {
+								rtc.add(new de.d3web.empiricaltesting.Finding(question, value));
+							}
+						}
+					}
+				}
+				else {
+					messages.add(new SimpleMessageError(
+							"There is no Question defined in Finding "
+									+ findingSections.indexOf(findingSection) +
+									" in RTC " + rtcIndex + " in STC " + stcIndex));
+				}
+			}
+		}
+
+		private void createRatedSolutions(Section<RatedTestCase> rtcSection, int stcIndex, int rtcIndex, de.d3web.empiricaltesting.RatedTestCase rtc, KnowledgeBaseManagement kbm, List<KDOMReportMessage> messages) {
+
+			// Get all RatedSolution sections
+			List<Section<RatedSolution>> ratedSolutionSections = new LinkedList<Section<RatedSolution>>();
+			rtcSection.findSuccessorsOfType(RatedSolution.class, ratedSolutionSections);
+
+			// Process each RatedSolution section
+			for (Section<RatedSolution> ratedSolutionSection : ratedSolutionSections) {
+
+				Solution solution = null;
+
+				// Get the SolutionReference section
+				Section<SolutionReference> solutionSection = ratedSolutionSection.findSuccessor(SolutionReference.class);
+
+				// Get the real Solution
+				if (solutionSection != null) {
+					solution = kbm.findSolution(clean(solutionSection.getOriginalText().trim()));
+
+					// Create error message if there is no question with this
+					// name in the KB
+					if (solution == null) {
+						messages.add(new NoSuchObjectError(
+								clean(solutionSection.getOriginalText().trim())));
+					}
+				}
+				else {
+					messages.add(new SimpleMessageError(
+							"There is no Solution defined in RatedSolution "
+									+ ratedSolutionSections.indexOf(ratedSolutionSection) +
+									" in RTC " + rtcIndex + " in STC " + stcIndex));
+				}
+
+				// Get the StateRating section
+				Section<StateRating> stateSection = ratedSolutionSection.findSuccessor(StateRating.class);
+
+				// Create a real StateRating
+				if (stateSection != null) {
+					if (solution != null) {
+						de.d3web.empiricaltesting.StateRating rating = new de.d3web.empiricaltesting.StateRating(
+								clean(stateSection.getOriginalText().trim()));
+
+						// And finally create the RatedSolution
+						rtc.addExpected(new de.d3web.empiricaltesting.RatedSolution(solution,
+								rating));
+					}
+
+				}
+				else {
+					messages.add(new SimpleMessageError(
+							"There is no Rating defined for Solution \""
+									+ solution + "\" in RTC " + rtcIndex + " in STC " + stcIndex));
+				}
+			}
+
 		}
 
 		/**
-		 * Because the knowledge base is in another article we need this method.
+		 * Due to the knowledge base is in another article we need this method.
 		 */
 		private KnowledgeBaseManagement loadKBM(KnowWEArticle a, Section<TestSuiteType> s) {
 			// We need this for the JUNIT Tests!
@@ -132,6 +305,13 @@ public class TestSuiteType extends DefaultMarkupType {
 			}
 
 			return kbm;
+		}
+
+		private String clean(String quotedString) {
+			if (quotedString.startsWith("\"") && quotedString.endsWith("\"")) {
+				return quotedString.substring(1, quotedString.length() - 1).trim();
+			}
+			return quotedString.trim();
 		}
 
 	}
