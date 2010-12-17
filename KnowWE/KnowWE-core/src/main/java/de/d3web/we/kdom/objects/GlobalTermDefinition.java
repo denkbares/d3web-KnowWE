0f81a46082bcefc10760fed91e4992ab255873ef
Jochen Reutelshöfer <jochen.reutelshoefer@denkbares.com>
2010-12-17 17:13:52 +0000

    added GlobalTermDefinition and GlobalTermReference

---
+++ KnowWE/KnowWE-core/src/main/java/de/d3web/we/kdom/objects/GlobalTermDefinition.java
@@ -0,0 +1,19 @@
+package de.d3web.we.kdom.objects;
+
+/**
+ * 
+ * An abstract class for TermDefinitions that are always acting in the global
+ * scope.
+ * 
+ * @author Jochen
+ * @created 17.12.2010
+ * @param <TermObject>
+ */
+public abstract class GlobalTermDefinition<TermObject> extends TermDefinition<TermObject> {
+
+	public GlobalTermDefinition(Class<TermObject> termObjectClass) {
+		super(termObjectClass);
+		this.setTermScope(KnowWETerm.GLOBAL);
+	}
+
+}
---
+++ KnowWE/KnowWE-core/src/main/java/de/d3web/we/kdom/objects/GlobalTermReference.java
@@ -0,0 +1,20 @@
+package de.d3web.we.kdom.objects;
+
+/**
+ * 
+ * An abstract class for TermReferences that are always acting in the global
+ * scope.
+ * 
+ * @author Jochen
+ * @created 17.12.2010
+ * @param <TermObject>
+ */
+
+public abstract class GlobalTermReference<TermObject> extends TermReference<TermObject> {
+
+	public GlobalTermReference(Class<TermObject> termObjectClass) {
+		super(termObjectClass);
+		this.setTermScope(KnowWETerm.GLOBAL);
+	}
+	
+}
