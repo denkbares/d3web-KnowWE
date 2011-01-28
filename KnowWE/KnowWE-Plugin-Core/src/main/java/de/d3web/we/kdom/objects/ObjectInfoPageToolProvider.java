da4ee0177ba646b2aa92daeabc877ad471cac749
Sebastian Furth <sebastian.furth@denkbares.com>
2011-01-28 13:04:39 +0000

    * added escape(..) to enable object names with  special chars like # in ObjectInfoTagHandler

--- KnowWE/KnowWE-Plugin-Core/src/main/java/de/d3web/we/kdom/objects/ObjectInfoPageToolProvider.java
+++ KnowWE/KnowWE-Plugin-Core/src/main/java/de/d3web/we/kdom/objects/ObjectInfoPageToolProvider.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (C) 2010 denkbares GmbH, Germany
- * 
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -29,7 +29,7 @@ import de.d3web.we.utils.KnowWEUtils;
 import de.d3web.we.wikiConnector.KnowWEUserContext;
 
 /**
- * 
+ *
  * @author volker_belli
  * @created 01.12.2010
  */
@@ -43,9 +43,9 @@ public class ObjectInfoPageToolProvider implements ToolProvider {
 
 	protected Tool getObjectInfoPageTool(KnowWEArticle article, Section<?> section, KnowWEUserContext userContext) {
 		String objectName = KnowWEUtils.trimQuotes(section.getOriginalText());
-		String jsAction = "window.location = " +
-				"'Wiki.jsp?page=ObjectInfoPage&objectname=" +
-				URLEncoder.encode(objectName) + "'";
+		String jsAction = "window.location.href = " +
+				"'Wiki.jsp?page=ObjectInfoPage&objectname=' + escape('" +
+				URLEncoder.encode(objectName) + "')";
 		return new DefaultTool(
 				"KnowWEExtension/d3web/icon/infoPage16.png",
 				"Show Info Page",
