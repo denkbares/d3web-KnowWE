4ed2327f07f079c5baa6041e2d165110163a3ce6
Sebastian Furth <sebastian.furth@denkbares.com>
2011-02-11 16:16:01 +0000

    * applied Singleton-Pattern to all subclasses of SectionFinderConstraint

--- KnowWE/KnowWE-KDOMParseUtils/src/main/java/de/d3web/we/kdom/constraint/AtMostOneFindingConstraint.java
+++ KnowWE/KnowWE-KDOMParseUtils/src/main/java/de/d3web/we/kdom/constraint/AtMostOneFindingConstraint.java
@@ -1,17 +1,17 @@
 /*
  * Copyright (C) 2010 Chair of Artificial Intelligence and Applied Informatics
  * Computer Science VI, University of Wuerzburg
- * 
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -20,7 +20,6 @@
 
 package de.d3web.we.kdom.constraint;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import de.d3web.we.kdom.KnowWEObjectType;
@@ -29,19 +28,18 @@ import de.d3web.we.kdom.sectionFinder.SectionFinderResult;
 
 public class AtMostOneFindingConstraint implements SectionFinderConstraint {
 
-	private static AtMostOneFindingConstraint instance = null;
+	private static AtMostOneFindingConstraint instance = new AtMostOneFindingConstraint();
 
 	public static AtMostOneFindingConstraint getInstance() {
-		if (instance == null) {
-			instance = new AtMostOneFindingConstraint();
-		}
 		return instance;
+	}
+
+	private AtMostOneFindingConstraint() {
 	}
 
 	@Override
 	public void filterCorrectResults(
 			List<SectionFinderResult> found, Section father, KnowWEObjectType type) {
-		List<SectionFinderResult> result = new ArrayList<SectionFinderResult>();
 		if (found == null || found.size() == 0) return;
 
 		SectionFinderResult firstResult = found.get(0);
