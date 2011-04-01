7de89429c32c7e4dad0dcde43b4afbc9a1c0dea0
Markus Friedrich <markus.friedrich@denkbares.com>
2011-04-01 12:13:03 +0000

    Adaptions due to movement of EmpiricalTesterComponents

--- KnowWE/KnowWE-Plugin-TestCase/src/main/java/de/d3web/we/testcase/action/TestCaseServlet.java
+++ KnowWE/KnowWE-Plugin-TestCase/src/main/java/de/d3web/we/testcase/action/TestCaseServlet.java
@@ -1,17 +1,17 @@
 /*
  * Copyright (C) 2010 Chair of Artificial Intelligence and Applied Informatics
  * Computer Science VI, University of Wuerzburg
- *
+ * 
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- *
+ * 
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -24,7 +24,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 import de.d3web.empiricaltesting.TestCase;
-import de.d3web.empiricaltesting.casevisualization.dot.DDBuilder;
 import de.d3web.empiricaltesting.writer.TestSuiteKnOfficeWriter;
 import de.d3web.empiricaltesting.writer.TestSuiteXMLWriter;
 import de.d3web.we.action.AbstractAction;
@@ -42,16 +41,14 @@ public class TestCaseServlet extends AbstractAction {
 		String type = context.getParameter("type");
 
 		// Load the TestSuite
-		//TestSuite t = (TestSuite) KnowWEUtils.getStoredObject(web, topic, nodeID,
+		// TestSuite t = (TestSuite) KnowWEUtils.getStoredObject(web, topic,
+		// nodeID,
 		// TestSuiteType.TESTSUITEKEY);
 
 		TestCase t = TestCaseUtils.loadTestSuite(topic, web);
 
 		if (t != null) {
-			if (type.equals("visualization")) {
-				generateVisualization(context, t, filename);
-			}
-			else if (type.equals("case")) {
+			if (type.equals("case")) {
 				generateCaseFile(context, t, filename);
 			}
 		}
@@ -59,43 +56,6 @@ public class TestCaseServlet extends AbstractAction {
 			context.getWriter().write(
 					"Error: There is no test case to download. Probably exist errors in your test case.");
 		}
-	}
-
-	private void generateVisualization(UserActionContext context, TestCase t, String filename) throws IOException {
-
-		if (filename.endsWith(".dot")) {
-
-			// Get the file content
-			ByteArrayOutputStream bstream = DDBuilder.getInstance().getByteArrayOutputStream(
-					t.getRepository());
-
-			// Response
-			context.setContentType("text/x-graphviz");
-			context.setHeader("Content-Disposition", "attachment;filename=\"" + filename + "\"");
-			context.setContentLength(bstream.size());
-
-			// Write the data from the ByteArray to the ServletOutputStream of
-			// the response
-			bstream.writeTo(context.getOutputStream());
-
-		}
-		else if (filename.endsWith(".pdf")) {
-
-			// Get the file content
-			ByteArrayOutputStream bstream = de.d3web.empiricaltesting.casevisualization.jung.JUNGCaseVisualizer.getInstance().getByteArrayOutputStream(
-					t.getRepository());
-
-			// Response
-			context.setContentType("application/pdf");
-			context.setHeader("Content-Disposition", "attachment;filename=\"" + filename + "\"");
-			context.setContentLength(bstream.size());
-
-			// Write the data from the ByteArray to the ServletOutputStream of
-			// the response
-			bstream.writeTo(context.getOutputStream());
-
-		}
-
 	}
 
 	private void generateCaseFile(UserActionContext context, TestCase t, String filename) throws IOException {
--- KnowWE/KnowWE-Plugin-TestCase/src/main/java/de/d3web/we/testcase/toolprovider/TestCaseRunnerToolProvider.java
+++ KnowWE/KnowWE-Plugin-TestCase/src/main/java/de/d3web/we/testcase/toolprovider/TestCaseRunnerToolProvider.java
@@ -1,26 +1,25 @@
 /*
  * Copyright (C) 2010 University Wuerzburg, Computer Science VI
- *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 3 of
- * the License, or (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * 
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
  */
 package de.d3web.we.testcase.toolprovider;
 
 import de.d3web.we.kdom.KnowWEArticle;
 import de.d3web.we.kdom.Section;
-import de.d3web.we.testcase.kdom.TestCaseRunnerType;
 import de.d3web.we.tools.DefaultTool;
 import de.d3web.we.tools.Tool;
 import de.d3web.we.tools.ToolProvider;
@@ -36,13 +35,8 @@ public class TestCaseRunnerToolProvider implements ToolProvider {
 
 	@Override
 	public Tool[] getTools(KnowWEArticle article, Section<?> section, UserContext userContext) {
-		String topic = TestCaseRunnerType.getTestCase(section);
-		Tool downloadPDF = getDownloadPDFTool(topic, article.getWeb(), section.getID());
-		Tool downloadGraphViz = getDownloadGraphVizTool(topic, article.getWeb(),
-				section.getID());
 		Tool refresh = getRefreshTool();
-		return new Tool[] {
-				downloadPDF, downloadGraphViz, refresh };
+		return new Tool[] { refresh };
 	}
 
 	protected Tool getRefreshTool() {
@@ -52,30 +46,6 @@ public class TestCaseRunnerToolProvider implements ToolProvider {
 				"KnowWEExtension/d3web/icon/refresh16.png",
 				"Re-Run",
 				"Re-Runs the test case.",
-				jsAction);
-	}
-
-	protected Tool getDownloadPDFTool(String topic, String web, String id) {
-		// tool to provide download capability
-		String jsAction = "window.location='action/TestCaseServlet?type=visualization&KWiki_Topic="
-				+ topic + "&web=" + web + "&filename=" + topic.replaceAll(" ", "_")
-				+ "_Visualization.pdf'";
-		return new DefaultTool(
-				"KnowWEExtension/images/pdf.png",
-				"Download PDF",
-				"Download the test case visualized in a pdf file.",
-				jsAction);
-	}
-
-	protected Tool getDownloadGraphVizTool(String topic, String web, String id) {
-		// tool to provide download capability
-		String jsAction = "window.location='action/TestCaseServlet?type=visualization&KWiki_Topic="
-				+ topic + "&web=" + web + "&filename=" + topic.replaceAll(" ", "_")
-				+ "_Visualization.dot'";
-		return new DefaultTool(
-				"KnowWEExtension/images/txt.png",
-				"Download GraphViz",
-				"Download the test case visualized in a GraphViz (.dot) file.",
 				jsAction);
 	}
 
