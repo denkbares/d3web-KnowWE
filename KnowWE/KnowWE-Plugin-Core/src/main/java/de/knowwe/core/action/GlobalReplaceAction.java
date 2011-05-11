dc17fed5d843feff97de0a65f66e0268931d7e11
Christian Grotheer <grotheer@d3web.svn.invalid>
2011-05-11 11:03:41 +0000

    fixed renaming-tool

--- KnowWE/KnowWE-Plugin-Core/src/main/java/de/knowwe/core/action/GlobalReplaceAction.java
+++ KnowWE/KnowWE-Plugin-Core/src/main/java/de/knowwe/core/action/GlobalReplaceAction.java
@@ -1,17 +1,17 @@
 /*
  * Copyright (C) 2009 Chair of Artificial Intelligence and Applied Informatics
  * Computer Science VI, University of Wuerzburg
- *
+ * 
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- *
+ * 
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -54,7 +54,7 @@ public class GlobalReplaceAction extends AbstractAction {
 
 		// replaceFindings auspacken
 		String replacements = context.getParameter(KnowWEAttributes.TEXT);
-		if (replacements == null) return rb.getString("KnowWE.renamingtool.noreplacement");
+		if (replacements == null) return rb.getString("KnowWE.renamingtool.noreplacements");
 		String[] replacementArray = replacements.split("__");
 
 		Map<Section, List<WordBasedRenameFinding>> findingsPerSection = new HashMap<Section, List<WordBasedRenameFinding>>();
@@ -128,7 +128,7 @@ public class GlobalReplaceAction extends AbstractAction {
 			buff.append(sec.getOriginalText().substring(lastEnd, sec.getOriginalText().length()));
 			// section text overridden => KDOM dirty
 			sec.setOriginalText(buff.toString());
-			sec.getChildren().clear();
+			sec.removeAllChildren();
 		}
 
 		// Artikel im JSPWiki speichern
