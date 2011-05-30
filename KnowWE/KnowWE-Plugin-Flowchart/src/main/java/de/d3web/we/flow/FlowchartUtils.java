db8c19a2b11cb83a0491a85e287b77f7b8594b63
Reinhard Hatko <hatko@d3web.svn.invalid>
2011-05-30 20:58:44 +0000

    * improvements on in-article rendering * added extension point for rendering extensions

--- KnowWE/KnowWE-Plugin-Flowchart/src/main/java/de/d3web/we/flow/FlowchartUtils.java
+++ KnowWE/KnowWE-Plugin-Flowchart/src/main/java/de/d3web/we/flow/FlowchartUtils.java
@@ -19,19 +19,16 @@
  */
 package de.d3web.we.flow;
 
-import java.util.LinkedList;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import de.d3web.we.core.KnowWEEnvironment;
+import de.d3web.plugin.Extension;
+import de.d3web.plugin.JPFPluginManager;
 import de.d3web.we.flow.kbinfo.JSPHelper;
-import de.d3web.we.flow.kbinfo.SearchInfoObjects;
 import de.d3web.we.flow.type.FlowchartPreviewContentType;
 import de.d3web.we.flow.type.FlowchartType;
 import de.d3web.we.kdom.Section;
 import de.d3web.we.kdom.Sections;
-import de.d3web.we.kdom.objects.TermReference;
 import de.d3web.we.kdom.xml.AbstractXMLType;
 import de.d3web.we.user.UserContext;
 import de.d3web.we.utils.KnowWEUtils;
@@ -45,6 +42,15 @@ public class FlowchartUtils {
 
 	public static final String PREVIEW_REGEX = "\\s*<!\\[CDATA\\[\\s*(.*)\\s*\\]\\]>\\s*";
 	public static final Pattern PREVIEW_PATTERN = Pattern.compile(PREVIEW_REGEX);
+	public static final String[] JS = new String[] {
+			"cc/flow/builder.js", "cc/kbinfo/kbinfo.js", "cc/flow/renderExtensions.js",
+			"cc/kbinfo/extensions.js", "cc/flow/flowchart.js",
+			"cc/flow/action.js", "cc/flow/guard.js", "cc/flow/node.js", "cc/flow/rule.js",
+			"cc/flow/router.js" };
+
+	private static final String[] CSS = new String[] {
+			"cc/flow/flowchart.css", "cc/flow/guard.css", "cc/flow/node.css",
+			"cc/flow/rule.css", "cc/flow/rendering.css" };
 
 	private FlowchartUtils() {
 	}
@@ -99,13 +105,13 @@ public class FlowchartUtils {
 	 */
 	public static String createRenderablePreview(Section<FlowchartType> flowSection, UserContext user) {
 		//return createFlowchartRenderer(flowSection, user);
-		String preview = extractPreview(flowSection);
+		 String preview = extractPreview(flowSection);
 		
-		if (preview == null) {
-			return null;
-		}
+		 if (preview == null) {
+			 return null;
+		 }
 		
-		return createRenderablePreview(preview);
+		 return createRenderablePreview(preview);
 
 	}
 
@@ -118,26 +124,7 @@ public class FlowchartUtils {
 		String width = AbstractXMLType.getAttributeMapFor(section).get("width");
 		String height = AbstractXMLType.getAttributeMapFor(section).get("height");
 
-		KnowWEEnvironment knowWEEnv = KnowWEEnvironment.getInstance();
-
-		List<Section<TermReference>> found = new LinkedList<Section<TermReference>>();
-		Sections.findSuccessorsOfType(section, TermReference.class, found);
-
-		StringBuilder builder = new StringBuilder();
-		for (Section<TermReference> term : found) {
-			if (builder.length() == 0) {
-				builder.append(term.getText());
-			}
-			else {
-				builder.append(",").append(term.getText());
-			}
-		}
-
-		List<String> searchObjects = SearchInfoObjects.searchObjects(knowWEEnv, web,
-				builder.toString(), null, 400);
-
 		if (user.getWeb() == null) return "";
-
 
 		String sourceID = name + "Source";
 		String initScript = "<script>" +
@@ -145,40 +132,52 @@ public class FlowchartUtils {
 				"Flowchart.createFromXML('" + name + "', $('" + sourceID
 				+ "')).setVisible(true);\n" +
 						"</script>\n";
-		String result = "\n<div>"
-				+
-				"<link rel='stylesheet' type='text/css' href='cc/flow/flowchart.css'></link>"
-				+
-				"<link rel='stylesheet' type='text/css' href='cc/flow/guard.css'></link>"
-				+
-				"<link rel='stylesheet' type='text/css' href='cc/flow/node.css'></link>"
-				+
-				"<link rel='stylesheet' type='text/css' href='cc/flow/rule.css'></link>"
-				+
-				"<script src='cc/flow/builder.js' type='text/javascript'></script>"
-				+
-				"<script src='cc/kbinfo/kbinfo.js' type='text/javascript'></script>"
-				+
-				"<script src='cc/flow/renderExtensions.js' type='text/javascript'></script>"
-				+
-				"<script src='cc/kbinfo/extensions.js' type='text/javascript'></script>"
-				+
-				"<script src='cc/flow/flowchart.js' type='text/javascript'></script>"
-				+
-				"<script src='cc/flow/action.js' type='text/javascript'></script>" +
-				"<script src='cc/flow/guard.js' type='text/javascript'></script>" +
-				"<script src='cc/flow/node.js' type='text/javascript'></script>" +
-				"<script src='cc/flow/rule.js' type='text/javascript'></script>" +
-				"<script src='cc/flow/router.js' type='text/javascript'></script>\n" +
-				"<xml id='" + sourceID + "' style ='display:none;'>" + source + "</xml>\n" +
-				"<xml id='referredKBInfo' style='display:none;'>" +
-				JSPHelper.getReferrdInfoObjectsAsXML(web) +
-				"</xml>" +
-				"<div id='" + name + "' style='width:" + width + "px; height: " + height + "px;'>" +
-				initScript +
-				"</div>\n" +
-				"</div>\n";
-		return KnowWEUtils.maskHTML(result);
 
+		StringBuilder result = new StringBuilder("\n<div>");
+
+		for (String cssfile : CSS) {
+			result.append("<link rel='stylesheet' type='text/css' href='" + cssfile + "'></link>");
+		}
+
+		for (String jsfile : JS) {
+			result.append("<script src='" + jsfile + "' type='text/javascript'></script>");
+		}
+
+		addDisplayPlugins(result);
+
+		result.append("\n");
+		result.append("<xml id='" + sourceID + "' style ='display:none;'>" + source + "</xml>\n");
+		result.append("<xml id='referredKBInfo' style='display:none;'>");
+		result.append(JSPHelper.getReferrdInfoObjectsAsXML(web));
+		result.append("</xml>");
+		result.append("<div id='" + name + "' style='width:" + width + "px; height: " + height
+				+ "px;'>");
+		result.append(initScript);
+		result.append("</div>\n</div>\n");
+
+		return KnowWEUtils.maskHTML(result.toString());
+
+	}
+
+	/**
+	 * 
+	 * @created 26.05.2011
+	 * @param result
+	 */
+	public static void addDisplayPlugins(StringBuilder result) {
+		Extension[] extensions = JPFPluginManager.getInstance().getExtensions(
+				DiaFluxDisplayEnhancement.PLUGIN_ID, DiaFluxDisplayEnhancement.EXTENSION_POINT_ID);
+		for (Extension extension : extensions) {
+			DiaFluxDisplayEnhancement enh = (DiaFluxDisplayEnhancement) extension.getNewInstance();
+
+			for (String script : enh.getScripts()) {
+				result.append("<script src='" + script + "' type='text/javascript'></script>");
+			}
+
+			for (String style : enh.getStylesheets()) {
+				result.append("<link rel='stylesheet' type='text/css' href='" + style + "'></link>");
+			}
+
+		}
 	}
 }
