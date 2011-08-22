62017d422afc20101b1f08991bc67856912787c9
Volker Belli <volker.belli@denkbares.com>
2011-08-22 06:31:27 +0000

    more work on denkbares skin: * search menu only on focus instead of annoying mouseover * fixed search results positioning if search expression is removed * fixed search results title if search expression is removed

--- KnowWE/KnowWE-Resources/src/main/webapp/templates/knowweTmps/skins/denkbares/skin.js
+++ KnowWE/KnowWE-Resources/src/main/webapp/templates/knowweTmps/skins/denkbares/skin.js
@@ -10,6 +10,8 @@ if (RoundedCorners) {
 
 }
 
+// overwrite "Wiki.locatemenu" to correct bug
+// if menu ist relative to a non-document element
 Wiki.locatemenu = function(base, el) {
 	var win = {
 		'x' : window.getWidth(),
@@ -37,25 +39,61 @@ Wiki.locatemenu = function(base, el) {
 			'x' : base.offsetWidth,
 			'y' : base.offsetHeight
 		};
-	}
-	else {
+	} else {
 		// search for parent defining it own coordinate system
-		for (var anchor = el.parentNode; anchor; anchor = anchor.parentNode) {
+		for ( var anchor = $(el.parentNode); anchor && anchor != document; anchor = $(anchor.parentNode)) {
 			var cssPosition = anchor.getStyle('position');
 			if (cssPosition == 'absolute' || cssPosition == 'relative') {
-				parent = $('page').getPosition();
+				parent = anchor.getPosition();
 				break;
 			}
 		}
 	}
 
 	for ( var z in prop) {
-		var pos = corner[z] + offset[z] - parent[z]; /*
-														 * top-left corner of
-														 * base
-														 */
+		// top-left corner of base
+		var pos = corner[z] + offset[z] - parent[z];
 		if ((pos + popup[z] - scroll[z]) > win[z])
 			pos = win[z] - popup[z] + scroll[z];
 		el.setStyle(prop[z], pos);
+	}
+};
+
+// hack into search menu
+// 1) to avoid popup at mouseover but having it on focus instead
+// 2) locate search-popup, even if text is selected
+if (SearchBox) {
+	
+	SearchBox.onPageLoadQuickSearch_original = SearchBox.onPageLoadQuickSearch;
+	SearchBox.ajaxQuickSearch_original = SearchBox.ajaxQuickSearch;
+	SearchBox.noSearchTargetText_original = null;
+
+	SearchBox.onPageLoadQuickSearch = function() {
+		// call original first, doing lots of other stuff
+		SearchBox.onPageLoadQuickSearch_original();
+		// remove hover events
+		$(this.query.form).removeEvents("mouseout");
+		$(this.query.form).removeEvents("mouseover");
+		// and add focus events instead
+		$(this.query).addEvent("blur", function() {
+			this.hover.start(0);
+		}.bind(this)).addEvent("focus", function() {
+			Wiki.locatemenu(this.query, $("searchboxMenu"));
+			this.hover.start(0.9);
+		}.bind(this));
+	}
+	
+	SearchBox.ajaxQuickSearch = function() {
+		// capture original text before first search
+		if (!SearchBox.noSearchTargetText_original) {
+			SearchBox.noSearchTargetText_original = $('searchTarget').innerHTML;
+		}
+		SearchBox.ajaxQuickSearch_original();
+		// if search is empty, restore original text and relocate menu
+		var a = this.query.value.stripScripts();
+		if ((a == null) || (a.trim() == "") || (a == this.query.defaultValue)) {
+			$('searchTarget').innerHTML = SearchBox.noSearchTargetText_original;
+			Wiki.locatemenu($("query"), $("searchboxMenu"));
+		}
 	}
 };
