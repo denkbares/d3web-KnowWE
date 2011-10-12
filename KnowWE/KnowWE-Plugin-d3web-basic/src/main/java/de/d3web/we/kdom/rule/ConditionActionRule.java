63731b66cbe53eda103a348afbab4355c38e6bef
Albrecht Striffler <albrecht.striffler@denkbares.com>
2011-10-12 11:09:41 +0000

    A ConditionActionRule now can only start if there are only white spaces between the new line and the starting IF.

--- KnowWE/KnowWE-Plugin-d3web-basic/src/main/java/de/d3web/we/kdom/rule/ConditionActionRule.java
+++ KnowWE/KnowWE-Plugin-d3web-basic/src/main/java/de/d3web/we/kdom/rule/ConditionActionRule.java
@@ -1,17 +1,17 @@
 /*
  * Copyright (C) 2010 Chair of Artificial Intelligence and Applied Informatics
  * Computer Science VI, University of Wuerzburg
- *
+ * 
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- *
+ * 
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -28,26 +28,25 @@ import de.knowwe.core.kdom.sectionFinder.RegexSectionFinder;
 
 /**
  * @author Jochen
- *
+ * 
  *         Container for ConditionActionRule
- *
+ * 
  */
 public class ConditionActionRule extends AbstractType implements IncrementalMarker {
 
+	private static final String RULE_START = "(?:IF|WENN)";
+
 	public ConditionActionRule() {
-		sectionFinder = new RegexSectionFinder(
-				"(IF|WENN).*?(?=(\\s*?(?m)^\\s*?$\\s*|\\s*IF|\\s*WENN"
-						+ "|\\s*\\z))\\p{Space}*",
-				Pattern.DOTALL);
+		this(null);
 	}
 
 	public ConditionActionRule(AbstractType action) {
 		sectionFinder = new RegexSectionFinder(
-				"(IF|WENN).*?(?=(\\s*?(?m)^\\s*?$\\s*|\\s*IF|\\s*WENN"
-						+ "|\\s*\\z))",
-				Pattern.DOTALL);
-
-		this.addChildType(new ConditionActionRuleContent(action));
+				RULE_START + ".*?(?=\\s*?(^\\s*?$|^\\s*" + RULE_START + "|\\z))",
+				Pattern.DOTALL + Pattern.MULTILINE);
+		if (action != null) {
+			this.addChildType(new ConditionActionRuleContent(action));
+		}
 	}
 
 }
