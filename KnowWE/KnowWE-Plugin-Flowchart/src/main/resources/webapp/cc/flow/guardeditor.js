3720e885ab1077e42ff27645fc8d62910a2ba909
Reinhard Hatko <hatko@d3web.svn.invalid>
2011-11-29 16:07:07 +0000

    textarea for formulas (on guards)

--- KnowWE/KnowWE-Plugin-Flowchart/src/main/resources/webapp/cc/flow/guardeditor.js
+++ KnowWE/KnowWE-Plugin-Flowchart/src/main/resources/webapp/cc/flow/guardeditor.js
@@ -80,8 +80,6 @@ GuardEditor.prototype.isVisible = function() {
  * If the guard has any variables inside, the current values are injected.
  */
 GuardEditor.prototype.getGuard = function() {
-	// check value of select is user has selected with cursor keys
-	this.updateFromUI();
 	var guard = this.getSelectedGuard();
 	if (guard) {
 		guard = new Guard(guard.getMarkup(), guard.getConditionString(), guard.getDisplayHTML(), guard.unit);
@@ -102,6 +100,10 @@ GuardEditor.prototype.getSelectedGuard = function() {
 	return this.possibleGuards[this.selectedIndex];
 }
 
+/**
+ * Is called after selecting a new Guard-Template. Creates new guard according to entered values 
+ * and refreshes the input fields.
+ */
 GuardEditor.prototype.handleValueSelected = function() {
 	if (!this.isVisible()) return;
 	this.updateFromUI();
@@ -109,11 +111,22 @@ GuardEditor.prototype.handleValueSelected = function() {
 	if (this.onChangeListener) this.onChangeListener(this.getGuard());
 }
 
+/**
+ * Is called after input into textfield/-area. Creates new guard according to entered values 
+ */
+GuardEditor.prototype.handleInput = function() {
+	if (!this.isVisible()) return;
+	this.updateFromUI();
+	if (this.onChangeListener) this.onChangeListener(this.getGuard());
+}
+
+
+
 GuardEditor.prototype.updateFromUI = function() {
 	if (!this.isVisible()) return;
 	var select = this.dom.select('.value select')[0];
 	this.selectedIndex = select.value;
-	var inputs = this.dom.select('.input input');
+	var inputs = this.dom.select('.inputParent .input');
 	var guard = this.getSelectedGuard();
 	var count = guard ? guard.countVariables() : 0;
 	this.values = [];
@@ -133,20 +146,14 @@ GuardEditor.prototype.updateFromUI = function() {
 
 GuardEditor.prototype.updateInputField = function() {
 	var guard = this.getSelectedGuard();
-	var inputs = this.dom.select('.input input');
 	var count = guard ? guard.countVariables() : 0;
-	var formula = this.getSelectedGuard() ? this.getSelectedGuard().isFormula() : false;
-	for (var i=0; i<inputs.length; i++) {
-		if (i<count) {
-			if (i ==0){
-				if (formula){ // enlarge input field for formulas
-					inputs[0].addClassName("formula");
-					
-				} else {
-					inputs[0].removeClassName("formula");
-					
-				}
-			}
+	var inputParent = this.dom.select('.inputParent')[0];
+	var inputs = this.createInputFields();
+	inputParent.update(''); //remove old inputs
+	
+	for (var i = 0; i < inputs.length; i++){
+		inputParent.insert(inputs[i]);
+		if (i < count) {
 			inputs[i].show();
 			//focuses the first of the inputs
 			if (i == 0){
@@ -209,21 +216,33 @@ GuardEditor.prototype.render = function() {
 		select += '<a href="#" class="inputFocus"></a>';
 	}
 	
-	var selectParent, inputParent;
-	var inputclass = this.getGuard() ? (this.getGuard().isFormula() ? 'formula' : '') : '';
-	var dom = Builder.node('div', {
-		className: 'GuardEditor '
-	}, 
+	var selectParent, inputParent, inputs;
+	inputs = this.createInputFields(); 
+	
+	var dom = Builder.node('div', {className: 'GuardEditor '}, 
 	[
 		selectParent = Builder.node('div', {className: 'value'}),	// value dropdown parent
-		inputParent = Builder.node('div', {className: 'input GuardEditorEventHandler'}, [
-			Builder.node('input', {	type: 'text', className: inputclass, style: 'display: none;', onBlur: 'this.parentNode.parentNode.__GuardEditor.handleValueSelected();', value: this.values.length > 0 ? this.values[0] : ''}),
-			Builder.node('input', {	type: 'text', style: 'display: none;', onBlur: 'this.parentNode.parentNode.__GuardEditor.handleValueSelected();', value: this.values.length > 1 ? this.values[1] : ''})
-		])
+		inputParent = Builder.node('div', {className: 'inputParent GuardEditorEventHandler'}, inputs)
 	]);
 	dom.__GuardEditor = this;
 	selectParent.innerHTML = select;
 	return dom;
+}
+
+GuardEditor.prototype.createInputFields = function(){
+	if (this.getGuard().isFormula()) {
+		return [
+		    Builder.node('textarea', {className: 'input formula', style: 'display: none;', onBlur: 'this.parentNode.parentNode.__GuardEditor.handleInput();', rows: 1},[this.values.length > 0 ? this.values[0] : '']),
+		    //this one should never be visible, but avoids to see '${num} when switching from formula to interval
+		    Builder.node('input', {	className: 'input', type: 'text', style: 'display: none;', onBlur: 'this.parentNode.parentNode.__GuardEditor.handleValueSelected();', value: ''})
+		];
+	} else {
+		return [
+			Builder.node('input', {	className: 'input', type: 'text', style: 'display: none;', onBlur: 'this.parentNode.parentNode.__GuardEditor.handleInput();', value: this.values.length > 0 ? this.values[0] : ''}),
+			Builder.node('input', {	className: 'input', type: 'text', style: 'display: none;', onBlur: 'this.parentNode.parentNode.__GuardEditor.handleInput();', value: this.values.length > 1 ? this.values[1] : ''})
+		];
+	}
+	
 }
 
 // avoid handling key events while editing the edit fields or the select
@@ -231,7 +250,7 @@ CCEvents.addClassListener('keydown', 'GuardEditorEventHandler', function(event) 
 	
 	switch(event.keyCode){
 	case Event.KEY_RETURN:
-		this.parentNode.__GuardEditor.handleValueSelected();
+		this.parentNode.__GuardEditor.handleInput();
 	case Event.KEY_ESC: 
 		//if the editor is not hidden before setting the selection to null, the values are set by onBlurHandler
 		this.parentNode.__GuardEditor.setVisible(false); 
