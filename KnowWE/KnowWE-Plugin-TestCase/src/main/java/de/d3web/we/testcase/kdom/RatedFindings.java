535f67d1de4eeaa167e3c255aacf3dfa76958004
Albrecht Striffler <albrecht.striffler@denkbares.com>
2011-12-02 11:18:03 +0000

    Fixed bug in SectionFinder for RatedFindings.

--- KnowWE/KnowWE-Plugin-TestCase/src/main/java/de/d3web/we/testcase/kdom/RatedFindings.java
+++ KnowWE/KnowWE-Plugin-TestCase/src/main/java/de/d3web/we/testcase/kdom/RatedFindings.java
@@ -1,20 +1,20 @@
 /*
  * Copyright (C) 2011 University Wuerzburg, Computer Science VI
- *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 3 of
- * the License, or (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * 
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
  */
 package de.d3web.we.testcase.kdom;
 
@@ -28,7 +28,6 @@ import de.knowwe.core.kdom.sectionFinder.SectionFinder;
 import de.knowwe.core.kdom.sectionFinder.SectionFinderResult;
 import de.knowwe.core.utils.SplitUtility;
 import de.knowwe.core.utils.StringFragment;
-
 
 /**
  * 
@@ -53,9 +52,10 @@ public class RatedFindings extends AbstractType {
 			if (ratedsolutions.size() > 1) {
 				int start = text.indexOf(ratedsolutions.get(1).getContentTrimmed());
 				int end = text.lastIndexOf(":");
-				SectionFinderResult s =
-						new SectionFinderResult(start, end);
-				result.add(s);
+				if (start < end) {
+					SectionFinderResult s = new SectionFinderResult(start, end);
+					result.add(s);
+				}
 			}
 
 			return result;
