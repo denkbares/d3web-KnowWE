ef53ef92131c657620f7dbf62eec60ae614cda62
Reinhard Hatko <hatko@d3web.svn.invalid>
2013-02-04 18:28:39 +0000

    Added type for JSPWiki Plugins [{...}]

---
+++ KnowWE/KnowWE-Plugin-JSPWiki-MarkupSet/src/main/java/de/knowwe/jspwiki/types/PluginType.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2013 University Wuerzburg, Computer Science VI
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package de.knowwe.jspwiki.types;
+
+import java.util.regex.Pattern;
+
+import de.knowwe.core.kdom.AbstractType;
+import de.knowwe.core.kdom.sectionFinder.RegexSectionFinder;
+
+
+/**
+ * A type that represents an included JSPWikiPlugin, ie [{Pluginname ...}]. This
+ * type does not handle KnowWE Taghandlers.
+ * 
+ * @author Reinhard Hatko
+ * @created 04.02.2013
+ */
+public class PluginType extends AbstractType {
+
+	public PluginType() {
+		// The sectionfinder could be improve to support nested plugins
+		// This is eg supported by JSPWiki's Table-Plugin
+		setSectionFinder(new RegexSectionFinder("\\[\\{.*?\\}\\]",
+				Pattern.CASE_INSENSITIVE | Pattern.DOTALL));
+	}
+}
