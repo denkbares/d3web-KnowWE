d4f6f921df183c4b108118d4a4fd696a2fa8ae50
Reinhard Hatko <hatko@d3web.svn.invalid>
2013-03-27 23:19:04 +0000

    test now only checks convention for TermDefs, not for TermRefs too. This greatly reduces the number of (repeated) errors

--- KnowWE/KnowWE-Plugin-CI4KE/src/main/java/de/d3web/we/ci4ke/test/TermnameConventionTest.java
+++ KnowWE/KnowWE-Plugin-CI4KE/src/main/java/de/d3web/we/ci4ke/test/TermnameConventionTest.java
@@ -30,7 +30,7 @@ import de.d3web.testing.TestParameter.Type;
 import de.d3web.testing.Utils;
 import de.knowwe.core.kdom.Article;
 import de.knowwe.core.kdom.RootType;
-import de.knowwe.core.kdom.objects.SimpleTerm;
+import de.knowwe.core.kdom.objects.SimpleDefinition;
 import de.knowwe.core.kdom.parsing.Section;
 import de.knowwe.core.kdom.parsing.Sections;
 import de.knowwe.core.utils.Strings;
@@ -57,11 +57,11 @@ public class TermnameConventionTest extends AbstractTest<Article> {
 	public Message execute(Article testObject, String[] args, String[]... ignores) throws InterruptedException {
 		Pattern pattern = Pattern.compile(args[0]);
 		Section<RootType> rootSection = testObject.getRootSection();
-		List<Section<SimpleTerm>> terms = Sections.findSuccessorsOfType(rootSection,
-				SimpleTerm.class);
+		List<Section<SimpleDefinition>> terms = Sections.findSuccessorsOfType(rootSection,
+				SimpleDefinition.class);
 		Set<String> invalidTerms = new HashSet<String>(); // using set to filter
 															// duplicates
-		for (Section<SimpleTerm> section : terms) {
+		for (Section<SimpleDefinition> section : terms) {
 			String termName = section.get().getTermName(section);
 			if (!pattern.matcher(termName).matches()) {
 				invalidTerms.add(termName);
