ddcba6fdf204c7f8b38e886b60747318038867d1
Albrecht Striffler <albrecht.striffler@denkbares.com>
2013-04-24 22:37:34 +0000

    * Added brushed.jspwiki.tableplugin to core-plugins * Added TablePluginType to JSPWiki-MarkupSet to support proper rendering and editing of the new tableplugin. * Implemented NestedBracketsFinder. A SectionFinder allowing to sectionize elements that have the same elements nested inside.

--- KnowWE/KnowWE-Plugin-JSPWiki-MarkupSet/src/main/java/de/knowwe/jspwiki/types/PluginType.java
+++ KnowWE/KnowWE-Plugin-JSPWiki-MarkupSet/src/main/java/de/knowwe/jspwiki/types/PluginType.java
@@ -1,28 +1,25 @@
 /*
  * Copyright (C) 2013 University Wuerzburg, Computer Science VI
- *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 3 of
- * the License, or (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * 
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
  */
 package de.knowwe.jspwiki.types;
 
-import java.util.regex.Pattern;
-
 import de.knowwe.core.kdom.AbstractType;
-import de.knowwe.core.kdom.sectionFinder.RegexSectionFinder;
-
+import de.knowwe.core.kdom.sectionFinder.NestedBracketsFinder;
 
 /**
  * A type that represents an included JSPWikiPlugin, ie [{Pluginname ...}]. This
@@ -34,9 +31,6 @@ import de.knowwe.core.kdom.sectionFinder.RegexSectionFinder;
 public class PluginType extends AbstractType {
 
 	public PluginType() {
-		// The sectionfinder could be improve to support nested plugins
-		// This is eg supported by JSPWiki's Table-Plugin
-		setSectionFinder(new RegexSectionFinder("\\[\\{.*?\\}\\]",
-				Pattern.CASE_INSENSITIVE | Pattern.DOTALL));
+		setSectionFinder(new NestedBracketsFinder("[{", "}]"));
 	}
 }
