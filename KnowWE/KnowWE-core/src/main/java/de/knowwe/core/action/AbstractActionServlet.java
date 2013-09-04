7601af699ce2e1653a9890ec2c1ccc133d9eac38
Albrecht Striffler <albrecht.striffler@denkbares.com>
2013-09-04 14:32:26 +0000

    Set AJAX get log message to fine.

--- KnowWE/KnowWE-core/src/main/java/de/knowwe/core/action/AbstractActionServlet.java
+++ KnowWE/KnowWE-core/src/main/java/de/knowwe/core/action/AbstractActionServlet.java
@@ -1,17 +1,17 @@
 /*
  * Copyright (C) 2009 Chair of Artificial Intelligence and Applied Informatics
  * Computer Science VI, University of Wuerzburg
- *
+ * 
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- *
+ * 
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -32,7 +32,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * ActionServlet is a Servlet for ajax-based interview or any other user
  * interfaces.
- *
+ * 
  * ActionServlet provides an extensible set of actions that can be used to
  * provide required functionality. These actions can be used from client side to
  * proceed e.g. within the interview or to deliver required information.
@@ -46,25 +46,25 @@ import javax.servlet.http.HttpServletResponse;
  * <p>
  * There are two methods of calling a action:
  * <ol>
- *
+ * 
  * <li>A action is called by using a POST or GET for the following URL:
- *
+ * 
  * <pre>
  * &quot;http://&lt;host&gt;:&lt;port&gt;[/&lt;servlet-locator&gt;]/&lt;command-name&gt;[/&lt;path-suffix&gt;]&quot;
  * </pre>
- *
+ * 
  * Depending on the action, it may utilize information given by query parameters
  * or the path suffix to produce the desired outcome.</li>
- *
+ * 
  * <li>In addition you can call a series of actions using an xml structure as
  * the body of a post request (or the query string). The url of this method is:
- *
+ * 
  * <pre>
  * &quot;http://&lt;host&gt;:&lt;port&gt;[/&lt;servlet-locator&gt;]/command&quot;
  * </pre>
- *
+ * 
  * The provided xml must have the following structure:
- *
+ * 
  * <pre>
  * 	&lt;commands&gt;
  * 		&lt;command name="ajax.Restart" path="ui.zip/index.html"&gt;
@@ -72,19 +72,19 @@ import javax.servlet.http.HttpServletResponse;
  * 		&lt;/command&gt;
  * 	&lt;/commands&gt;
  * </pre>
- *
+ * 
  * The action tag can be repeated multiple times. The example above is almost
  * identical to the following url:
- *
+ * 
  * <pre>
  * &quot;http://&lt;host&gt;:&lt;port&gt;[/&lt;servlet-locator&gt;]/ajax.Restart/ui.zip/index.html?foo=bla&quot;
  * </pre>
- *
+ * 
  * </li>
  * </ol>
- *
+ * 
  * @author Volker Belli (refactored by Sebastian Furth)
- *
+ * 
  */
 public abstract class AbstractActionServlet extends HttpServlet {
 
@@ -92,7 +92,7 @@ public abstract class AbstractActionServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		Logger.getLogger("ActionServlet").info(
+		Logger.getLogger("ActionServlet").fine(
 				"GET: " + request.getRequestURI() + "?" + request.getQueryString());
 		if (request.getPathInfo() == null || request.getPathInfo().length() <= 1) {
 			// no path provided (or only "/" as path)
