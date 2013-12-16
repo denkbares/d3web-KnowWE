9cc9f81b08e7977145d8a316afb8fa47df3a007c
Reinhard Hatko <hatko@d3web.svn.invalid>
2013-12-16 13:28:50 +0000

    added renderer, that simply masks jspwikimarkup

---
+++ KnowWE/KnowWE-Plugin-JSPWiki-Connector/src/main/java/de/knowwe/jspwiki/WikiMarkupMaskingRenderer.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2013 University Wuerzburg, Computer Science VI
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package de.knowwe.jspwiki;
+
+import de.knowwe.core.kdom.parsing.Section;
+import de.knowwe.core.kdom.rendering.RenderResult;
+import de.knowwe.core.kdom.rendering.Renderer;
+import de.knowwe.core.user.UserContext;
+
+
+/**
+ * Renderer that simply masks JSPWikiMarkup.
+ * 
+ * @author Reinhard Hatko
+ * @created 16.12.2013
+ */
+public class WikiMarkupMaskingRenderer implements Renderer {
+
+	@Override
+	public void render(Section<?> section, UserContext user, RenderResult result) {
+		result.appendJSPWikiMarkup(section.getText());
+	}
+
+}
