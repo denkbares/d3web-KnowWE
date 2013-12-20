6cb91ee73083346c4d06f3b9837e9f5d2c558a41
Jochen Reutelshöfer <jochen.reutelshoefer@denkbares.com>
2013-12-20 16:22:41 +0000

    minor improvement of correction provider

--- KnowWE/KnowWE-Plugin-Ontology-MarkupSet/src/main/java/de/knowwe/ontology/edit/URITermCorrectionProvider.java
+++ KnowWE/KnowWE-Plugin-Ontology-MarkupSet/src/main/java/de/knowwe/ontology/edit/URITermCorrectionProvider.java
@@ -18,6 +18,7 @@ import de.knowwe.ontology.kdom.resource.ResourceReference;
 
 public class URITermCorrectionProvider implements CorrectionProvider {
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<CorrectionProvider.Suggestion> getSuggestions(Article article, Section<?> section, int threshold) {
 		Section<ResourceReference> ref = null;
@@ -31,9 +32,9 @@ public class URITermCorrectionProvider implements CorrectionProvider {
 			ref = (Section<ResourceReference>) section;
 		}
 
-
 		TerminologyManager terminologyManager = KnowWEUtils.getTerminologyManager(article);
-		if (terminologyManager.isDefinedTerm(ref.get().getTermIdentifier(ref))) {
+		Identifier originalTermIdentifier = ref.get().getTermIdentifier(ref);
+		if (terminologyManager.isDefinedTerm(originalTermIdentifier)) {
 			// no suggestions if term is correct
 			return null;
 		}
@@ -43,17 +44,30 @@ public class URITermCorrectionProvider implements CorrectionProvider {
 				termReference.getTermObjectClass(Sections.cast(section, Term.class))
 				);
 
-		String originalText = ref.get().getTermIdentifier(ref).toExternalForm();
+		String originalText = originalTermIdentifier.toExternalForm();
 		List<CorrectionProvider.Suggestion> suggestions = new LinkedList<CorrectionProvider.Suggestion>();
 		Levenstein l = new Levenstein();
-
 		for (Identifier match : localTermMatches) {
 			double score = l.score(originalText, match.toExternalForm());
 			if (score >= -threshold) {
 				suggestions.add(new CorrectionProvider.Suggestion(match.getLastPathElement(),
 						(int) score));
+			}/* infix test */
+			else if (match.getPathElementAt(0).equals(originalTermIdentifier.getPathElementAt(0)) && 
+					match.getLastPathElement().matches(".*" + originalTermIdentifier.getLastPathElement() + ".*")) {
+
+				// prevent trivial infix matches
+				if (originalTermIdentifier.getLastPathElement().length() > 4) {
+					int infixScore = -1
+							* (match.getLastPathElement().length() - originalTermIdentifier.getLastPathElement().length());
+					suggestions.add(new CorrectionProvider.Suggestion(
+							match.getLastPathElement(), infixScore
+							));
+
+				}
 			}
 		}
+		
 
 		return suggestions;
 	}
