f70ab15c20d950eacd204290e4d57540ace6d540
Jochen Reutelshöfer <jochen.reutelshoefer@denkbares.com>
2014-01-07 12:29:33 +0000

    CorrectionProvider: added simple matching strategy when infix and suffix are matching to a certain length

--- KnowWE/KnowWE-Plugin-Ontology-MarkupSet/src/main/java/de/knowwe/ontology/edit/URITermCorrectionProvider.java
+++ KnowWE/KnowWE-Plugin-Ontology-MarkupSet/src/main/java/de/knowwe/ontology/edit/URITermCorrectionProvider.java
@@ -19,19 +19,20 @@ public class URITermCorrectionProvider implements CorrectionProvider {
 
 	@SuppressWarnings("unchecked")
 	@Override
-	public List<Suggestion> getSuggestions(TermCompiler compiler, Section<?> section, int threshold) {
+	public List<Suggestion> getSuggestions(TermCompiler compiler,
+			Section<?> section, int threshold) {
 		Section<ResourceReference> ref = null;
 
 		if (!(section.get() instanceof ResourceReference)) {
-			section = Sections.findSuccessor(section,
-					ResourceReference.class);
-			if (section == null) return null;
-		}
-		else {
+			section = Sections.findSuccessor(section, ResourceReference.class);
+			if (section == null)
+				return null;
+		} else {
 			ref = (Section<ResourceReference>) section;
 		}
 
-		TerminologyManager terminologyManager = compiler.getTerminologyManager();
+		TerminologyManager terminologyManager = compiler
+				.getTerminologyManager();
 		Identifier originalTermIdentifier = ref.get().getTermIdentifier(ref);
 		if (terminologyManager.isDefinedTerm(originalTermIdentifier)) {
 			// no suggestions if term is correct
@@ -39,9 +40,9 @@ public class URITermCorrectionProvider implements CorrectionProvider {
 		}
 		Term termReference = ((Term) section.get());
 
-		Collection<Identifier> localTermMatches = terminologyManager.getAllDefinedTermsOfType(
-				termReference.getTermObjectClass(Sections.cast(section, Term.class))
-				);
+		Collection<Identifier> localTermMatches = terminologyManager
+				.getAllDefinedTermsOfType(termReference
+						.getTermObjectClass(Sections.cast(section, Term.class)));
 
 		String originalText = originalTermIdentifier.toExternalForm();
 		List<CorrectionProvider.Suggestion> suggestions = new LinkedList<CorrectionProvider.Suggestion>();
@@ -49,27 +50,77 @@ public class URITermCorrectionProvider implements CorrectionProvider {
 		for (Identifier match : localTermMatches) {
 			double score = l.score(originalText, match.toExternalForm());
 			if (score >= -threshold) {
-				suggestions.add(new CorrectionProvider.Suggestion(match.getLastPathElement(),
-						(int) score));
+				suggestions.add(new CorrectionProvider.Suggestion(match
+						.getLastPathElement(), (int) score));
 			}/* infix test */
-			else if (match.getPathElementAt(0).equals(originalTermIdentifier.getPathElementAt(0))
-					&&
-					match.getLastPathElement().matches(
-							".*" + originalTermIdentifier.getLastPathElement() + ".*")) {
+			else if (match.getPathElementAt(0).equals(
+					originalTermIdentifier.getPathElementAt(0))
+					&& match.getLastPathElement().matches(
+							".*" + originalTermIdentifier.getLastPathElement()
+									+ ".*")) {
 
 				// prevent trivial infix matches
 				if (originalTermIdentifier.getLastPathElement().length() > 4) {
 					int infixScore = -1
-							* (match.getLastPathElement().length() - originalTermIdentifier.getLastPathElement().length());
-					suggestions.add(new CorrectionProvider.Suggestion(
-							match.getLastPathElement(), infixScore
-							));
+							* (match.getLastPathElement().length() - originalTermIdentifier
+									.getLastPathElement().length());
+					suggestions.add(new CorrectionProvider.Suggestion(match
+							.getLastPathElement(), infixScore));
 
+				}
+			} else {
+				/*
+				 * prefix+suffix match 
+				 */
+				double singleDifferenceScore = singleDifferenceMatchScore(
+						match.getLastPathElement(),
+						originalTermIdentifier.getLastPathElement());
+				if (singleDifferenceScore > 0.6) {
+					suggestions.add(new CorrectionProvider.Suggestion(match
+							.getLastPathElement(),
+							(int) (singleDifferenceScore * 10)));
 				}
 			}
 		}
 
 		return suggestions;
+	}
+
+	private double singleDifferenceMatchScore(String string1, String string2) {
+		/*
+		 * how long strings match from the beginning
+		 */
+		int prefixMatchLength = 0;
+		for (int i = 0; i < string1.length(); i++) {
+			if (i >= string2.length())
+				break;
+			if (string1.charAt(i) == string2.charAt(i)) {
+				prefixMatchLength++;
+			} else {
+				break;
+			}
+		}
+
+		/*
+		 * how long strings match when starting from the end respectively
+		 */
+		int suffixMatchLength = 0;
+		for (int i = 0; i < string1.length(); i++) {
+			if (string2.length() - 1 - i < 0)
+				break;
+			if (string1.charAt(string1.length() - 1 - i) == string2
+					.charAt(string2.length() - 1 - i)) {
+				suffixMatchLength++;
+			} else {
+				break;
+			}
+		}
+
+		int maxLength = Math.max(string1.length(), string2.length());
+
+		double score = (((double) (prefixMatchLength + suffixMatchLength)) / maxLength);
+
+		return score;
 	}
 
 }
