fc2053d237adc417e2edc0241e4bea1c258ca704
Sebastian Furth <sebastian.furth@denkbares.com>
2014-02-17 13:01:21 +0000

    added CITest that checks URIs for some bad smells

---
+++ KnowWE/KnowWE-Plugin-Ontology-MarkupSet/src/main/java/de/knowwe/ontology/ci/URIPatternTest.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (C) 2013 denkbares GmbH
+ *
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
+ */
+package de.knowwe.ontology.ci;
+
+import de.d3web.strings.Identifier;
+import de.d3web.testing.AbstractTest;
+import de.d3web.testing.Message;
+import de.knowwe.core.compile.terminology.TerminologyManager;
+import de.knowwe.ontology.compile.OntologyCompiler;
+import de.knowwe.ontology.kdom.namespace.NamespaceAbbreviationDefinition;
+
+import java.util.Collection;
+import java.util.LinkedList;
+
+
+/**
+ * 
+ * @author Sebastian Furth (denkbares GmbH)
+ * @created 10.01.2014
+ */
+public class URIPatternTest extends AbstractTest<OntologyCompiler> {
+
+	private static final String URI_CHARS = "[^\\w\\d!#$&;=?\\[]_~-]";
+	private static final String FILE_EXTENSION = ".+\\..{2,4}$";
+	private static final String HASH_TAG = ".+#.+";
+	private static final String VERSION_NUMBER = ".*(v|V|version|Version)\\d+(\\.?\\d+)*.*";
+	private static final String QUERY_STRING = ".*\\?.+=.+(&.+=.+)?.*";
+
+	@Override
+	public Message execute(OntologyCompiler testObject, String[] args, String[]... ignores) throws InterruptedException {
+
+		TerminologyManager manager = testObject.getTerminologyManager();
+		Collection<Identifier> identifiers = manager.getAllDefinedTerms();
+		Collection<Message> messages = new LinkedList<Message>();
+		for (Identifier identifier : identifiers) {
+			if (!isNamespaceDefinition(identifier, manager)) {
+				// TODO: avoid using auto-increment
+				if (matchesPattern(URI_CHARS, identifier.getPathElements())) {
+					messages.add(new Message(Message.Type.FAILURE, "URI contains characters that will be encoded: " + identifier));
+				}
+				if (matchesPattern(FILE_EXTENSION, identifier.getLastPathElement())) {
+					messages.add(new Message(Message.Type.FAILURE, "URI contains file extension: " + identifier));
+				}
+				if (matchesPattern(HASH_TAG, identifier.getLastPathElement())) {
+					messages.add(new Message(Message.Type.FAILURE, "concept name contains #. " + identifier));
+				}
+				if (matchesPattern(VERSION_NUMBER, identifier.getPathElements())) {
+					messages.add(new Message(Message.Type.FAILURE, "URIs shouldn't refer to a special version of a concept: "
+							+ identifier));
+				}
+				if (matchesPattern(QUERY_STRING, identifier.getLastPathElement())) {
+					messages.add(new Message(Message.Type.FAILURE, "URIs shouldn't use query strings: " + identifier));
+				}
+			}
+		}
+
+		if (!messages.isEmpty()) {
+			return new Message(Message.Type.FAILURE, mergeMessages(messages));
+		}
+
+		return Message.SUCCESS;
+	}
+
+	private boolean isNamespaceDefinition(Identifier identifier, TerminologyManager manager) {
+		Collection<Class<?>> classes = manager.getTermClasses(identifier);
+		return classes.size() == 1 && classes.contains(NamespaceAbbreviationDefinition.class);
+	}
+
+	private boolean matchesPattern(String pattern, String... elements) {
+		for (String element : elements) {
+			if (!element.isEmpty() && element.matches(pattern)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private String mergeMessages(Collection<Message> errorMessages) {
+		StringBuilder result = new StringBuilder();
+		result.append("The following smells where detected:\n");
+		for (Message message : errorMessages) {
+			result.append(message.getText());
+			result.append("\n");
+		}
+		return result.toString();
+	}
+
+	@Override
+	public Class<OntologyCompiler> getTestObjectClass() {
+		return OntologyCompiler.class;
+	}
+
+	@Override
+	public String getDescription() {
+		return "Checks for all Terms (URIs) whether they follow best practice URL patterns.";
+	}
+
+}
