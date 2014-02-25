220c34c832deb8a514226535add61cc1c7091918
Volker Belli <volker.belli@denkbares.com>
2014-02-25 06:39:12 +0000

    Some work on Footnotes * warning and removal of missing footnotes * consistent numbering across pages and files

--- KnowWE/KnowWE-Plugin-Include/src/main/java/de/knowwe/include/export/FootnoteReferenceExporter.java
+++ KnowWE/KnowWE-Plugin-Include/src/main/java/de/knowwe/include/export/FootnoteReferenceExporter.java
@@ -19,8 +19,8 @@
 package de.knowwe.include.export;
 
 import java.math.BigInteger;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.poi.xwpf.usermodel.VerticalAlign;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
@@ -30,19 +30,20 @@ import de.d3web.utils.Pair;
 import de.knowwe.core.kdom.Article;
 import de.knowwe.core.kdom.parsing.Section;
 import de.knowwe.core.kdom.parsing.Sections;
+import de.knowwe.core.report.Messages;
 import de.knowwe.jspwiki.types.FootnoteType;
 import de.knowwe.jspwiki.types.LinkType;
 
 /**
- * Exporter for exporting inner-wiki links that are pointing to a footnote of
- * that page.
- * 
+ * Exporter for exporting inner-wiki links that are pointing to a footnote of that page.
+ *
  * @author Volker Belli (denkbares GmbH)
  * @created 07.02.2014
  */
 public class FootnoteReferenceExporter implements Exporter<LinkType> {
 
-	private final Set<Pair<Article, String>> exportedIDs = new HashSet<Pair<Article, String>>();
+	private final Map<Pair<Article, String>, String> exportedIDs =
+			new HashMap<Pair<Article, String>, String>();
 
 	@Override
 	public boolean canExport(Section<LinkType> section) {
@@ -55,55 +56,75 @@ public class FootnoteReferenceExporter implements Exporter<LinkType> {
 	}
 
 	/**
-	 * Returns if the footnote can be exported as a real reference. We do not
-	 * export as a reference in one of the following cases:
-	 * <ol>
-	 * <li>the footnote is used more than once and already be exported (word or
-	 * poi does not support this, leads to corrupt document)
-	 * <li>the footnote definition is not found on that page (also lead to
-	 * corrupt document otherwise)
-	 * </ol>
-	 * 
-	 * @created 19.02.2014
+	 * Checks if the footnote can be exported as a real reference and returns null in this case. Otherwise it returns a
+	 * string that shall be used as the referencing String. This method also reports some warnings to the export if
+	 * there are any. We do not export as a reference in one of the following cases: <ol> <li>the footnote is used more
+	 * than once and already be exported (word or poi does not support this, leads to corrupt document) <li>the footnote
+	 * definition is not found on that page (also lead to corrupt document otherwise) </ol>
+	 *
 	 * @param manager the export manager of this export
 	 * @param article the current article
 	 * @param refNumber the wiki footnote number
-	 * @return if the footnote-reference can be exported as real reference
+	 * @return null if the footnote-reference can be exported as real reference, a synthetic label otherwise
+	 * @created 19.02.2014
 	 */
-	private boolean canExportAsReference(ExportManager manager, Article article, String refNumber) {
+	private String getForcedLabel(DocumentBuilder manager, Article article, String refNumber) {
 		// check if this is the first occurrence
-		Pair<Article, String> key = new Pair<Article, String>(article, refNumber.trim());
-		boolean isNew = exportedIDs.add(key);
-		if (!isNew) return false;
+		Pair<Article, String> key = new Pair<Article, String>(article, refNumber);
+		String exportedID = exportedIDs.get(key);
+		if (exportedID != null) return exportedID;
 
 		// check for footnote section
 		for (Section<FootnoteType> footnote : Sections.successors(article, FootnoteType.class)) {
 			String id = footnote.get().getFootnoteID(footnote);
 			// if footnote matches, return if the footnote
 			// is included in the export (if not, footnote will go wrong)
-			if (refNumber.equals(id)) return manager.isContained(footnote);
+			if (refNumber.equals(id)) {
+				boolean contained = manager.getModel().getManager().isContained(footnote);
+				if (!contained) {
+					manager.getModel().addMessage(Messages.warning("" +
+							"The footnote #" + refNumber + " " +
+							"in article '" + article.getTitle() + "' " +
+							"is referenced, but not included in the export."));
+					return "";
+				}
+				return null;
+			}
 		}
 		// otherwise do not export
-		return false;
+		manager.getModel().addMessage(Messages.warning("" +
+				"The footnote #" + refNumber + " " +
+				"referenced in article '" + article.getTitle() + "' " +
+				"is not defined."));
+		return "";
+	}
+
+	private void addForcedLabel(Article article, String refNumber, String label) {
+		Pair<Article, String> key = new Pair<Article, String>(article, refNumber);
+		exportedIDs.put(key, label);
 	}
 
 	@Override
 	public void export(Section<LinkType> section, DocumentBuilder manager) throws ExportException {
 		// find id for our footnote
 		String refNumber = LinkType.getLink(section);
-		BigInteger noteId = FootnoteExporter.getFootnoteID(manager, section.getArticle(), refNumber);
 
 		// add reference to text
 		XWPFRun run = manager.getParagraph().createRun();
 		run.setSubscript(VerticalAlign.SUPERSCRIPT);
-		if (canExportAsReference(manager.getModel().getManager(), section.getArticle(), refNumber)) {
+		Article article = section.getArticle();
+		String label = getForcedLabel(manager, article, refNumber);
+		if (label == null) {
 			// add reference to footnote
 			CTFtnEdnRef textRef = run.getCTR().addNewFootnoteReference();
+			BigInteger noteId = FootnoteExporter.getFootnoteID(manager, article, refNumber);
 			textRef.setId(noteId);
+			addForcedLabel(article, refNumber, String.valueOf(noteId));
 		}
 		else {
 			// otherwise only add text in similar style
-			run.setText(refNumber);
+			run.setText(label);
+			addForcedLabel(article, refNumber, label);
 		}
 	}
 
