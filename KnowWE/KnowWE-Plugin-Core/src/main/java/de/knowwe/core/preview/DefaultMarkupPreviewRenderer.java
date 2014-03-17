ebfe979f32e2391bbc94f71d3fc8ff0ca9144be1
Volker Belli <volker.belli@denkbares.com>
2014-03-17 16:49:34 +0000

    pimped turtle preview for long object lists.

--- KnowWE/KnowWE-Plugin-Core/src/main/java/de/knowwe/core/preview/DefaultMarkupPreviewRenderer.java
+++ KnowWE/KnowWE-Plugin-Core/src/main/java/de/knowwe/core/preview/DefaultMarkupPreviewRenderer.java
@@ -15,20 +15,18 @@ import de.knowwe.core.utils.Scope;
 import de.knowwe.kdom.defaultMarkup.DefaultMarkupRenderer;
 
 /**
- * Default implementation of a {@link PreviewRenderer} that can be applied to
- * any ancestor-section of a default markup type (as scope of the preview
- * rendering extension), as well as to the whole default markup type. if it is
- * applied to the whole default markup type. the default markup frame is not
- * rendered, but the whole content and all annotations sections.
- * <p>
- * If it is only scoped to certain sub-sections only these sections are
- * rendered. In this case you can additionally specify a set of (relative)
- * scopes that will be additionally rendered.
- * <p>
- * Please not that this preview renderer is for selective rendering only. If you
- * wand to render a complete default markup section without its frame, you may
- * use the {@link DefaultPreviewRenderer} instead.
- * 
+ * Default implementation of a {@link PreviewRenderer} that can be applied to any ancestor-section
+ * of a default markup type (as scope of the preview rendering extension), as well as to the whole
+ * default markup type. if it is applied to the whole default markup type. the default markup frame
+ * is not rendered, but the whole content and all annotations sections.
+ * <p/>
+ * If it is only scoped to certain sub-sections only these sections are rendered. In this case you
+ * can additionally specify a set of (relative) scopes that will be additionally rendered.
+ * <p/>
+ * Please not that this preview renderer is for selective rendering only. If you wand to render a
+ * complete default markup section without its frame, you may use the {@link DefaultPreviewRenderer}
+ * instead.
+ *
  * @author Volker Belli (denkbares GmbH)
  * @created 26.08.2013
  */
@@ -46,22 +44,26 @@ public class DefaultMarkupPreviewRenderer extends AbstractPreviewRenderer {
 		relevant,
 
 		/**
-		 * Shows successors that contains relevant items, but all if there are
-		 * no successors with relevant items
+		 * Shows successors that contains relevant items, but if there are no successors with
+		 * relevant items, some example successors are shown (e.g. 5) and then abbreviated by "..."
+		 */
+		relevantOrSome,
+
+		/**
+		 * /** Shows successors that contains relevant items, but all if there are no successors
+		 * with relevant items
 		 */
 		relevantOrAll,
 
 		/**
-		 * Includes the sections matching the scope if the sibling after (!) the
-		 * matched section has been included by a scope selection that is prior
-		 * to this scope selection
+		 * Includes the sections matching the scope if the sibling after (!) the matched section has
+		 * been included by a scope selection that is prior to this scope selection
 		 */
 		beforeSelected,
 
 		/**
-		 * Includes the sections matching the scope if the sibling before (!)
-		 * the matched section has been included by a scope selection that is
-		 * prior to this scope selection
+		 * Includes the sections matching the scope if the sibling before (!) the matched section
+		 * has been included by a scope selection that is prior to this scope selection
 		 */
 		afterSelected
 	}
@@ -72,26 +74,31 @@ public class DefaultMarkupPreviewRenderer extends AbstractPreviewRenderer {
 	}
 
 	/**
-	 * Adds a new scope of sub-items to be shown. When rendering the preview all
-	 * sub-sections will be visible that matches the scopes specified through
-	 * this method. If forceVisible is true. all the matching sub-section are
-	 * shown. If not, only the matching sub-sections are shown that contains
-	 * relevant items.
-	 * 
-	 * @created 26.08.2013
+	 * Adds a new scope of sub-items to be shown. When rendering the preview all sub-sections will
+	 * be visible that matches the scopes specified through this method. If forceVisible is true.
+	 * all the matching sub-section are shown. If not, only the matching sub-sections are shown that
+	 * contains relevant items.
+	 *
 	 * @param scope the sub-scope to be shown
-	 * @param selector if the are always shown or only if the contain relevant
-	 *        items
+	 * @param selector if the are always shown or only if the contain relevant items
+	 * @created 26.08.2013
 	 */
 	public void addPreviewItem(Scope scope, Select selector) {
 		previewItems.put(scope, selector);
 	}
 
 	/**
-	 * Adds the text of plain text section ins between annotation sections,
-	 * containing the line breaks and white-spaces between the single
-	 * annotations.
-	 * 
+	 * Removes all preview items that previously have been added to this preview renderer. You may
+	 * use this method to reinitialize the renderer.
+	 */
+	public void clear() {
+		previewItems.clear();
+	}
+
+	/**
+	 * Adds the text of plain text section ins between annotation sections, containing the line
+	 * breaks and white-spaces between the single annotations.
+	 *
 	 * @created 27.08.2013
 	 */
 	public void addTextBeforeAnnotations() {
@@ -108,47 +115,129 @@ public class DefaultMarkupPreviewRenderer extends AbstractPreviewRenderer {
 			List<Section<?>> matches = scope.getMatchingSuccessors(section);
 			switch (preview) {
 
-			case all:
-				// add all matches if requested
-				previews.addAll(matches);
-				break;
-
-			case relevant:
-			case relevantOrAll:
-				// add only relevant matches if requested
-				boolean added = false;
-				for (Section<?> match : matches) {
-					if (hasSuccessor(match, relevantSubSections)) {
-						previews.add(match);
-						added = true;
-					}
-				}
-				// add all if no one has shown to be relevant
-				// if this has been requested
-				if (preview.equals(Select.relevantOrAll) && !added) {
+				case all:
+					// add all matches if requested
 					previews.addAll(matches);
-				}
-				break;
+					break;
 
-			case afterSelected:
-			case beforeSelected:
-				for (Section<?> match : matches) {
-					List<Section<?>> siblings = match.getParent().getChildren();
-					int index = siblings.indexOf(match);
-					if (index == -1) continue;
-					// move index to the sibling that has to be included
-					int selectedIndex = Select.afterSelected.equals(preview)
-							? index - 1 : index + 1;
-					if (selectedIndex < 0) continue;
-					if (selectedIndex >= siblings.size()) continue;
-					Section<?> sibling = siblings.get(selectedIndex);
-					// and add this match if the desired sibling is available
-					if (previews.contains(sibling)) previews.add(match);
-				}
-				break;
+				case relevant:
+				case relevantOrSome:
+				case relevantOrAll:
+					// add only relevant matches if requested
+					boolean added = false;
+					for (Section<?> match : matches) {
+						if (hasSuccessor(match, relevantSubSections)) {
+							previews.add(match);
+							added = true;
+						}
+					}
+					// add all if no one has shown to be relevant
+					// if this has been requested
+					if (!preview.equals(Select.relevant) && !added) {
+						if (preview.equals(Select.relevantOrSome) && matches.size() > 8) {
+							matches = matches.subList(0, 5);
+						}
+						previews.addAll(matches);
+					}
+					break;
+
+				case afterSelected:
+					for (Section<?> match : matches) {
+						if (containsSectionBefore(previews, match)) {
+							previews.add(match);
+						}
+					}
+					break;
+
+				case beforeSelected:
+					for (Section<?> match : matches) {
+						if (containsSectionAfter(previews, match)) {
+							previews.add(match);
+						}
+//						List<Section<?>> siblings = match.getParent().getChildren();
+//						int index = siblings.indexOf(match);
+//						if (index == -1) continue;
+//						// move index to the sibling that has to be included
+//						int selectedIndex = Select.afterSelected.equals(preview)
+//								? index - 1 : index + 1;
+//						if (selectedIndex < 0) continue;
+//						if (selectedIndex >= siblings.size()) continue;
+//						Section<?> sibling = siblings.get(selectedIndex);
+//						// and add this match if the desired sibling is available
+//						if (previews.contains(sibling)) previews.add(match);
+					}
+					break;
 			}
 		}
 		renderSections(previews, user, result);
+	}
+
+	/**
+	 * Returns if the specified collection of Sections contains any of the direct sections after the
+	 * specified section. The sections after are all sections, that does not intersect with the
+	 * specified section, but have their start index equal to the end index of the specified start
+	 * section.
+	 *
+	 * @param set the sections to check for
+	 * @param start the section to examine the sections after it
+	 * @return if the section after the specified section is in the list
+	 */
+	private static boolean containsSectionAfter(Collection<Section<?>> set, Section<?> start) {
+		Section<?> grandSibling = null;
+		Section<?> iter = start;
+		while (iter.getParent() != null) {
+			List<Section<?>> siblings = iter.getParent().getChildren();
+			int index = siblings.indexOf(iter) + 1;
+			if (index < siblings.size()) {
+				grandSibling = siblings.get(index);
+				break;
+			}
+			iter = iter.getParent();
+		}
+		// check if we are in the right-most branch
+		if (grandSibling == null) return false;
+		// now descent the left-most branch if any of the children are in our set
+		iter = grandSibling;
+		while (true) {
+			if (set.contains(iter)) return true;
+			List<Section<?>> children = iter.getChildren();
+			if (children.isEmpty()) return false;
+			iter = children.get(0);
+		}
+	}
+
+	/**
+	 * Returns if the specified collection of Sections contains any of the direct sections before
+	 * the specified section. The sections before are all sections, that does not intersect with the
+	 * specified section, but have their end index equal to the start index of the specified start
+	 * section.
+	 *
+	 * @param set the sections to check for
+	 * @param start the section to examine the sections after it
+	 * @return if the section after the specified section is in the list
+	 */
+	private static boolean containsSectionBefore(Collection<Section<?>> set, Section<?> start) {
+		Section<?> grandSibling = null;
+		Section<?> iter = start;
+		while (iter.getParent() != null) {
+			List<Section<?>> siblings = iter.getParent().getChildren();
+			int index = siblings.indexOf(iter) - 1;
+			if (index >= 0) {
+				grandSibling = siblings.get(index);
+				break;
+			}
+			iter = iter.getParent();
+		}
+		// check if we are in the right-most branch
+		if (grandSibling == null) return false;
+		// now descent the left-most branch if any of the children are in our set
+		iter = grandSibling;
+		while (true) {
+			if (set.contains(iter)) return true;
+			List<Section<?>> children = iter.getChildren();
+			if (children.isEmpty()) return false;
+			iter = children.get(children.size() - 1);
+		}
 	}
 
 	static void renderSections(List<Section<?>> previews, UserContext user, RenderResult result) {
@@ -157,13 +246,13 @@ public class DefaultMarkupPreviewRenderer extends AbstractPreviewRenderer {
 	}
 
 	/**
-	 * Returns true if the specified parent section hat at least one of the
-	 * specified successor sections as an successor.
-	 * 
-	 * @created 26.08.2013
+	 * Returns true if the specified parent section hat at least one of the specified successor
+	 * sections as an successor.
+	 *
 	 * @param parent the parent section to start the search from
 	 * @param successors the list of potential successors
 	 * @return
+	 * @created 26.08.2013
 	 */
 	private static boolean hasSuccessor(Section<?> parent, Collection<Section<?>> successors) {
 		if (successors.contains(parent)) return true;
--- KnowWE/KnowWE-Plugin-Ontology-MarkupSet/src/main/java/de/knowwe/ontology/kdom/preview/TurtlePreviewRenderer.java
+++ KnowWE/KnowWE-Plugin-Ontology-MarkupSet/src/main/java/de/knowwe/ontology/kdom/preview/TurtlePreviewRenderer.java
@@ -6,17 +6,38 @@ import de.knowwe.core.utils.Scope;
 public class TurtlePreviewRenderer extends DefaultMarkupPreviewRenderer {
 
 	public TurtlePreviewRenderer() {
-		// otherwise show at least defined class and relevant properties
+		initPreviewItems();
+
+	}
+
+	void initPreviewItems() {
+		clear();
+
+		// show at least defined class and relevant properties
 		addPreviewItem(Scope.getScope("TurtleContent/TurtleSentence/Subject"),
 				Select.all);
 		addPreviewItem(Scope.getScope(
-				"TurtleContent/TurtleSentence/PredicateObjectSentenceList/PredicateSentence"),
+				"TurtleContent/TurtleSentence/PredicateObjectSentenceList/PredicateSentence/Predicate"),
 				Select.relevantOrAll);
+		addPreviewItem(Scope.getScope(
+				"TurtleContent/TurtleSentence/PredicateObjectSentenceList/PredicateSentence/ObjectList/Object"),
+				Select.relevantOrSome);
 
 		addPreviewItem(Scope.getScope("TurtleContent/TurtleSentence/PlainText"),
 				Select.afterSelected);
+		addPreviewItem(Scope.getScope("TurtleContent/TurtleDragDropDotType"),
+				Select.afterSelected);
 		addPreviewItem(Scope.getScope(
-				"TurtleContent/TurtleSentence/PredicateObjectSentenceList/PlainText"),
+				"TurtleContent/TurtleSentence/PredicateObjectSentenceList/PredicateSentence/PlainText"),
+				Select.afterSelected);
+		addPreviewItem(Scope.getScope(
+				"TurtleContent/TurtleSentence/PredicateObjectSentenceList/TurtleDragDropSemicolonType"),
+				Select.afterSelected);
+		addPreviewItem(Scope.getScope(
+				"TurtleContent/TurtleSentence/PredicateObjectSentenceList/PredicateSentence/ObjectList/PlainText"),
+				Select.afterSelected);
+		addPreviewItem(Scope.getScope(
+				"TurtleContent/TurtleSentence/PredicateObjectSentenceList/PredicateSentence/ObjectList/TurtleDragDropCommaType"),
 				Select.afterSelected);
 	}
 }
