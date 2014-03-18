e1c6c7b9d6d34a096a3e82b2e7b8b202c7a4b132
Stefan Plehn <stefan.plehn@denkbares.com>
2014-03-18 14:36:55 +0000

    in any extension where it's possible to define a scope you can also now define an exclude

--- KnowWE/KnowWE-core/src/main/java/de/knowwe/core/utils/ScopeUtils.java
+++ KnowWE/KnowWE-core/src/main/java/de/knowwe/core/utils/ScopeUtils.java
@@ -1,5 +1,25 @@
+/*
+ * Copyright (C) 2014 denkbares GmbH, Germany
+ *
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
+ */
+
 package de.knowwe.core.utils;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -11,51 +31,48 @@ import de.knowwe.core.utils.Scope.TypePath;
 public class ScopeUtils {
 
 	/**
-	 * Returns the all matching extension of a specified array of extensions. A
-	 * match is an extension with its scope matching to the specified section.
-	 * <p>
-	 * If you call this method often for a specific Extension[] and recurring
-	 * type paths, you should use the {@link ScopeExtensions} class which can
-	 * handle a set of scoped extensions more efficient.
-	 * 
-	 * @created 30.11.2013
+	 * Returns the all matching extension of a specified array of extensions. A match is an extension with its scope
+	 * matching to the specified section.
+	 * <p/>
+	 * If you call this method often for a specific Extension[] and recurring type paths, you should use the {@link
+	 * ScopeExtensions} class which can handle a set of scoped extensions more efficient.
+	 *
 	 * @param extensions the extensions to be checked
-	 * @param section the section to check the extensions against
+	 * @param section    the section to check the extensions against
 	 * @return the matching extensions
+	 * @created 30.11.2013
 	 */
 	public static List<Extension> getMatchingExtensions(Extension[] extensions, Section<?> section) {
 		return getMatchingExtensions(extensions, Scope.getTypePath(section));
 	}
 
 	/**
-	 * Returns the all matching extension of a specified array of extensions. A
-	 * match is an extension with its scope matching to the specified type path.
-	 * <p>
-	 * If you call this method often for a specific Extension[] and recurring
-	 * type paths, you should use the {@link ScopeExtensions} class which can
-	 * handle a set of scoped extensions more efficient.
-	 * 
-	 * @created 30.11.2013
+	 * Returns the all matching extension of a specified array of extensions. A match is an extension with its scope
+	 * matching to the specified type path.
+	 * <p/>
+	 * If you call this method often for a specific Extension[] and recurring type paths, you should use the {@link
+	 * ScopeExtensions} class which can handle a set of scoped extensions more efficient.
+	 *
 	 * @param extensions the extensions to be checked
-	 * @param typePath the type path to check the extensions against
+	 * @param typePath   the type path to check the extensions against
 	 * @return the matching extensions
+	 * @created 30.11.2013
 	 */
 	public static List<Extension> getMatchingExtensions(Extension[] extensions, Type[] typePath) {
 		return getMatchingExtensions(extensions, new TypePath(typePath));
 	}
 
 	/**
-	 * Returns the all matching extension of a specified array of extensions. A
-	 * match is an extension with its scope matching to the specified type path.
-	 * <p>
-	 * If you call this method often for a specific Extension[] and recurring
-	 * type paths, you should use the {@link ScopeExtensions} class which can
-	 * handle a set of scoped extensions more efficient.
-	 * 
-	 * @created 30.11.2013
+	 * Returns the all matching extension of a specified array of extensions. A match is an extension with its scope
+	 * matching to the specified type path.
+	 * <p/>
+	 * If you call this method often for a specific Extension[] and recurring type paths, you should use the {@link
+	 * ScopeExtensions} class which can handle a set of scoped extensions more efficient.
+	 *
 	 * @param extensions the extensions to be checked
-	 * @param typePath the type path to check the extensions against
+	 * @param typePath   the type path to check the extensions against
 	 * @return the matching extensions
+	 * @created 30.11.2013
 	 */
 	public static List<Extension> getMatchingExtensions(Extension[] extensions, TypePath typePath) {
 		List<Extension> matches = new LinkedList<Extension>();
@@ -72,58 +89,66 @@ public class ScopeUtils {
 				}
 			}
 		}
+		Iterator<Extension> iterator = matches.iterator();
+		while (iterator.hasNext()) {
+			Extension match = iterator.next();
+			for (String scopeString : match.getParameters("exclude")) {
+				Scope scope = Scope.getScope(scopeString);
+				if (scope.matches(typePath)) {
+					// if any has matched, remove it
+					iterator.remove();
+					// and proceed to next extension
+					break;
+				}
+			}
+
+		}
 		return matches;
 	}
 
 	/**
-	 * Returns the best/first matching extension of a specified array of
-	 * extensions. A match is an extension with its scope matching to the
-	 * specified section.
-	 * <p>
-	 * If you call this method often for a specific Extension[] and recurring
-	 * type paths, you should use the {@link ScopeExtensions} class which can
-	 * handle a set of scoped extensions more efficient.
-	 * 
-	 * @created 30.11.2013
+	 * Returns the best/first matching extension of a specified array of extensions. A match is an extension with its
+	 * scope matching to the specified section.
+	 * <p/>
+	 * If you call this method often for a specific Extension[] and recurring type paths, you should use the {@link
+	 * ScopeExtensions} class which can handle a set of scoped extensions more efficient.
+	 *
 	 * @param extensions the extensions to be checked
-	 * @param section the section to check the extensions against
+	 * @param section    the section to check the extensions against
 	 * @return the first extension matching the section
+	 * @created 30.11.2013
 	 */
 	public static Extension getMatchingExtension(Extension[] extensions, Section<?> section) {
 		return getMatchingExtension(extensions, new TypePath(section));
 	}
 
 	/**
-	 * Returns the best/first matching extension of a specified array of
-	 * extensions. A match is an extension with its scope matching to the
-	 * specified type path.
-	 * <p>
-	 * If you call this method often for a specific Extension[] and recurring
-	 * type paths, you should use the {@link ScopeExtensions} class which can
-	 * handle a set of scoped extensions more efficient.
-	 * 
-	 * @created 30.11.2013
+	 * Returns the best/first matching extension of a specified array of extensions. A match is an extension with its
+	 * scope matching to the specified type path.
+	 * <p/>
+	 * If you call this method often for a specific Extension[] and recurring type paths, you should use the {@link
+	 * ScopeExtensions} class which can handle a set of scoped extensions more efficient.
+	 *
 	 * @param extensions the extensions to be checked
-	 * @param typePath the type path to check the extensions against
+	 * @param typePath   the type path to check the extensions against
 	 * @return the first extension matching the type path
+	 * @created 30.11.2013
 	 */
 	public static Extension getMatchingExtension(Extension[] extensions, Type[] typePath) {
 		return getMatchingExtension(extensions, new TypePath(typePath));
 	}
 
 	/**
-	 * Returns the best/first matching extension of a specified array of
-	 * extensions. A match is an extension with its scope matching to the
-	 * specified type path.
-	 * <p>
-	 * If you call this method often for a specific Extension[] and recurring
-	 * type paths, you should use the {@link ScopeExtensions} class which can
-	 * handle a set of scoped extensions more efficient.
-	 * 
-	 * @created 30.11.2013
+	 * Returns the best/first matching extension of a specified array of extensions. A match is an extension with its
+	 * scope matching to the specified type path.
+	 * <p/>
+	 * If you call this method often for a specific Extension[] and recurring type paths, you should use the {@link
+	 * ScopeExtensions} class which can handle a set of scoped extensions more efficient.
+	 *
 	 * @param extensions the extensions to be checked
-	 * @param typePath the type path to check the extensions against
+	 * @param typePath   the type path to check the extensions against
 	 * @return the first extension matching the type path
+	 * @created 30.11.2013
 	 */
 	public static Extension getMatchingExtension(Extension[] extensions, TypePath typePath) {
 		for (Extension extension : extensions) {
