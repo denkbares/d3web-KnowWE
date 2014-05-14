af466a43833251999bce3535dd12c5accb7893ed
Veronika Sehne <veronika.sehne@denkbares.com>
2014-05-14 15:49:00 +0000

    Register sparql markup name in terminolgy manager and autocompletion for InlineSparqlMarkup

---
+++ KnowWE/KnowWE-Plugin-Ontology-MarkupSet/src/main/java/de/knowwe/ontology/compile/SparqlNameRegistrationScript.java
@@ -0,0 +1,38 @@
+package de.knowwe.ontology.compile;
+
+
+import de.d3web.strings.Identifier;
+import de.knowwe.core.kdom.parsing.Section;
+import de.knowwe.core.report.CompilerMessage;
+import de.knowwe.kdom.defaultMarkup.DefaultMarkupType;
+import de.knowwe.rdf2go.sparql.SparqlMarkupType;
+
+/**
+ * Register sparql name in terminolgy manager
+ * Created by Veronika Sehne on 14.05.2014.
+ */
+public class SparqlNameRegistrationScript extends OntologyCompileScript<SparqlMarkupType> {
+
+	@Override
+	public void compile(OntologyCompiler compiler, Section<SparqlMarkupType> section) throws CompilerMessage {
+		Identifier nameIdentifier = getIdentifier(section);
+		if (nameIdentifier != null) {
+			compiler.getTerminologyManager().registerTermDefinition(compiler, section, SparqlMarkupType.class, nameIdentifier);
+		}
+
+	}
+
+	private Identifier getIdentifier(Section<SparqlMarkupType> section) {
+		String sparqlName = DefaultMarkupType.getAnnotation(section, SparqlMarkupType.NAME);
+		if (sparqlName == null) {
+			return null;
+		} else {
+			return new Identifier("SPARQL", sparqlName);
+		}
+	}
+
+	@Override
+	public void destroy(OntologyCompiler compiler, Section<SparqlMarkupType> section) {
+		compiler.getTerminologyManager().unregisterTermDefinition(compiler, section, SparqlMarkupType.class, getIdentifier(section));
+	}
+}
--- KnowWE/KnowWE-Plugin-Ontology-MarkupSet/src/main/java/de/knowwe/ontology/kdom/sparql/InlineSparqlMarkup.java
+++ KnowWE/KnowWE-Plugin-Ontology-MarkupSet/src/main/java/de/knowwe/ontology/kdom/sparql/InlineSparqlMarkup.java
@@ -5,25 +5,30 @@ import java.util.Iterator;
 import java.util.List;
 
 import org.ontoware.aifbcommons.collection.ClosableIterator;
+import org.ontoware.rdf2go.model.QueryResultTable;
 import org.ontoware.rdf2go.model.QueryRow;
 import org.ontoware.rdf2go.model.node.Node;
 
+import de.d3web.strings.Identifier;
 import de.d3web.strings.Strings;
 import de.knowwe.core.compile.Compilers;
+import de.knowwe.core.compile.terminology.TermCompiler;
 import de.knowwe.core.kdom.parsing.Section;
+import de.knowwe.core.kdom.parsing.Sections;
 import de.knowwe.core.kdom.rendering.RenderResult;
 import de.knowwe.core.kdom.rendering.Renderer;
 import de.knowwe.core.user.UserContext;
 import de.knowwe.kdom.defaultMarkup.DefaultMarkup;
 import de.knowwe.kdom.defaultMarkup.DefaultMarkupType;
-import de.knowwe.ontology.ci.provider.SparqlTestObjectProviderUtils;
 import de.knowwe.ontology.compile.OntologyCompiler;
+import de.knowwe.rdf2go.Rdf2GoCompiler;
+import de.knowwe.rdf2go.Rdf2GoCore;
 import de.knowwe.rdf2go.sparql.SparqlContentType;
 import de.knowwe.rdf2go.utils.Rdf2GoUtils;
 
 /**
  * Shows contents of references of SparqlQueryInline.
- * 
+ * <p/>
  * Created by Veronika Sehne on 30.04.2014.
  */
 public class InlineSparqlMarkup extends DefaultMarkupType {
@@ -34,8 +39,10 @@ public class InlineSparqlMarkup extends DefaultMarkupType {
 
 	public static final String SEPARATOR = "separator";
 
+	public static final String MARKUP_NAME = "InlineSparql";
+
 	static {
-		MARKUP = new DefaultMarkup("InlineSparql");
+		MARKUP = new DefaultMarkup(MARKUP_NAME);
 		MARKUP.setInline(true);
 		MARKUP.addAnnotation(SEPARATOR, true);
 		MARKUP.addAnnotation(ROW_SEPARATOR, true);
@@ -62,21 +69,43 @@ public class InlineSparqlMarkup extends DefaultMarkupType {
 				rowSeparator = Strings.unquote(rowSeparator);
 
 				sparqlName = clean(sparqlName);
-				Collection<Section<SparqlContentType>> sparqlSections = SparqlTestObjectProviderUtils.getSparqlQueryContentSection(sparqlName);
-				Section<SparqlContentType> sparqlSection = sparqlSections.iterator().next();
-				String query = sparqlSection.getText();
+				Collection<TermCompiler> compilers = Compilers.getCompilers(section, TermCompiler.class);
+
+				TermCompiler termCompiler = null;
+				for (TermCompiler compiler : compilers) {
+					if (compiler instanceof Rdf2GoCompiler) {
+						termCompiler = compiler;
+						break;
+					}
+				}
+				if (termCompiler == null) {
+					return;
+				}
+
+				Identifier identifier = new Identifier("SPARQL", sparqlName);
+				Section<?> sparqlSection = termCompiler.getTerminologyManager()
+						.getTermDefiningSection(identifier);
+				if (sparqlSection == null) {
+					result.appendHtml("<span style='color:#C00000; background-color:#FFF6BF'>");
+					result.append("Sparql with name '" + sparqlName + "' not found.");
+					result.appendHtml("</span>");
+					return;
+				}
+				Section<SparqlContentType> contentSection = Sections.findSuccessor(sparqlSection, SparqlContentType.class);
+
+				String query = contentSection.getText();
 				OntologyCompiler compiler = Compilers.getCompiler(section, OntologyCompiler.class);
 
 				if (compiler != null) {
-					de.knowwe.rdf2go.Rdf2GoCore core = (de.knowwe.rdf2go.Rdf2GoCore) compiler.getRdf2GoCore();
+					Rdf2GoCore core = compiler.getRdf2GoCore();
 					query = Rdf2GoUtils.createSparqlString(core, query);
-					org.ontoware.rdf2go.model.QueryResultTable resultTable = (org.ontoware.rdf2go.model.QueryResultTable) core.sparqlSelect(query);
+					QueryResultTable resultTable = core.sparqlSelect(query);
 
 					ClosableIterator<QueryRow> rowIterator = resultTable.iterator();
 					List<String> variables = resultTable.getVariables();
 
 					String line = "";
-					String cell = "";
+					String cell;
 
 					while (rowIterator.hasNext()) {
 						QueryRow row = rowIterator.next();
