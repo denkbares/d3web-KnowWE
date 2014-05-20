8e74ddca21895e424cc93d432070848617ee3dee
Albrecht Striffler <albrecht.striffler@denkbares.com>
2014-05-20 09:37:45 +0000

    AttributeTable no longer renders escape ~ in intervalls.

--- KnowWE/KnowWE-Plugin-d3web-MarkupSet/src/main/java/de/d3web/we/kdom/abstractiontable/QuestionNumCell.java
+++ KnowWE/KnowWE-Plugin-d3web-MarkupSet/src/main/java/de/d3web/we/kdom/abstractiontable/QuestionNumCell.java
@@ -20,17 +20,24 @@ import de.d3web.strings.Strings;
 import de.knowwe.core.compile.PackageCompiler;
 import de.knowwe.core.kdom.AbstractType;
 import de.knowwe.core.kdom.parsing.Section;
+import de.knowwe.core.kdom.rendering.RenderResult;
 import de.knowwe.core.report.Messages;
+import de.knowwe.core.user.UserContext;
 import de.knowwe.kdom.renderer.StyleRenderer;
-import de.knowwe.kdom.renderer.StyleRenderer.MaskMode;
 
 public class QuestionNumCell extends AbstractType {
 
 	private static Pattern INTERVAL_PATTERN = Pattern.compile("(\\[|\\]|\\() *(\\d+\\.?\\d*) +(\\d+\\.?\\d*) *(\\[|\\]|\\))");
 
 	public QuestionNumCell() {
-		StyleRenderer renderer = new StyleRenderer("color:rgb(125, 80, 102)");
-		renderer.setMaskMode(MaskMode.htmlEntities);
+		StyleRenderer renderer = new StyleRenderer("color:rgb(125, 80, 102)") {
+
+			@Override
+			protected void renderContent(Section<?> section, UserContext user, RenderResult string) {
+				string.appendJSPWikiMarkup(section.getText().replace("~", ""));
+			}
+
+		};
 		this.setRenderer(renderer);
 	}
 
