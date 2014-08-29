9eb51808f1e2062dcf874b082f888a2b79b58ee9
Volker Belli <volker.belli@denkbares.com>
2014-08-29 06:12:29 +0000

    tools for selecting path from/to node selected node(s)

--- KnowWE/KnowWE-Plugin-DiaFlux/src/main/resources/webapp/cc/flow/edittools.js
+++ KnowWE/KnowWE-Plugin-DiaFlux/src/main/resources/webapp/cc/flow/edittools.js
@@ -42,44 +42,33 @@ FlowEditor.EditorToolMenu.prototype.hideMenu = function() {
 
 FlowEditor.EditorToolMenu.prototype.showMenu = function() {
 	var editor = this.flowEditor;
-	var html = "";
-	for (var i = 0; i < this.editTools.length; i++) {
-		var title = this.editTools[i].getTitle(editor);
-		if (!title) html += "<li>-</li>";
-		else {
-			var active = this.editTools[i].isActive(editor);
-			html += "<li" + (active ? "" : " class='ui-state-disabled'") +
-				" data-menuitem='" + this.editTools[i].getId() + "'"+
-				"><a href='#'>" +
-				title + "</a></li>";
-		}
-	}
-	html = "<div class='EditorToolMenu' style='position;absolute'>" +
+//	var html = "";
+//	for (var i = 0; i < this.editTools.length; i++) {
+//		var title = this.editTools[i].getTitle(editor);
+//		if (!title) html += "<li>-</li>";
+//		else {
+//			var active = this.editTools[i].isActive(editor);
+//			html += "<li" + (active ? "" : " class='ui-state-disabled'") +
+//				" data-menuitem='" + this.editTools[i].getId() + "'" +
+//				"><a href='#'>" +
+//				title + "</a></li>";
+//		}
+//	}
+	var html = jq$("<div class='EditorToolMenu' style='position;absolute'>" +
 		"<div class='closearea'></div>" +
-		"<div class='menuarea'><ul>" + html +
-		"</ul></div></div>";
+		"<div class='menuarea'><ul></ul></div></div>");
+	var ul = html.find("ul");
+	var self = this;
+	jq$.each(this.editTools, function(index, item) {
+		ul.append(item.render(self, editor));
+	});
 	jq$(this.menuButton).addClass('active');
 	jq$(this.menuButton).closest('body').append(html);
 	jq$('.EditorToolMenu').offset(jq$(this.menuButton).offset());
 	jq$('.EditorToolMenu .menuarea ul').menu({role : 'menuitem'});
-	var self = this;
-
 	jq$('.EditorToolMenu .closearea').click(function() {
-		self.hideMenu()
+		self.hideMenu();
 	});
-	jq$('.EditorToolMenu .menuarea ul li').click(function(event) {
-		var id = jq$(event.currentTarget).data("menuitem");
-		self._selectMenu(id);
-	});
-};
-
-FlowEditor.EditorToolMenu.prototype._selectMenu = function(menuItemId) {
-	for (var i = 0; i < this.editTools.length; i++) {
-		if (this.editTools[i].getId() == menuItemId) {
-			this.editTools[i].execute(this.flowEditor);
-			return;
-		}
-	}
 };
 
 FlowEditor.EditorToolMenu.prototype.initTools = function() {
@@ -102,11 +91,10 @@ FlowEditor.EditorToolMenu.prototype.initTools = function() {
 		return infoObject instanceof KBInfo.Flowchart;
 	};
 
-	var actionSelectPath = function(flowEditor) {
+	var selectPath = function(flowEditor, backwards, includeRules) {
 		var flow = flowEditor.getFlowchart();
 		var open = [];
-		for (var i = 0; i < flow.selection.length; i++) {
-			var item = flow.selection[i];
+		jq$.each(flow.selection, function(index, item) {
 			if (item instanceof Node) {
 				open.push(item);
 			}
@@ -114,28 +102,41 @@ FlowEditor.EditorToolMenu.prototype.initTools = function() {
 				open.push(item.getTargetNode());
 				flow.setSelection(item.getTargetNode(), true, false);
 			}
-		}
+		});
 		while (open.length > 0) {
-			var rules = open.pop().getOutgoingRules();
-			for (var i = 0; i < rules.length; i++) {
+			var rules = backwards ? open.pop().getIncomingRules() : open.pop().getOutgoingRules();
+			jq$.each(rules, function(index, rule) {
 				// select rule
-//				if (!flow.isSelected(rules[i])) {
-//					flow.setSelection(rules[i], true, false);
-//				}
-				// select target node
-				var next = rules[i].getTargetNode();
+				if (includeRules && !flow.isSelected(rule)) {
+					flow.setSelection(rule, true, false);
+				}
+				// select next node
+				var next = backwards ? rule.getSourceNode() : rule.getTargetNode();
 				if (!flow.isSelected(next)) {
 					flow.setSelection(next, true, false);
 					open.push(next);
 				}
-			}
+			});
 		}
 	};
 
+	var selectPathAfter = function(flowEditor) {
+		selectPath(flowEditor, false, false);
+	};
+
+	var selectPathBefore = function(flowEditor) {
+		selectPath(flowEditor, true, false);
+	};
+
 	this.editTools = [];
-	this.editTools.push(new FlowEditor.EditTool("Select Path", actionSelectPath, oneOrMore));
-	this.editTools.push(new FlowEditor.EditTool("Cleanup Flow", null, function() {return false;}));
-	this.editTools.push(new FlowEditor.EditTool(null, null, null));
+	this.editTools.push(new FlowEditor.EditToolGroup("Select", [
+		new FlowEditor.EditTool("Path after node", selectPathAfter, oneOrMore),
+		new FlowEditor.EditTool("Path to node", selectPathBefore, oneOrMore)
+	]));
+	this.editTools.push(new FlowEditor.EditTool("Cleanup Flow", null, function() {
+		return false;
+	}));
+	this.editTools.push(FlowEditor.EditTool.SEPARATOR);
 	this.editTools.push(new FlowEditor.EditTool("Unfold Subflow", null, oneComposed));
 	this.editTools.push(new FlowEditor.EditTool("Create Subflow", null, oneComposed));
 };
@@ -146,20 +147,32 @@ FlowEditor.EditorToolMenu.prototype.initTools = function() {
  */
 
 FlowEditor.EditTool = function(title, actionFun, isActiveFun) {
-	this.id = title ? (FlowEditor.EditTool.idCounter++) + "." + title.replace(/[^\w]+/g, "") : null;
 	this.title = title;
 	this.actionFun = actionFun;
 	this.isActiveFun = isActiveFun;
 };
 
-FlowEditor.EditTool.idCounter = 1;
+FlowEditor.EditTool.SEPARATOR = new FlowEditor.EditTool();
 
-FlowEditor.EditTool.prototype.getId = function() {
-	return this.id;
+FlowEditor.EditTool.prototype.getTitle = function() {
+	return this.title;
 };
 
-FlowEditor.EditTool.prototype.getTitle = function(flowEditor) {
-	return this.title;
+FlowEditor.EditTool.prototype.render = function(toolMenu, flowEditor) {
+	var title = this.getTitle();
+	if (!title) return jq$("<li>-</li>");
+
+	var self = this;
+	var li = jq$(document.createElement("li"))
+		.append(jq$(document.createElement("a")).text(title))
+		.click(function() {
+			self.execute(flowEditor);
+			toolMenu.hideMenu();
+		});
+	if (!this.isActive(flowEditor)) {
+		li.addClass("ui-state-disabled");
+	}
+	return li;
 };
 
 FlowEditor.EditTool.prototype.isActive = function(flowEditor) {
@@ -169,4 +182,43 @@ FlowEditor.EditTool.prototype.isActive = function(flowEditor) {
 FlowEditor.EditTool.prototype.execute = function(flowEditor) {
 	if (this.actionFun) this.actionFun(flowEditor);
 };
+
+/**
+ * Class for sub-menus of EditTools
+ */
+
+FlowEditor.EditToolGroup = function(title, menuItems) {
+	this.title = title;
+	this.menuItems = menuItems;
+};
+
+FlowEditor.EditToolGroup.prototype.getTitle = function() {
+	return this.title;
+};
+
+FlowEditor.EditToolGroup.prototype.render = function(toolMenu, flowEditor) {
+	var li = jq$(document.createElement("li"))
+		.append(jq$(document.createElement("a")).text(this.getTitle()))
+		.click(function() {
+			window.event.stop()
+		});
+	if (!this.isActive(flowEditor)) {
+		li.addClass("ui-state-disabled");
+	}
+	var subList = jq$(document.createElement("ul")).appendTo(li);
+	jq$.each(this.menuItems, function(index, item) {
+		subList.append(item.render(toolMenu, flowEditor));
+	});
+	return li;
+};
+
+FlowEditor.EditToolGroup.prototype.isActive = function(flowEditor) {
+	var anyActive = false;
+	jq$.each(this.menuItems, function(index, item) {
+		anyActive |= item.isActive(flowEditor);
+		return !anyActive;
+	});
+	return anyActive;
+};
+
 
