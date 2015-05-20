87e41ce867e6274fa00ded9e296f3895a90eed96
Jochen Reutelshöfer <jochen.reutelshoefer@denkbares.com>
2015-05-20 14:51:33 +0000

    OntologyTable allows for non-trivial blanknodes in Object cells

--- KnowWE/KnowWE-Plugin-Ontology-MarkupSet/src/main/java/de/knowwe/ontology/kdom/table/LineHandler.java
+++ KnowWE/KnowWE-Plugin-Ontology-MarkupSet/src/main/java/de/knowwe/ontology/kdom/table/LineHandler.java
@@ -18,29 +18,26 @@
  */
 package de.knowwe.ontology.kdom.table;
 
-import java.util.LinkedList;
-import java.util.List;
-
-import de.knowwe.kdom.table.TableCellContent;
-import de.knowwe.ontology.turtle.Predicate;
-import de.knowwe.ontology.turtle.compile.NodeProvider;
-import org.ontoware.rdf2go.model.Statement;
-import org.ontoware.rdf2go.model.node.Node;
-import org.ontoware.rdf2go.model.node.URI;
-
 import de.knowwe.core.kdom.parsing.Section;
 import de.knowwe.core.kdom.parsing.Sections;
 import de.knowwe.core.report.CompilerMessage;
 import de.knowwe.core.report.Messages;
+import de.knowwe.kdom.table.TableCellContent;
 import de.knowwe.kdom.table.TableLine;
 import de.knowwe.kdom.table.TableUtils;
 import de.knowwe.ontology.compile.OntologyCompileScript;
 import de.knowwe.ontology.compile.OntologyCompiler;
-import de.knowwe.ontology.kdom.objectproperty.AbbreviatedPropertyReference;
-import de.knowwe.ontology.kdom.resource.AbbreviatedResourceReference;
 import de.knowwe.ontology.turtle.Object;
+import de.knowwe.ontology.turtle.Predicate;
+import de.knowwe.ontology.turtle.compile.NodeProvider;
+import de.knowwe.ontology.turtle.compile.StatementProviderResult;
 import de.knowwe.rdf2go.Rdf2GoCore;
-import org.openrdf.model.Resource;
+import org.ontoware.rdf2go.model.Statement;
+import org.ontoware.rdf2go.model.node.Node;
+import org.ontoware.rdf2go.model.node.URI;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * @author Sebastian Furth (denkbares GmbH)
@@ -48,45 +45,50 @@ import org.openrdf.model.Resource;
  */
 public class LineHandler extends OntologyCompileScript<TableLine> {
 
-	@Override
-	public void compile(OntologyCompiler compiler, Section<TableLine> section) throws CompilerMessage {
+    @Override
+    public void compile(OntologyCompiler compiler, Section<TableLine> section) throws CompilerMessage {
 
-		if (TableUtils.isHeaderRow(section)) {
-			Messages.clearMessages(compiler, section, getClass());
-			return;
-		}
+        if (TableUtils.isHeaderRow(section)) {
+            Messages.clearMessages(compiler, section, getClass());
+            return;
+        }
 
-		Rdf2GoCore core = compiler.getRdf2GoCore();
-		List<Statement> statements = new LinkedList<>();
-		Section<NodeProvider> subjectReference = findSubject(section);
-		if(subjectReference == null) {
-			// obviously no subject in this line, could be an empty table line
-			return;
-		}
-		Node subjectNode = subjectReference.get().getNode(subjectReference, compiler);
-		List<Section<Object>> objects = findObjects(section);
-		for (Section<Object> objectReference : objects) {
-			Section<Predicate> propertyReference = TableUtils.getColumnHeader(objectReference, Predicate.class);
-			URI propertyUri = propertyReference.get().getNode(propertyReference, compiler).asURI();
-			Node objectNode = objectReference.get().getNode(objectReference, compiler);
-			statements.add(core.createStatement(subjectNode.asResource(), propertyUri, objectNode));
-		}
+        Rdf2GoCore core = compiler.getRdf2GoCore();
+        List<Statement> statements = new LinkedList<>();
+        Section<NodeProvider> subjectReference = findSubject(section);
+        if (subjectReference == null) {
+            // obviously no subject in this line, could be an empty table line
+            return;
+        }
+        Node subjectNode = subjectReference.get().getNode(subjectReference, compiler);
+        List<Section<Object>> objects = findObjects(section);
+        for (Section<Object> objectReference : objects) {
+            if (objectReference.getParent().getParent().get() instanceof TableCellContent) {
+                Section<Predicate> propertyReference = TableUtils.getColumnHeader(objectReference, Predicate.class);
+                URI propertyUri = propertyReference.get().getNode(propertyReference, compiler).asURI();
+                Node objectNode = objectReference.get().getNode(objectReference, compiler);
+                statements.add(core.createStatement(subjectNode.asResource(), propertyUri, objectNode));
+            } else {
+                final StatementProviderResult statementProviderResult = objectReference.get().getStatements(objectReference, compiler);
+                statements.addAll(statementProviderResult.getStatments());
+            }
+        }
 
-		core.addStatements(section, statements);
-	}
+        core.addStatements(section, statements);
+    }
 
-	private List<Section<Object>> findObjects(Section<TableLine> section) {
-		return Sections.successors(section, Object.class);
-	}
+    private List<Section<Object>> findObjects(Section<TableLine> section) {
+        return Sections.successors(section, Object.class);
+    }
 
-	private Section<NodeProvider> findSubject(Section<TableLine> section) {
-		final Section<TableCellContent> firstCell = Sections.successor(section, TableCellContent.class);
-		return Sections.successor(firstCell, NodeProvider.class);
-	}
+    private Section<NodeProvider> findSubject(Section<TableLine> section) {
+        final Section<TableCellContent> firstCell = Sections.successor(section, TableCellContent.class);
+        return Sections.successor(firstCell, NodeProvider.class);
+    }
 
-	@Override
-	public void destroy(OntologyCompiler compiler, Section<TableLine> section) {
-		Rdf2GoCore core = compiler.getRdf2GoCore();
-		core.removeStatements(section);
-	}
+    @Override
+    public void destroy(OntologyCompiler compiler, Section<TableLine> section) {
+        Rdf2GoCore core = compiler.getRdf2GoCore();
+        core.removeStatements(section);
+    }
 }
