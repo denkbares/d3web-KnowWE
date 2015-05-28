3dfc65499bc5b797656cb052986d60fdabade643
Albrecht Striffler <albrecht.striffler@denkbares.com>
2015-05-28 07:33:16 +0000

    Another attempt to make UI tests more stable.

--- KnowWE/KnowWE-UITests/src/test/java/de/knowwe/uitest/DiaFluxSystemTest.java
+++ KnowWE/KnowWE-UITests/src/test/java/de/knowwe/uitest/DiaFluxSystemTest.java
@@ -25,7 +25,11 @@ import java.util.List;
 import java.util.Set;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
@@ -75,6 +79,44 @@ public abstract class DiaFluxSystemTest {
 			logIn();
 		}
 	}
+
+	public class Retry implements TestRule {
+
+		private int retryCount;
+
+		public Retry(int retryCount) {
+			this.retryCount = retryCount;
+		}
+
+		public Statement apply(Statement base, Description description) {
+			return statement(base, description);
+		}
+
+		private Statement statement(final Statement base, final Description description) {
+			return new Statement() {
+				@Override
+				public void evaluate() throws Throwable {
+					Throwable caughtThrowable = null;
+
+					// implement retry logic here
+					for (int i = 0; i < retryCount; i++) {
+						try {
+							base.evaluate();
+							return;
+						} catch (Throwable t) {
+							caughtThrowable = t;
+							System.err.println(description.getDisplayName() + ": run " + (i+1) + " failed");
+						}
+					}
+					System.err.println(description.getDisplayName() + ": giving up after " + retryCount + " failures");
+					throw caughtThrowable;
+				}
+			};
+		}
+	}
+
+	@Rule
+	public Retry retry = new Retry(3);
 
 	private void logIn() {
 		List<WebElement> elements = getDriver().findElements(By.cssSelector("a.action.login"));
