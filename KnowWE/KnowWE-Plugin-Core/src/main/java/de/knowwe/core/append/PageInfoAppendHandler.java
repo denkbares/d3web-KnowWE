fb17ea43463655fbe881e7635b67731f3ac8e2f6
Albrecht Striffler <albrecht.striffler@denkbares.com>
2015-06-12 09:15:35 +0000

    Adding some infos about user rights to page info to allow for render adjustments.

--- KnowWE/KnowWE-Plugin-Core/src/main/java/de/knowwe/core/append/PageInfoAppendHandler.java
+++ KnowWE/KnowWE-Plugin-Core/src/main/java/de/knowwe/core/append/PageInfoAppendHandler.java
@@ -20,6 +20,7 @@ package de.knowwe.core.append;
 
 import de.d3web.strings.Strings;
 import de.knowwe.core.Environment;
+import de.knowwe.core.kdom.Article;
 import de.knowwe.core.kdom.rendering.RenderResult;
 import de.knowwe.core.user.UserContext;
 import de.knowwe.core.utils.KnowWEUtils;
@@ -37,20 +38,26 @@ public class PageInfoAppendHandler implements PageAppendHandler {
 	@Override
 	public void append(String web, String title, UserContext user, RenderResult html) {
 		WikiConnector connector = Environment.getInstance().getWikiConnector();
+		Article article = KnowWEUtils.getArticle(web, title);
 		int version = connector.getVersion(title);
 		long modDate = connector.getLastModifiedDate(title, -1).getTime();
 		String userName = user.getUserName();
 		String overallStatus = KnowWEUtils.getOverallStatus(user);
 
 		html.appendHtml("<input type='hidden' id='knowWEInfoWeb' value='" + web + "'>");
-		html.appendHtml("<input type='hidden' id='knowWEInfoPageName' value="
-				+ Strings.quote(title) + ">");
+		html.appendHtml("<input type='hidden' id='knowWEInfoPageName' value=" + Strings.quote(title) + ">");
 		html.appendHtml("<input type='hidden' id='knowWEInfoPageVersion' value='" + version + "'>");
 		html.appendHtml("<input type='hidden' id='knowWEInfoPageDate' value='" + modDate + "'>");
 		html.appendHtml("<input type='hidden' id='knowWEInfoUser' value=" + Strings.quote(userName) + ">");
 		html.appendHtml("<input type='hidden' id='knowWEInfoLanguage' value='" + connector.getLocale(user.getRequest())
 				.getLanguage() + "'>");
 		html.appendHtml("<input type='hidden' id='knowWEInfoStatus' value='" + overallStatus + "'>");
+		html.appendHtml("<div style='display:none' comment='The following infos can be edited by the user any time, "
+				+ "so it should only be used for superficial rendering/styling, not as a security check!'>");
+		html.appendHtml("<input type='hidden' id='knowWEInfoAdmin' value='" + KnowWEUtils.isAdmin(user) + "'>");
+		html.appendHtml("<input type='hidden' id='knowWEInfoCanWrite' value='" + KnowWEUtils.canWrite(article, user) + "'>");
+		html.appendHtml("<input type='hidden' id='knowWEInfoCanView' value='" + KnowWEUtils.canView(article, user) + "'>");
+		html.appendHtml("</div>");
 	}
 
 	@Override
