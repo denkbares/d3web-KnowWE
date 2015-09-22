9699f33a901fa9de69ccd4d5e17bfc81c6beac07
Albrecht Striffler <albrecht.striffler@denkbares.com>
2015-09-22 10:59:57 +0000

    Update jquery cookie plugin to 1.4.1. It now provides a new method jq$.removeCookie(name)...

--- KnowWE/KnowWE-Resources/src/main/webapp/KnowWEExtension/scripts/jquery-plugin-collection.js
+++ KnowWE/KnowWE-Resources/src/main/webapp/KnowWEExtension/scripts/jquery-plugin-collection.js
@@ -145,61 +145,123 @@
  * External Plugins below
  */
 
-(function(jq$) {
-	jq$.cookie = function(key, value, options) {
+/*!
+ * jQuery Cookie Plugin v1.4.1
+ * https://github.com/carhartl/jquery-cookie
+ *
+ * Copyright 2013 Klaus Hartl
+ * Released under the MIT license
+ */
+(function (factory) {
+	if (typeof define === 'function' && define.amd) {
+		// AMD
+		define(['jquery'], factory);
+	} else if (typeof exports === 'object') {
+		// CommonJS
+		factory(require('jquery'));
+	} else {
+		// Browser globals
+		factory(jQuery);
+	}
+}(function (jq$) {
 
-		// key and at least value given, set cookie...
-		if (arguments.length > 1 && (!/Object/.test(Object.prototype.toString.call(value)) || value === null || value === undefined)) {
-			options = jq$.extend({}, options);
+	var pluses = /\+/g;
 
-			if (value === null || value === undefined) {
-				options.expires = -1;
-			}
+	function encode(s) {
+		return config.raw ? s : encodeURIComponent(s);
+	}
+
+	function decode(s) {
+		return config.raw ? s : decodeURIComponent(s);
+	}
+
+	function stringifyCookieValue(value) {
+		return encode(config.json ? JSON.stringify(value) : String(value));
+	}
+
+	function parseCookieValue(s) {
+		if (s.indexOf('"') === 0) {
+			// This is a quoted cookie as according to RFC2068, unescape...
+			s = s.slice(1, -1).replace(/\\"/g, '"').replace(/\\\\/g, '\\');
+		}
+
+		try {
+			// Replace server-side written pluses with spaces.
+			// If we can't decode the cookie, ignore it, it's unusable.
+			// If we can't parse the cookie, ignore it, it's unusable.
+			s = decodeURIComponent(s.replace(pluses, ' '));
+			return config.json ? JSON.parse(s) : s;
+		} catch(e) {}
+	}
+
+	function read(s, converter) {
+		var value = config.raw ? s : parseCookieValue(s);
+		return jq$.isFunction(converter) ? converter(value) : value;
+	}
+
+	var config = jq$.cookie = function (key, value, options) {
+
+		// Write
+
+		if (value !== undefined && !jq$.isFunction(value)) {
+			options = jq$.extend({}, config.defaults, options);
 
 			if (typeof options.expires === 'number') {
 				var days = options.expires, t = options.expires = new Date();
-				t.setDate(t.getDate() + days);
+				t.setTime(+t + days * 864e+5);
 			}
 
-			value = String(value);
-
 			return (document.cookie = [
-				encodeURIComponent(key), '=', options.raw ? value : encodeURIComponent(value),
+				encode(key), '=', stringifyCookieValue(value),
 				options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
-				options.path ? '; path=' + options.path : '',
-				options.domain ? '; domain=' + options.domain : '',
-				options.secure ? '; secure' : ''
+				options.path    ? '; path=' + options.path : '',
+				options.domain  ? '; domain=' + options.domain : '',
+				options.secure  ? '; secure' : ''
 			].join(''));
 		}
 
-		// key and possibly options given, get cookie...
-		options = value || {};
-		var decode = options.raw ? function(s) {
-			return s;
-		} : decodeURIComponent;
+		// Read
 
-		var pairs = document.cookie.split('; ');
-		for (var i = 0, pair; pair = pairs[i] && pairs[i].split('='); i++) {
-			if (decode(pair[0]) === key) return decode(pair[1] || ''); // IE saves cookies with empty string as "c; ", e.g. without "=" as opposed to EOMB, thus pair[1] may be undefined
-		}
-		return null;
-	};
-	jq$.extend({
-		getUrlVars : function() {
-			var vars = [], hash;
-			var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-			for (var i = 0; i < hashes.length; i++) {
-				hash = hashes[i].split('=');
-				vars.push(hash[0]);
-				vars[hash[0]] = hash[1];
+		var result = key ? undefined : {};
+
+		// To prevent the for loop in the first place assign an empty array
+		// in case there are no cookies at all. Also prevents odd result when
+		// calling jq$.cookie().
+		var cookies = document.cookie ? document.cookie.split('; ') : [];
+
+		for (var i = 0, l = cookies.length; i < l; i++) {
+			var parts = cookies[i].split('=');
+			var name = decode(parts.shift());
+			var cookie = parts.join('=');
+
+			if (key && key === name) {
+				// If second argument (value) is a function it's a converter...
+				result = read(cookie, value);
+				break;
 			}
-			return vars;
-		},
-		getUrlVar : function(name) {
-			return $.getUrlVars()[name];
+
+			// Prevent storing a cookie that we couldn't decode.
+			if (!key && (cookie = read(cookie)) !== undefined) {
+				result[name] = cookie;
+			}
 		}
-	});
-})(jQuery);
+
+		return result;
+	};
+
+	config.defaults = {};
+
+	jq$.removeCookie = function (key, options) {
+		if (jq$.cookie(key) === undefined) {
+			return false;
+		}
+
+		// Must not alter options, thus extending a fresh object...
+		jq$.cookie(key, '', jq$.extend({}, options, { expires: -1 }));
+		return !jq$.cookie(key);
+	};
+
+}));
 
 
 (function(jq$) {
