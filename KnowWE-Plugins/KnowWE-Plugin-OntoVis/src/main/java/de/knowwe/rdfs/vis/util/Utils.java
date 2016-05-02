3946f7391687df0dfa3d3b0947875ba0c6844edb
Volker Belli <vb@denkbares.com>
2016-05-02 09:46:18 +0200

    fixed imports

--- KnowWE-Plugins/KnowWE-Plugin-OntoVis/src/main/java/de/knowwe/rdfs/vis/util/Utils.java
+++ KnowWE-Plugins/KnowWE-Plugin-OntoVis/src/main/java/de/knowwe/rdfs/vis/util/Utils.java
@@ -24,7 +24,6 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import org.jetbrains.annotations.Nullable;
 import org.ontoware.rdf2go.model.QueryResultTable;
