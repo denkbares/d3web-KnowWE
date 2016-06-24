28cea2e0b21e97a259c34bab0b80a27b3178785d
Albrecht Striffler <albrecht.striffler@denkbares.com>
2016-06-24 10:12:16 +0200

    Error handling and other minor improvements to GetSectionTextAction.

--- KnowWE-Essentials/KnowWE-Plugin-Core/src/main/java/de/knowwe/core/action/GetSectionTextAction.java
+++ KnowWE-Essentials/KnowWE-Plugin-Core/src/main/java/de/knowwe/core/action/GetSectionTextAction.java
@@ -5,17 +5,20 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
+import javax.servlet.http.HttpServletResponse;
+
 import de.d3web.strings.Strings;
 import de.d3web.utils.Streams;
 import de.knowwe.core.Attributes;
 import de.knowwe.core.kdom.parsing.Section;
+import de.knowwe.core.kdom.parsing.Sections;
 import de.knowwe.core.utils.KnowWEUtils;
 import de.knowwe.kdom.defaultMarkup.DefaultMarkupType;
 
 import static de.knowwe.core.kdom.parsing.Sections.$;
 
 /**
- * Returns the text of a specified section.
+ * Returns the text of a specified section as a text file.
  *
  * @author Albrecht Striffler (denkbares GmbH)
  * @created 23.06.16
@@ -27,40 +30,52 @@ public class GetSectionTextAction extends AbstractAction {
 
 		String sectionText = null;
 		String title = context.getParameter("title");
-		String name = context.getParameter("name");
+		if (title == null) title = context.getParameter("article");
+		final String name = context.getParameter("name");
+		String sectionId = context.getParameter(Attributes.SECTION_ID);
+		String fileName = null;
 
-		if (context.getParameter(Attributes.SECTION_ID) != null) {
-			Section<?> section = getSection(context);
-			sectionText = section.getText();
-			name = section.getID();
+		Section<?> referencedSection = null;
+		if (sectionId != null) {
+			referencedSection = Sections.get(sectionId);
+			fileName = referencedSection.getID();
 		}
 		else if (title != null && name != null) {
-			final String finalName = name;
-			Section<DefaultMarkupType> namedSection = $(context.getArticleManager()
+			referencedSection = $(context.getArticleManager()
 					.getArticle(title)).successor(DefaultMarkupType.class)
-					.filter(markupSection -> finalName.equals(DefaultMarkupType.getAnnotation(markupSection, "name")))
+					.filter(markupSection -> name.equals(DefaultMarkupType.getAnnotation(markupSection, "name")))
 					.getFirst();
-			if (namedSection != null && KnowWEUtils.canView(namedSection, context)) {
-				sectionText = namedSection.getText();
-			}
+			fileName = name;
 		}
 
-		if (sectionText != null) {
-			File tempTextFile = File.createTempFile(name, "_SectionText.txt");
-			try {
-				Strings.writeFile(tempTextFile.getPath(), sectionText);
-
-				context.setContentType("application/x-bin");
-				context.setHeader("Content-Disposition", "attachment;filename=\"" + name + ".txt\"");
-
-				FileInputStream in = new FileInputStream(tempTextFile);
-				OutputStream out = context.getOutputStream();
-				Streams.streamAndClose(in, out);
+		if (referencedSection != null) {
+			if (KnowWEUtils.canView(referencedSection, context)) {
+				sectionText = referencedSection.getText();
 			}
-			finally {
-				tempTextFile.delete();
-				tempTextFile.deleteOnExit();
+			else {
+				context.sendError(HttpServletResponse.SC_FORBIDDEN, "Not authorized to view/download section");
+				return;
 			}
+		}
+		else {
+			context.sendError(HttpServletResponse.SC_NOT_FOUND, "No valid section id or title and name given");
+			return;
+		}
+
+		File tempTextFile = File.createTempFile(name, "_SectionText.txt");
+		try {
+			Strings.writeFile(tempTextFile.getPath(), sectionText);
+
+			context.setContentType("application/x-bin");
+			context.setHeader("Content-Disposition", "attachment;filename=\"" + fileName + ".txt\"");
+
+			FileInputStream in = new FileInputStream(tempTextFile);
+			OutputStream out = context.getOutputStream();
+			Streams.streamAndClose(in, out);
+		}
+		finally {
+			tempTextFile.delete();
+			tempTextFile.deleteOnExit();
 		}
 	}
 }
