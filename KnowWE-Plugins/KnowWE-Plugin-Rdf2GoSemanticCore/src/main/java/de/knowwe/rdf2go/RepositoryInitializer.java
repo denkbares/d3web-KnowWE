275a0a685064a54e59170b355e88c34ea7d2037e
Albrecht Striffler <albrecht.striffler@denkbares.com>
2016-06-24 16:12:19 +0200

    KnowWE SemanticCore now makes sure that each KnowWE web app creates its own repository directory.

---
+++ KnowWE-Plugins/KnowWE-Plugin-Rdf2GoSemanticCore/src/main/java/de/knowwe/rdf2go/RepositoryInitializer.java
@@ -0,0 +1,28 @@
+package de.knowwe.rdf2go;
+
+import java.io.IOException;
+
+import com.denkbares.semanticcore.SemanticCore;
+import de.d3web.utils.Log;
+import de.knowwe.core.Environment;
+import de.knowwe.plugin.Instantiation;
+
+/**
+ * Instantiation to initialize the SemanticCore with a proper temp directory.
+ *
+ * @author Albrecht Striffler (denkbares GmbH)
+ * @created 24.06.16
+ */
+public class RepositoryInitializer implements Instantiation {
+
+	@Override
+	public void init(String web) {
+		String context = Environment.getInstance().getWikiConnector().getRealPath().replaceAll("\\W", "");
+		try {
+			SemanticCore.initializeRepositoryManager(SemanticCore.createRepositoryPath(context));
+		}
+		catch (IOException e) {
+			Log.severe("Unable to initialize repository for SemanticCore!", e);
+		}
+	}
+}
