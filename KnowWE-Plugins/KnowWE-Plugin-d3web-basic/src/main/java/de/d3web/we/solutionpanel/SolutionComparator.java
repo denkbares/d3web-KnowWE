509b4cc270e1c2a01a4486c502c820f36af9ba23
Albrecht Striffler <albrecht.striffler@denkbares.com>
2017-07-18 17:19:35 +0200

    Solutions in %%ShowSolutions are now sorted by occurrence in object tree, in case score is the same. Before, they were sorted by score and name.

--- KnowWE-Plugins/KnowWE-Plugin-d3web-basic/src/main/java/de/d3web/we/solutionpanel/SolutionComparator.java
+++ KnowWE-Plugins/KnowWE-Plugin-d3web-basic/src/main/java/de/d3web/we/solutionpanel/SolutionComparator.java
@@ -1,9 +1,12 @@
 package de.d3web.we.solutionpanel;
 
 import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
 
 import de.d3web.core.knowledge.terminology.Rating;
 import de.d3web.core.knowledge.terminology.Solution;
+import de.d3web.core.manage.KnowledgeBaseUtils;
 import de.d3web.core.session.Session;
 
 public class SolutionComparator implements Comparator<Solution> {
@@ -19,7 +22,19 @@ public class SolutionComparator implements Comparator<Solution> {
 		Rating rating1 = session.getBlackboard().getRating(o1);
 		Rating rating2 = session.getBlackboard().getRating(o2);
 		int comparison = rating2.compareTo(rating1);
-		if (comparison == 0) return o1.getName().compareTo(o2.getName());
+		if (comparison == 0) {
+			List<Integer> thisPos = KnowledgeBaseUtils.getPositionInTree(o1);
+			Iterator<Integer> thisIter = thisPos.iterator();
+			List<Integer> otherPos = KnowledgeBaseUtils.getPositionInTree(o2);
+			Iterator<Integer> otherIter = otherPos.iterator();
+
+			while (comparison == 0 && thisIter.hasNext() && otherIter.hasNext()) {
+				comparison = thisIter.next().compareTo(otherIter.next());
+			}
+			if (comparison == 0) {
+				comparison = thisPos.size() - otherPos.size();
+			}
+		}
 		return comparison;
 	}
 }
