7cac12b6900ae77816f467dee51e4cfee38309af
Albrecht Striffler <albrecht.striffler@denkbares.com>
2017-07-19 08:58:14 +0200

    Simplified (and speed up) SolutionComparator.

--- KnowWE-Plugins/KnowWE-Plugin-d3web-basic/src/main/java/de/d3web/we/solutionpanel/SolutionComparator.java
+++ KnowWE-Plugins/KnowWE-Plugin-d3web-basic/src/main/java/de/d3web/we/solutionpanel/SolutionComparator.java
@@ -1,12 +1,9 @@
 package de.d3web.we.solutionpanel;
 
 import java.util.Comparator;
-import java.util.Iterator;
-import java.util.List;
 
 import de.d3web.core.knowledge.terminology.Rating;
 import de.d3web.core.knowledge.terminology.Solution;
-import de.d3web.core.manage.KnowledgeBaseUtils;
 import de.d3web.core.session.Session;
 
 public class SolutionComparator implements Comparator<Solution> {
@@ -23,17 +20,7 @@ public class SolutionComparator implements Comparator<Solution> {
 		Rating rating2 = session.getBlackboard().getRating(o2);
 		int comparison = rating2.compareTo(rating1);
 		if (comparison == 0) {
-			List<Integer> thisPos = KnowledgeBaseUtils.getPositionInTree(o1);
-			Iterator<Integer> thisIter = thisPos.iterator();
-			List<Integer> otherPos = KnowledgeBaseUtils.getPositionInTree(o2);
-			Iterator<Integer> otherIter = otherPos.iterator();
-
-			while (comparison == 0 && thisIter.hasNext() && otherIter.hasNext()) {
-				comparison = thisIter.next().compareTo(otherIter.next());
-			}
-			if (comparison == 0) {
-				comparison = thisPos.size() - otherPos.size();
-			}
+			comparison = Integer.compare(session.getKnowledgeBase().getManager().getTreeIndex(o1), session.getKnowledgeBase().getManager().getTreeIndex(o2));
 		}
 		return comparison;
 	}
