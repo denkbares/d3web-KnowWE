04d52a4c7f5920285bc6fb09be2178fcbafcd55c
Albrecht Striffler <albrecht.striffler@denkbares.com>
2017-08-21 15:30:10 +0200

    Fixed potential minor memory leak.

--- KnowWE-Plugins/KnowWE-Plugin-Include/src/main/java/de/knowwe/include/GetWikiSectionTextAction.java
+++ KnowWE-Plugins/KnowWE-Plugin-Include/src/main/java/de/knowwe/include/GetWikiSectionTextAction.java
@@ -37,35 +37,39 @@ public class GetWikiSectionTextAction extends GetSectionTextAction {
 		}
 
 		Article auxiliaryArticle = Article.createArticle("%%include\n[" + wikiReference + "]\n%", "AuxArticle", "AuxWeb");
-		Section<WikiReference> wikiReferenceSection = Sections.successor(auxiliaryArticle.getRootSection(), WikiReference.class);
-		if (wikiReferenceSection == null) {
-			context.sendError(HttpServletResponse.SC_NOT_FOUND, "Invalid reference: " + wikiReference);
-			return;
-		}
+		try {
 
-		Section<?> headerSection = WikiReference.findReferencedSection(wikiReferenceSection, KnowWEUtils.getArticleManager(Environment.DEFAULT_WEB));
-		if (headerSection != null && headerSection.get() instanceof HeaderType) {
-			List<Section<? extends Type>> sectionList = JSPWikiMarkupUtils.getContent(Sections.cast(headerSection, HeaderType.class));
-			StringBuilder sectionText = new StringBuilder();
-			Instant lastModified = Environment.getInstance()
-					.getWikiConnector()
-					.getLastModifiedDate(headerSection.getTitle(), -1)
-					.toInstant();
-			for (Section<? extends Type> subSection : sectionList) {
-				if (KnowWEUtils.canView(subSection, context)) {
-					sectionText.append(subSection.getText());
-				}
-				else {
-					context.sendError(HttpServletResponse.SC_FORBIDDEN, "Not authorized to view/download section");
-					return;
-				}
+			Section<WikiReference> wikiReferenceSection = Sections.successor(auxiliaryArticle.getRootSection(), WikiReference.class);
+			if (wikiReferenceSection == null) {
+				context.sendError(HttpServletResponse.SC_NOT_FOUND, "Invalid reference: " + wikiReference);
+				return;
 			}
-			writeFile(context, sectionText.toString(), wikiReference, lastModified);
-		}
-		else {
-			context.sendError(HttpServletResponse.SC_NOT_FOUND, "Reference not found: " + wikiReference);
-		}
 
+			Section<?> headerSection = WikiReference.findReferencedSection(wikiReferenceSection, KnowWEUtils.getArticleManager(Environment.DEFAULT_WEB));
+			if (headerSection != null && headerSection.get() instanceof HeaderType) {
+				List<Section<? extends Type>> sectionList = JSPWikiMarkupUtils.getContent(Sections.cast(headerSection, HeaderType.class));
+				StringBuilder sectionText = new StringBuilder();
+				Instant lastModified = Environment.getInstance()
+						.getWikiConnector()
+						.getLastModifiedDate(headerSection.getTitle(), -1)
+						.toInstant();
+				for (Section<? extends Type> subSection : sectionList) {
+					if (KnowWEUtils.canView(subSection, context)) {
+						sectionText.append(subSection.getText());
+					}
+					else {
+						context.sendError(HttpServletResponse.SC_FORBIDDEN, "Not authorized to view/download section");
+						return;
+					}
+				}
+				writeFile(context, sectionText.toString(), wikiReference, lastModified);
+			}
+			else {
+				context.sendError(HttpServletResponse.SC_NOT_FOUND, "Reference not found: " + wikiReference);
+			}
+		}
+		finally {
+			auxiliaryArticle.destroy(null);
+		}
 	}
-
 }
