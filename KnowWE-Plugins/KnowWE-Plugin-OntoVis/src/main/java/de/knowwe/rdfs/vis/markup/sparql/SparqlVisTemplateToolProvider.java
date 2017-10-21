e581399d6e7a64d59e1f04f339a6b3afa5dcb1c9
jochenreutelshofer <jochen.reutelshoefer@denkbares.com>
2017-10-21 18:38:37 +0200

    spreading some existing tool functionality to more existing term reference types; fixed SparqlVisTemplateToolProvider to be able to handle multiple templates for a concept

--- KnowWE-Plugins/KnowWE-Plugin-OntoVis/src/main/java/de/knowwe/rdfs/vis/markup/sparql/SparqlVisTemplateToolProvider.java
+++ KnowWE-Plugins/KnowWE-Plugin-OntoVis/src/main/java/de/knowwe/rdfs/vis/markup/sparql/SparqlVisTemplateToolProvider.java
@@ -2,14 +2,17 @@ package de.knowwe.rdfs.vis.markup.sparql;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.openrdf.model.URI;
 
 import com.denkbares.collections.PartialHierarchyTree;
 import com.denkbares.strings.Identifier;
+import com.denkbares.utils.Log;
 import de.knowwe.core.kdom.objects.Term;
 import de.knowwe.core.kdom.parsing.Section;
 import de.knowwe.core.kdom.parsing.Sections;
@@ -33,31 +36,36 @@ public class SparqlVisTemplateToolProvider implements ToolProvider {
 
 	@Override
 	public Tool[] getTools(Section<?> section, UserContext userContext) {
-		Section<SparqlVisualizationType> templateSection = findApplicableTemplate(section);
-		if (templateSection == null) return new Tool[] {};
-		String templateClass = DefaultMarkupType.getAnnotation(templateSection, SparqlVisualizationType.VIS_TEMPLATE_CLASS);
-		if (templateClass == null) return new Tool[] {};
+		List<Section<SparqlVisualizationType>> templateSections = findApplicableTemplate(section);
+		assert templateSections != null;
+		if (templateSections.isEmpty()) return new Tool[] {};
+		List<Tool> tools = new ArrayList<>();
+		for (Section<SparqlVisualizationType> templateSection : templateSections) {
+			String templateClass = DefaultMarkupType.getAnnotation(templateSection, SparqlVisualizationType.VIS_TEMPLATE_CLASS);
+			if (templateClass == null) return new Tool[] {};
 
-		URI uri = getURI(section);
-		OntologyCompiler compiler = OntologyUtils
-				.getOntologyCompiler(section);
-		if (compiler == null || uri == null) return new Tool[] {};
+			URI uri = getURI(section);
+			OntologyCompiler compiler = OntologyUtils
+					.getOntologyCompiler(section);
+			if (compiler == null || uri == null) return new Tool[] {};
 
-		String reducedConceptURI = Rdf2GoUtils.reduceNamespace(compiler.getRdf2GoCore(), uri.toString());
-		String link = KnowWEUtils.getURLLink(templateSection.getTitle());
+			String reducedConceptURI = Rdf2GoUtils.reduceNamespace(compiler.getRdf2GoCore(), uri.toString());
+			String link = KnowWEUtils.getURLLink(templateSection.getTitle());
 
-		try {
-			String conceptParameterAppendix = "&concept=" + URLEncoder.encode(reducedConceptURI, "UTF-8");
-			link += conceptParameterAppendix;
+			try {
+				String conceptParameterAppendix = "&concept=" + URLEncoder.encode(reducedConceptURI, "UTF-8");
+				link += conceptParameterAppendix;
+			}
+			catch (UnsupportedEncodingException e) {
+				Log.severe("problem encoding vis template link", e);
+			}
+
+			tools.add(new DefaultTool(
+					Icon.SHOWTRACE,
+					"Visualize with SparqlVis template '" + templateClass + "'", "Open SparqlVis template for class " + templateClass,
+					link, Tool.ActionType.HREF, Tool.CATEGORY_INFO));
 		}
-		catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
-
-		return new Tool[] { new DefaultTool(
-				Icon.SHOWTRACE,
-				"Visualize with SparqlVis template '" + templateClass + "'", "Open SparqlVis template for class " + templateClass,
-				link, Tool.ActionType.HREF, Tool.CATEGORY_INFO) };
+		return tools.toArray(new Tool[tools.size()]);
 	}
 
 	@Override
@@ -77,22 +85,23 @@ public class SparqlVisTemplateToolProvider implements ToolProvider {
 		return null;
 	}
 
-	private Section<SparqlVisualizationType> findApplicableTemplate(Section<?> section) {
+	private List<Section<SparqlVisualizationType>> findApplicableTemplate(Section<?> section) {
 		Map<String, Section<SparqlVisualizationType>> templates = getClassVisTemplates(section);
 		OntologyCompiler compiler = OntologyUtils
 				.getOntologyCompiler(section);
 		URI uri = getURI(section);
 		if (uri == null || compiler == null) return null;
 		PartialHierarchyTree<URI> classHierarchy = Rdf2GoUtils.getClassHierarchy(compiler.getRdf2GoCore(), uri);
+		List<Section<SparqlVisualizationType>> possibleVisTemplate = new ArrayList<>();
 		for (URI clazzURI : classHierarchy.getNodesDFSOrder()) {
 			Section<SparqlVisualizationType> sparqlVisualizationTypeSection = templates.get(Rdf2GoUtils.reduceNamespace(compiler
 					.getRdf2GoCore(), clazzURI.toString()));
 			if (sparqlVisualizationTypeSection != null) {
 				// found applicable template
-				return sparqlVisualizationTypeSection;
+				possibleVisTemplate.add(sparqlVisualizationTypeSection);
 			}
 		}
-		return null;
+		return possibleVisTemplate;
 	}
 
 	private Map<String, Section<SparqlVisualizationType>> getClassVisTemplates(Section<?> section) {
