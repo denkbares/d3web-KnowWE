c18a300b133d4c28dfd25f706edc76dab3c2109c
Albrecht Striffler <albrecht.striffler@denkbares.com>
2017-12-18 10:21:34 +0100

    Fixed multiple white spaces in word export.

--- KnowWE-Plugins/KnowWE-Plugin-Include/src/main/java/de/knowwe/include/export/WikiTextExporter.java
+++ KnowWE-Plugins/KnowWE-Plugin-Include/src/main/java/de/knowwe/include/export/WikiTextExporter.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (C) 2014 denkbares GmbH
- * 
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -47,12 +47,16 @@ public class WikiTextExporter implements Exporter<WikiTextType> {
 		String[] lines = text.split("[\\s\u00A0]*\\\\\\\\+[\\s ]*");
 		boolean first = true;
 		for (String line : lines) {
-			if (first) first = false;
+			if (first) {
+				first = false;
+			}
 			else {
 				manager.appendLineBreak();
 			}
-			manager.append(line);
+			// replace (multiple) tab/spaces/returns by one space
+			// this can be done, because multiple paragraphs are NOT (!) included in one WikiTextType section
+			// but the wiki allows single line breaks as floating text
+			manager.append(line.replaceAll("\\s+", " "));
 		}
 	}
-
 }
