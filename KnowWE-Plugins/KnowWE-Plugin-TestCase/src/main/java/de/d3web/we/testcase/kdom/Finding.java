abcd07bc32a7882508c0b83c6aa779266543f3a1
volker.belli <volker.belli@denkbares.com>
2018-08-17 15:54:23 +0200

    using the newly created section finders

--- KnowWE-Plugins/KnowWE-Plugin-TestCase/src/main/java/de/d3web/we/testcase/kdom/Finding.java
+++ KnowWE-Plugins/KnowWE-Plugin-TestCase/src/main/java/de/d3web/we/testcase/kdom/Finding.java
@@ -1,17 +1,17 @@
 /*
  * Copyright (C) 2009 Chair of Artificial Intelligence and Applied Informatics
  * Computer Science VI, University of Wuerzburg
- * 
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -31,6 +31,8 @@ import de.d3web.we.object.QuestionReference;
 import de.knowwe.core.kdom.AbstractType;
 import de.knowwe.core.kdom.Type;
 import de.knowwe.core.kdom.parsing.Section;
+import de.knowwe.core.kdom.sectionFinder.LeftOfTokenFinder;
+import de.knowwe.core.kdom.sectionFinder.RightOfTokenFinder;
 import de.knowwe.core.kdom.sectionFinder.SectionFinder;
 import de.knowwe.core.kdom.sectionFinder.SectionFinderResult;
 
@@ -39,17 +41,17 @@ public class Finding extends AbstractType {
 	public Finding() {
 
 		QuestionReference question = new QuestionReference();
-		question.setSectionFinder(new QuestionSectionFinder());
+		question.setSectionFinder(new LeftOfTokenFinder("="));
 
 		AnswerReference answer = new FindingAnswerReference();
-		answer.setSectionFinder(new AnswerSectionFinder());
+		answer.setSectionFinder(new RightOfTokenFinder("="));
 
 		this.addChildType(question);
 		this.addChildType(answer);
 		this.setSectionFinder(new FindingSectionFinder());
 	}
 
-	public class FindingSectionFinder implements SectionFinder {
+	public static class FindingSectionFinder implements SectionFinder {
 
 		@Override
 		public List<SectionFinderResult> lookForSections(String text, Section<?> father, Type type) {
@@ -68,50 +70,5 @@ public class Finding extends AbstractType {
 
 			return result;
 		}
-
 	}
-
-	public class AnswerSectionFinder implements SectionFinder {
-
-		@Override
-		public List<SectionFinderResult> lookForSections(String text, Section<?> father, Type type) {
-
-			List<SectionFinderResult> result = new ArrayList<>();
-			List<StringFragment> findings = Strings.splitUnquoted(text, "=", true);
-
-			if (findings.size() > 1) {
-				int start = text.indexOf(findings.get(1).getContent().trim());
-				int end = start + findings.get(1).getContent().trim().length();
-
-				SectionFinderResult s = new SectionFinderResult(start, end);
-
-				result.add(s);
-
-			}
-
-			return result;
-		}
-
-	}
-
-	class QuestionSectionFinder implements SectionFinder {
-
-		@Override
-		public List<SectionFinderResult> lookForSections(String text, Section<?> father, Type type) {
-
-			List<SectionFinderResult> result = new ArrayList<>();
-			List<StringFragment> findings = Strings.splitUnquoted(text, "=");
-
-			int start = text.indexOf(findings.get(0).getContent().trim());
-			int end = start + findings.get(0).getContent().trim().length();
-
-			SectionFinderResult s =
-					new SectionFinderResult(start, end);
-			result.add(s);
-
-			return result;
-		}
-
-	}
-
 }
