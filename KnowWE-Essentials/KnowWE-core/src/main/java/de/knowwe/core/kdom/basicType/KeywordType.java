e3e6167ea455777b86d7d29d7b4e1b8f2f0dd629
volker.belli <volker.belli@denkbares.com>
2018-08-17 22:52:49 +0200

    improved sectionizing
    * prevent possible endless looping: using splitting instead of rule-container
    * fixed potential NPE in condition building: skip null-conditions
    * added keyword sections for top-level-blocks

--- KnowWE-Essentials/KnowWE-core/src/main/java/de/knowwe/core/kdom/basicType/KeywordType.java
+++ KnowWE-Essentials/KnowWE-core/src/main/java/de/knowwe/core/kdom/basicType/KeywordType.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (C) 2011 University Wuerzburg, Computer Science VI
- * 
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -29,23 +29,21 @@ import de.knowwe.kdom.renderer.StyleRenderer.MaskMode;
 
 /**
  * Type to match a special keyword surrounded by only white-spaces.
- * 
+ *
  * @author volker_belli
  * @created 21.01.2011
  */
 public class KeywordType extends AbstractType {
 
 	private final String keyWord;
-	private static final Renderer DEFAULT_RENDERER =
-			new StyleRenderer(StyleRenderer.KEYWORDS, MaskMode.htmlEntities);
+	private static final Renderer DEFAULT_RENDERER = new StyleRenderer(StyleRenderer.KEYWORDS, MaskMode.htmlEntities);
 	private static final Renderer PLAIN_RENDERER = DefaultTextRenderer.getInstance();
 
 	/**
-	 * Creates a new keyword type for a specific keyword that if treated as a
-	 * literal string, not as a regular expression. Thus also characters may be
-	 * included in the terminalKeyWord that usually interpreted as regular
+	 * Creates a new keyword type for a specific keyword that if treated as a literal string, not as a regular
+	 * expression. Thus also characters may be included in the terminalKeyWord that usually interpreted as regular
 	 * expression control characters.
-	 * 
+	 *
 	 * @param literalKeyWord the literal keyword
 	 */
 	public KeywordType(String literalKeyWord) {
@@ -53,13 +51,12 @@ public class KeywordType extends AbstractType {
 	}
 
 	/**
-	 * Creates a new keyword type for a specific keyword that if treated as a
-	 * literal string, not as a regular expression. Thus also characters may be
-	 * included in the terminalKeyWord that usually interpreted as regular
+	 * Creates a new keyword type for a specific keyword that if treated as a literal string, not as a regular
+	 * expression. Thus also characters may be included in the terminalKeyWord that usually interpreted as regular
 	 * expression control characters.
-	 * 
+	 *
 	 * @param literalKeyWord the literal keyword
-	 * @param renderPlain specified if the keyword shall be rendered plain
+	 * @param renderPlain    specified if the keyword shall be rendered plain
 	 */
 	public KeywordType(String literalKeyWord, boolean renderPlain) {
 		this.keyWord = literalKeyWord;
@@ -70,9 +67,8 @@ public class KeywordType extends AbstractType {
 	}
 
 	/**
-	 * Creates a new keyword type for a specific keyword interpreted as a
-	 * regular expression.
-	 * 
+	 * Creates a new keyword type for a specific keyword interpreted as a regular expression.
+	 *
 	 * @param keyWordPattern the keyword pattern
 	 */
 	public KeywordType(Pattern keyWordPattern) {
@@ -80,23 +76,21 @@ public class KeywordType extends AbstractType {
 	}
 
 	/**
-	 * Creates a new keyword type for a specific keyword interpreted as a
-	 * regular expression.
-	 * 
+	 * Creates a new keyword type for a specific keyword interpreted as a regular expression.
+	 *
 	 * @param keyWordPattern the keyword pattern
-	 * @param group the group to take the regular expression from
+	 * @param group          the group to take the regular expression from
 	 */
 	public KeywordType(Pattern keyWordPattern, int group) {
 		this(keyWordPattern, group, false);
 	}
 
 	/**
-	 * Creates a new keyword type for a specific keyword interpreted as a
-	 * regular expression.
-	 * 
+	 * Creates a new keyword type for a specific keyword interpreted as a regular expression.
+	 *
 	 * @param keyWordPattern the keyword pattern
-	 * @param group the group to take the regular expression from
-	 * @param renderPlain specified if the keyword shall be rendered plain
+	 * @param group          the group to take the regular expression from
+	 * @param renderPlain    specified if the keyword shall be rendered plain
 	 */
 	public KeywordType(Pattern keyWordPattern, int group, boolean renderPlain) {
 		this.keyWord = keyWordPattern.pattern() + ".group(" + group + ")";
