f28af60fef9e7619dbde5c1a3ed54ae6c42d0404
volker.belli <volker.belli@denkbares.com>
2018-08-17 20:53:39 +0200

    javadoc and some code cleanup

--- KnowWE-Essentials/KnowWE-core/src/main/java/de/knowwe/kdom/renderer/TooltipRenderer.java
+++ KnowWE-Essentials/KnowWE-core/src/main/java/de/knowwe/kdom/renderer/TooltipRenderer.java
@@ -22,6 +22,7 @@ package de.knowwe.kdom.renderer;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
+import com.denkbares.strings.Strings;
 import de.knowwe.core.Attributes;
 import de.knowwe.core.kdom.parsing.Section;
 import de.knowwe.core.kdom.rendering.DelegateRenderer;
@@ -40,25 +41,43 @@ public abstract class TooltipRenderer implements Renderer {
 
 	private final Renderer delegate;
 
+	/**
+	 * Creates a new renderer that (potentially) displays a tooltip for the rendered sections.
+	 */
 	public TooltipRenderer() {
 		this(null);
 	}
 
+	/**
+	 * Creates a new renderer that (potentially) displays a tooltip for the rendered sections. It used the specified
+	 * delegate renderer, to render the section's content that will be fitted with a tooltip.
+	 */
 	public TooltipRenderer(Renderer delegate) {
 		this.delegate = delegate;
 	}
 
+	/**
+	 * Utility method to create a tooltip renderer without subclassing this class. Instead, you can provide a supplier
+	 * function that returns the tooltip, or null if no tooltip should be provided for the section.
+	 *
+	 * @param delegate the delegate renderer to be decorated by a tooltip
+	 * @param supplier the tooltip supplier function
+	 * @return the tooltip as HTML text, or null
+	 */
 	public static TooltipRenderer create(Renderer delegate, Function<Section<?>, String> supplier) {
 		return create(delegate, (section, user) -> supplier.apply(section));
 	}
 
+	/**
+	 * Utility method to create a tooltip renderer without subclassing this class. Instead, you can provide a supplier
+	 * function that returns the tooltip, or null if no tooltip should be provided for the section.
+	 *
+	 * @param delegate the delegate renderer to be decorated by a tooltip
+	 * @param supplier the tooltip supplier function
+	 * @return the tooltip as HTML text, or null
+	 */
 	public static TooltipRenderer create(Renderer delegate, BiFunction<Section<?>, UserContext, String> supplier) {
 		return new TooltipRenderer(delegate) {
-			@Override
-			public boolean hasTooltip(Section<?> section, UserContext user) {
-				return getTooltip(section, user) != null;
-			}
-
 			@Override
 			public String getTooltip(Section<?> section, UserContext user) {
 				return supplier.apply(section, user);
@@ -79,8 +98,32 @@ public abstract class TooltipRenderer implements Renderer {
 		if (hasTooltip) postRenderTooltip(result);
 	}
 
-	public abstract boolean hasTooltip(Section<?> section, UserContext user);
+	/**
+	 * Returns true if there is a tooltip to be potentially displayed for the specified section and the specified user
+	 * context. You may overwrite this method to perform a fast check as it is called every time the section is
+	 * rendered. If not, it defaults to true, if a non-blank tooltip could be created, see {@link #getTooltip(Section,
+	 * UserContext)}.
+	 *
+	 * @param section the section to create the tooltip for
+	 * @param user    the user context to create the tooltip for
+	 * @return true if a tooltip is available
+	 */
+	public boolean hasTooltip(Section<?> section, UserContext user) {
+		return Strings.nonBlank(getTooltip(section, user));
+	}
 
+	/**
+	 * Returns the tooltip to be displayed for the specified section and the specified user context. The method may
+	 * return null (or an empty or blank string), if there is no tooltip to be displayed. Otherwise the returned text
+	 * should be HTML syntax.
+	 * <p>
+	 * If this method is complex to compute, you may consider to overwrite {@link #hasTooltip(Section, UserContext)} to
+	 * perform a fast check, to avoid that this method is called frequently.
+	 *
+	 * @param section the section to create the tooltip for
+	 * @param user    the user context to create the tooltip for
+	 * @return the tooltip as HTML text, or null
+	 */
 	public abstract String getTooltip(Section<?> section, UserContext user);
 
 	/**
