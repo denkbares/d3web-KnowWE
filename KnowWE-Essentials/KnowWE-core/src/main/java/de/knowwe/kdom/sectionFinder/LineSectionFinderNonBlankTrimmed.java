72ef482608e8ddbf8d50fde171a0bd97ebd1d5ca
volker.belli <volker.belli@denkbares.com>
2018-08-18 11:36:18 +0200

    fixed typo, added javadoc

---
+++ KnowWE-Essentials/KnowWE-core/src/main/java/de/knowwe/kdom/sectionFinder/LineSectionFinderNonBlankTrimmed.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2018 denkbares GmbH. All rights reserved.
+ */
+
+package de.knowwe.kdom.sectionFinder;
+
+/**
+ * Section finder that detects non-empty (or blank) lines and creates a trimmed section for each of these lines. Even if
+ * multiple subsequent lines have content, each of those lines is sectionized into an individual section.
+ */
+public class LineSectionFinderNonBlankTrimmed extends SplittingSectionFinder {
+
+	private static final LineSectionFinderNonBlankTrimmed instance = new LineSectionFinderNonBlankTrimmed();
+
+	private LineSectionFinderNonBlankTrimmed() {
+		super("[\\s\\h\\v]*\\n[\\s\\h\\v]*");
+	}
+
+	/**
+	 * Returns the singleton instance for this section finder.
+	 */
+	public static LineSectionFinderNonBlankTrimmed getInstance() {
+		return instance;
+	}
+}
