871c76587b7bc393459bb7b6ea3d319e660a529f
volker.belli <volker.belli@denkbares.com>
2018-08-20 09:03:50 +0200

    awaiting the tsm registration to be completed, before searching for tsm referenced terms

--- KnowWE-Plugins/KnowWE-Plugin-d3web-OntologyBridge/src/main/java/de/knowwe/d3web/ontology/bridge/BridgedReferenceRegistrationScript.java
+++ KnowWE-Plugins/KnowWE-Plugin-d3web-OntologyBridge/src/main/java/de/knowwe/d3web/ontology/bridge/BridgedReferenceRegistrationScript.java
@@ -4,11 +4,14 @@
 package de.knowwe.d3web.ontology.bridge;
 
 import de.d3web.we.knowledgebase.D3webCompiler;
+import de.knowwe.core.compile.Priority;
 import de.knowwe.core.compile.terminology.TerminologyManager;
 import de.knowwe.core.kdom.objects.SimpleReferenceRegistrationScript;
 import de.knowwe.core.kdom.objects.Term;
 import de.knowwe.core.kdom.parsing.Section;
 import de.knowwe.core.report.CompilerMessage;
+import de.knowwe.core.report.Messages;
+import de.knowwe.ontology.compile.OntologyCompiler;
 
 /**
  * @author Albrecht Striffler (denkbares GmbH)
@@ -16,12 +19,14 @@ import de.knowwe.core.report.CompilerMessage;
  */
 public class BridgedReferenceRegistrationScript extends SimpleReferenceRegistrationScript<D3webCompiler> {
 
+	private final Priority definitionPriority;
+
 	/**
 	 * Creates a new compile script for the given compiler, automatically validating that the term is defined in the
 	 * bridged ontology of the d3web compiler.
 	 */
 	public BridgedReferenceRegistrationScript() {
-		super(D3webCompiler.class);
+		this(null);
 	}
 
 	/**
@@ -30,7 +35,26 @@ public class BridgedReferenceRegistrationScript extends SimpleReferenceRegistrat
 	 * bridged ontology of the d3web compiler.
 	 */
 	public BridgedReferenceRegistrationScript(boolean validate) {
+		this(null, validate);
+	}
+
+	/**
+	 * Creates a new compile script for the given compiler, automatically validating that the term is defined in the
+	 * bridged ontology of the d3web compiler.
+	 */
+	public BridgedReferenceRegistrationScript(Priority definitionPriority) {
+		super(D3webCompiler.class);
+		this.definitionPriority = definitionPriority;
+	}
+
+	/**
+	 * Creates a new compile script for the given compiler. If validate is set to false, the script will register
+	 * without checking for the validity of the reference. Otherwise it validates that the term is defined in the
+	 * bridged ontology of the d3web compiler.
+	 */
+	public BridgedReferenceRegistrationScript(Priority definitionPriority, boolean validate) {
 		super(D3webCompiler.class, validate);
+		this.definitionPriority = definitionPriority;
 	}
 
 	@Override
@@ -38,6 +62,15 @@ public class BridgedReferenceRegistrationScript extends SimpleReferenceRegistrat
 		// apply some explicit error handling if the ontology is not properly linked
 		if (!OntologyBridge.hasOntology(compiler)) {
 			throw CompilerMessage.error("No ontology linked to the given d3web compiler");
+		}
+		if (definitionPriority != null) {
+			try {
+				OntologyCompiler ontology = OntologyBridge.getOntology(compiler);
+				compiler.getCompilerManager().awaitCompilePriorityCompleted(ontology, definitionPriority);
+			}
+			catch (InterruptedException e) {
+				throw new CompilerMessage(Messages.error(e));
+			}
 		}
 		super.compile(compiler, section);
 	}
