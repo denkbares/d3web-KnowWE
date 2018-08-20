d0fd8db89c2b695b6c334d2d8a3431cb51302d14
volker.belli <volker.belli@denkbares.com>
2018-08-20 04:05:58 +0200

    enabled to skip blank sections in KDOM-tree-view

--- KnowWE-Essentials/KnowWE-Plugin-Core/src/main/java/de/knowwe/core/kdom/renderer/RenderKDOMType.java
+++ KnowWE-Essentials/KnowWE-Plugin-Core/src/main/java/de/knowwe/core/kdom/renderer/RenderKDOMType.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (C) 2012 denkbares GmbH
- * 
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -18,8 +18,13 @@
  */
 package de.knowwe.core.kdom.renderer;
 
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+
 import com.denkbares.strings.Strings;
+import de.knowwe.core.kdom.basicType.PlainText;
 import de.knowwe.core.kdom.parsing.Section;
+import de.knowwe.core.kdom.parsing.Sections;
 import de.knowwe.core.kdom.rendering.RenderResult;
 import de.knowwe.core.user.UserContext;
 import de.knowwe.kdom.defaultMarkup.DefaultMarkup;
@@ -32,13 +37,12 @@ import de.knowwe.kdom.defaultMarkup.DefaultMarkupType;
  */
 public class RenderKDOMType extends DefaultMarkupType {
 
-	/**
-	 * @param markup
-	 */
+	private static final String ANNOTATION_HIDE_EMPTY = "hideBlank";
 	private static final DefaultMarkup MARKUP;
 
 	static {
 		MARKUP = new DefaultMarkup("RenderKDOM");
+		MARKUP.addAnnotation(ANNOTATION_HIDE_EMPTY, false, "yes", "no", "plain");
 	}
 
 	public RenderKDOMType() {
@@ -46,10 +50,30 @@ public class RenderKDOMType extends DefaultMarkupType {
 		setRenderer(new KDOMRenderer());
 	}
 
-	private class KDOMRenderer extends DefaultMarkupRenderer {
+	/**
+	 * Returns a section filter that only accepts the sections that should be displayed by the specified KDOM renderer
+	 * markuo.
+	 */
+	public static Predicate<Section<?>> getSectionFilter(Section<RenderKDOMType> section) {
+		String hide = getAnnotation(section, ANNOTATION_HIDE_EMPTY);
+		if (hide == null || "no".equalsIgnoreCase(hide)) {
+			return s -> true;
+		}
+
+		Pattern blank = Pattern.compile("[\\h\\s\\v]*");
+		Predicate<Section<?>> nonBlank = s -> !blank.matcher(s.getText()).matches();
+		if ("plain".equalsIgnoreCase(hide)) {
+			Predicate<Section<?>> nonPlain = s -> !(s.get() instanceof PlainText);
+			return nonPlain.or(nonBlank);
+		}
+		return nonBlank;
+	}
+
+	private static class KDOMRenderer extends DefaultMarkupRenderer {
 
 		@Override
 		protected void renderContents(Section<?> section, UserContext user, RenderResult string) {
+
 			String html = "";
 			html += "<table class='renderKDOMTable wikitable' article='" + section.getTitle() + "'>";
 			html += "<tr>";
@@ -61,15 +85,19 @@ public class RenderKDOMType extends DefaultMarkupType {
 			html += "<th>Text</th>";
 			html += "</tr>";
 
+			// prepare filter predicate for the displayed sections
+			Predicate<Section<?>> filter = getSectionFilter(Sections.cast(section, RenderKDOMType.class));
+
+			// and render the subtree with the filter
 			string.appendHtml(html);
-			renderSubtree(section.getArticle().getRootSection(), string, 1);
+			renderSubtree(section.getArticle().getRootSection(), string, filter);
 
 			string.appendHtml("</table>");
 		}
 
-		protected void renderSubtree(Section<?> s, RenderResult string, int count) {
-			string.appendHtml("<tr data-tt-id='kdom-row-" + s.getID()
-					+ "'");
+		private void renderSubtree(Section<?> s, RenderResult string, Predicate<Section<?>> filter) {
+			if (!filter.test(s)) return;
+			string.appendHtml("<tr data-tt-id='kdom-row-" + s.getID() + "'");
 			if (s.getParent() != null) {
 				string.append(" data-tt-parent-id='kdom-row-" + s.getParent().getID() + "'");
 			}
@@ -77,13 +105,7 @@ public class RenderKDOMType extends DefaultMarkupType {
 			if (s.getParent() != null) {
 				string.append(" child-of-kdom-row-" + s.getParent().getID());
 			}
-			if (count % 2 != 0) {
-				string.append(" odd' ");
-			}
-			else {
-				string.append("'");
-			}
-			string.appendHtml(">");
+			string.append("'").appendHtml(">");
 			String typeName = s.get().getClass().getSimpleName();
 			if (Strings.isBlank(typeName)) {
 				typeName = s.get().getClass().getName();
@@ -101,10 +123,9 @@ public class RenderKDOMType extends DefaultMarkupType {
 			string.appendHtml("</tr>");
 			if (!s.getChildren().isEmpty()) {
 				for (Section<?> child : s.getChildren()) {
-					renderSubtree(child, string, (count + 1));
+					renderSubtree(child, string, filter);
 				}
 			}
 		}
 	}
-
 }
