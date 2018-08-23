e12272922ff71f9a048fe304bfd95ebdcf9244ac
volker.belli <volker.belli@denkbares.com>
2018-08-23 09:07:06 +0200

    case-insensitive tag cloud sorting

--- KnowWE-Plugins/KnowWE-Plugin-Tagging/src/main/java/de/knowwe/tagging/TagCloud.java
+++ KnowWE-Plugins/KnowWE-Plugin-Tagging/src/main/java/de/knowwe/tagging/TagCloud.java
@@ -1,17 +1,17 @@
 /*
  * Copyright (C) 2009 Chair of Artificial Intelligence and Applied Informatics
  * Computer Science VI, University of Wuerzburg
- * 
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -20,9 +20,6 @@
 
 package de.knowwe.tagging;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 
 import com.denkbares.strings.Strings;
@@ -38,19 +35,17 @@ public class TagCloud extends AbstractHTMLTagHandler {
 
 	@Override
 	public void renderHTML(String web, String topic, UserContext user, Map<String, String> values, RenderResult result) {
-		Map<String, Integer> weightedlist = TaggingMangler.getInstance().getCloudList(8, 20);
 		result.appendHtml("<p>");
-		// TagSearch.jsp?query=test+auto&ok=Find!&start=0&maxitems=20
-		List<String> tlist = new ArrayList<>();
-		tlist.addAll(weightedlist.keySet());
-		Collections.sort(tlist);
-		for (String cur : tlist) {
-			result.appendHtml(" <a href =\"Wiki.jsp?page=TagSearch&tag=" + Strings.encodeURL(cur)
-					+ "\" style=\"font-size:" + weightedlist.get(cur)
-					+ "px\">");
-			result.append(cur);
+
+		Map<String, Integer> fontSizes = TaggingMangler.getInstance().getCloudList(8, 20);
+		fontSizes.keySet().stream().sorted(Strings.CASE_INSENSITIVE_ORDER).forEach(tag -> {
+			result.appendHtml(" <a href ='Wiki.jsp?page=TagSearch&tag=").append(Strings.encodeURL(tag))
+					.appendHtml("' style='font-size:").append(fontSizes.get(tag))
+					.appendHtml("px'>");
+			result.append(tag);
 			result.appendHtml("</a>");
-		}
+		});
+
 		result.appendHtml("</p>");
 	}
 }
