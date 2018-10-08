603a61d22481f816b6cdca7675bc1ce55b2cda22
Albrecht Striffler <albrecht.striffler@denkbares.com>
2018-10-09 00:46:42 +0200

    Rdf2GoCoreDestroyEvent now extends ModifiedCoreDataEvent

--- KnowWE-Plugins/KnowWE-Plugin-Rdf2GoSemanticCore/src/main/java/de/knowwe/rdf2go/Rdf2GoCoreDestroyEvent.java
+++ KnowWE-Plugins/KnowWE-Plugin-Rdf2GoSemanticCore/src/main/java/de/knowwe/rdf2go/Rdf2GoCoreDestroyEvent.java
@@ -1,22 +1,14 @@
 package de.knowwe.rdf2go;
 
-import com.denkbares.events.Event;
-
 /**
  * Gets fired when a Rdf2GoCore is to be destroyed.
  *
  * @author Albrecht Striffler (denkbares GmbH)
  * @created 18.03.2014
  */
-public class Rdf2GoCoreDestroyEvent implements Event {
+public class Rdf2GoCoreDestroyEvent extends ModifiedCoreDataEvent {
 
-	private final Rdf2GoCore core;
-
-	public Rdf2GoCoreDestroyEvent(Rdf2GoCore rdf2GoCore) {
-		this.core = rdf2GoCore;
-	}
-
-	public Rdf2GoCore getRdf2GoCore() {
-		return this.core;
+	public Rdf2GoCoreDestroyEvent(Rdf2GoCore core) {
+		super(core);
 	}
 }
