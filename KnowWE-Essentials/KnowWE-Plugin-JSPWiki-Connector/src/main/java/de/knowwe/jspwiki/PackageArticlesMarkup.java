e67419ed6ac2627c5be05259a7082142f2dad4f3
Tim.Abler <tim.abler@denkbares.com>
2018-10-19 14:08:23 +0200

    Added annotation exclude to PackageArticlesMarkup

--- KnowWE-Essentials/KnowWE-Plugin-JSPWiki-Connector/src/main/java/de/knowwe/jspwiki/PackageArticlesMarkup.java
+++ KnowWE-Essentials/KnowWE-Plugin-JSPWiki-Connector/src/main/java/de/knowwe/jspwiki/PackageArticlesMarkup.java
@@ -8,16 +8,11 @@ import java.util.function.Predicate;
 
 import de.knowwe.core.compile.packaging.PackageManager;
 import de.knowwe.core.kdom.parsing.Section;
-import de.knowwe.core.kdom.rendering.RenderResult;
-import de.knowwe.core.kdom.rendering.Renderer;
-import de.knowwe.core.user.UserContext;
 import de.knowwe.core.utils.KnowWEUtils;
 import de.knowwe.kdom.defaultMarkup.DefaultMarkup;
-import de.knowwe.kdom.defaultMarkup.DefaultMarkupRenderer;
 import de.knowwe.kdom.defaultMarkup.DefaultMarkupType;
 
-import static com.denkbares.strings.Strings.splitUnquotedToArray;
-import static com.denkbares.strings.Strings.unquote;
+import static com.denkbares.strings.Strings.*;
 
 /**
  * @author Tim Abler
@@ -36,79 +31,122 @@ public class PackageArticlesMarkup extends DefaultMarkupType {
 
 	public PackageArticlesMarkup() {
 		super(MARKUP);
-		this.setRenderer(new Renderer() {
-			@Override
-			public void render(Section<?> section, UserContext user, RenderResult string) {
-				Collection<Section<?>> packageSections = KnowWEUtils.getPackageManager(section)
-						.getSectionsOfPackage(section.getPackageNames().toArray(new String[0]));
+		this.setRenderer((section, user, string) -> {
+			Collection<Section<?>> packageSections = KnowWEUtils.getPackageManager(section)
+					.getSectionsOfPackage(section.getPackageNames().toArray(new String[0]));
 
-				//annotation template
-				String prefix = "*";
-				String suffix = "";
-				boolean replace = false;
-				String oldString = "";
-				String newString = "";
+			string.appendHtml("<div>");
+			string.append("\n");
 
-				String template = DefaultMarkupType.getAnnotation(section, "template");
-				if (template != null) {
-					if (template.contains("link")) {
-						if (template.endsWith("\\")) {
-							suffix = "\\\\";
-						}
-						if (template.matches(".* link.*")) {
-							prefix = template.substring(0, template.indexOf("link") - 1);
-						}
-						else if (template.matches("link.*")) {
-							prefix = template.substring(0, template.indexOf("link"));
-						}
-						if (template.matches(".*link\\.replace(.*,.*)")) {
-							replace = true;
+			//annotation template
+			String template = DefaultMarkupType.getAnnotation(section, "template");
+			TemplateResult templateResult = new TemplateResult(template).invoke();
+			String prefix = templateResult.getPrefix();
+			String suffix = templateResult.getSuffix();
+			String oldString = templateResult.getOldString();
+			String newString = templateResult.getNewString();
 
-							template = template.substring(template.indexOf("(") + 1, template.lastIndexOf(")"));
-							String[] temp = splitUnquotedToArray(template, ",");
-							if (temp.length == 2) {
-								oldString = unquote(temp[0].trim());
-								newString = unquote(temp[1].trim());
-							}
-							else {
-								throw new RuntimeException("The replace methode can only handle two arguments!");
-							}
-						}
-					}
-				}
+			final String prefixFinal = prefix;
+			final String suffixFinal = suffix;
+			final String oldStringFinal = oldString;
+			final String newStringFinal = newString;
 
-				final String prefixFinal = prefix;
-				final String suffixFinal = suffix;
-				final String oldStringFinal;
-				final String newStringFinal;
-
-				if (replace) {
-					oldStringFinal = oldString;
-					newStringFinal = newString;
-				}
-				else {
-					oldStringFinal = "";
-					newStringFinal = "";
-				}
-
-				packageSections.stream()
-						.filter(section1 -> {
-							assert section.getTitle() != null;
-							return !section.getTitle().equals(section1.getTitle());
-						})
-						.filter(distinctByKey(Section::getTitle))
-
-						.forEach(s -> {
-							assert s.getTitle() != null;
-							string.append(prefixFinal + "[" + s.getTitle()
-									.replace(oldStringFinal, newStringFinal) + "|" + s.getTitle() + "]\n" + suffixFinal);
-						});
+			//annotation exclude
+			String exclude = DefaultMarkupType.getAnnotation(section, "exclude");
+			String[] excludeRegex = splitUnquotedToArray(exclude, ",");
+			for (int i = 0; i < excludeRegex.length; i++) {
+				excludeRegex[i] = unquote(trim(excludeRegex[i]));
 			}
+
+			packageSections.stream()
+					.filter(s -> {
+						assert s.getTitle() != null;
+						for (String regex : excludeRegex) {
+							if (s.getTitle().matches(regex)) {
+								return false;
+							}
+						}
+						return true;
+					})
+					.filter(section1 -> {
+						assert section.getTitle() != null;
+						return !section.getTitle().equals(section1.getTitle());
+					})
+					.filter(distinctByKey(Section::getTitle))
+
+					.forEach(s -> {
+						assert s.getTitle() != null;
+						string.append(prefixFinal + "[" + s.getTitle()
+								.replace(oldStringFinal, newStringFinal) + "|" + s.getTitle() + "]\n" + suffixFinal);
+					});
+
+			string.appendHtml("</div>");
 		});
 	}
 
 	private static <T> Predicate<T> distinctByKey(Function<? super T, Object> keyExtractor) {
 		Map<Object, Boolean> seen = new ConcurrentHashMap<>();
 		return t -> seen.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
+	}
+
+	private static class TemplateResult {
+		private String template;
+		private String prefix;
+		private String suffix;
+		private String oldString;
+		private String newString;
+
+		TemplateResult(String template) {
+			this.template = template;
+		}
+
+		public String getPrefix() {
+			return prefix;
+		}
+
+		public String getSuffix() {
+			return suffix;
+		}
+
+		String getOldString() {
+			return oldString;
+		}
+
+		String getNewString() {
+			return newString;
+		}
+
+		public TemplateResult invoke() {
+			//annotation template
+			prefix = "*";
+			suffix = "";
+			oldString = "";
+			newString = "";
+			if (template == null) return this;
+			if (template.contains("link")) {
+				if (template.endsWith("\\")) {
+					suffix = "\\\\";
+				}
+				if (template.matches(".* link.*")) {
+					prefix = template.substring(0, template.indexOf("link") - 1);
+				}
+				else if (template.matches("link.*")) {
+					prefix = template.substring(0, template.indexOf("link"));
+				}
+				if (template.matches(".*link\\.replace(.*,.*)")) {
+
+					template = template.substring(template.indexOf("(") + 1, template.lastIndexOf(")"));
+					String[] temp = splitUnquotedToArray(template, ",");
+					if (temp.length == 2) {
+						oldString = unquote(temp[0].trim());
+						newString = unquote(temp[1].trim());
+					}
+					else {
+						throw new RuntimeException("The replace methode can only handle two arguments!");
+					}
+				}
+			}
+			return this;
+		}
 	}
 }
