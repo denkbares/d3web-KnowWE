1b986682d83082c145300e89d1dce6537579b151
Jochen Reutelshöfer <jochen.reutelshoefer@denkbares.com>
2018-11-16 21:53:38 +0100

    AsynchronousActionTool can handle set of various parameters to be passed to the called action

--- KnowWE-Essentials/KnowWE-core/src/main/java/de/knowwe/tools/AsynchronousActionTool.java
+++ KnowWE-Essentials/KnowWE-core/src/main/java/de/knowwe/tools/AsynchronousActionTool.java
@@ -19,6 +19,10 @@
 
 package de.knowwe.tools;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 import de.knowwe.core.action.Action;
 import de.knowwe.core.kdom.parsing.Section;
 import de.knowwe.util.Icon;
@@ -34,34 +38,48 @@ import static de.knowwe.core.Attributes.TOPIC;
  */
 public class AsynchronousActionTool extends DefaultTool {
 
+
+    public AsynchronousActionTool(Icon icon, String title, String description, Class<? extends Action> action, Section<?> section, Map<String, String> params) {
+        this(icon, title, description, action, section, null, params);
+    }
+
+
     public AsynchronousActionTool(Icon icon, String title, String description, Class<? extends Action> action, Section<?> section) {
-        this(icon, title, description, action, section, null);
+        this(icon, title, description, action, section, null, new HashMap<>());
+    }
+
+    public AsynchronousActionTool(Icon icon, String title, String description, Class<? extends Action> action, Section<?> section, String currentPageTitle, Map<String, String> params) {
+        super(icon, title, description,
+                buildJsAction(action, section, currentPageTitle, "window.location.reload()", params),
+                Tool.ActionType.ONCLICK, null);
     }
 
     public AsynchronousActionTool(Icon icon, String title, String description, Class<? extends Action> action, Section<?> section, String currentPageTitle) {
         super(icon, title, description,
-                buildJsAction(action, section, currentPageTitle, "window.location.reload()"),
+                buildJsAction(action, section, currentPageTitle, "window.location.reload()", new HashMap<>()),
                 Tool.ActionType.ONCLICK, null);
     }
 
     public AsynchronousActionTool(Icon icon, String title, String description, Class<? extends Action> action, Section<?> section, String currentPageTitle, String redirectPage) {
         super(icon, title, description,
-                buildJsAction(action, section, currentPageTitle, "window.location='Wiki.jsp?page=" + redirectPage + "'"),
+                buildJsAction(action, section, currentPageTitle, "window.location='Wiki.jsp?page=" + redirectPage + "'", new HashMap<>()),
                 Tool.ActionType.ONCLICK, null);
     }
 
-    private static String buildJsAction(Class<? extends Action> action, Section<?> section, String currentPageTitle, String successFunction) {
+    private static String buildJsAction(Class<? extends Action> action, Section<?> section, String currentPageTitle, String successFunction, Map<String, String> params) {
+        params.put(TOPIC, currentPageTitle);
         return "jq$.ajax({url : 'action/" + action.getSimpleName() + "', " +
                 "cache : false, " +
-                createData(section.getID(), currentPageTitle) +
+                createData(section.getID(), params) +
                 "success : function() {" + successFunction + "} })";
     }
 
-    private static String createData(String sectionId, String currentPageTitle) {
-        String topic = "";
-        if (currentPageTitle != null) {
-            topic = "'," + TOPIC + " : '" + currentPageTitle;
+    private static String createData(String sectionId, Map<String, String> params) {
+        String additionalKeyValuePairs = "";
+        Set<String> keySet = params.keySet();
+        for (String key : keySet) {
+            additionalKeyValuePairs += "'," + key + " : '" + params.get(key);
         }
-        return "data: { " + SECTION_ID + " : '" + sectionId + topic + "'}, ";
+        return "data: { " + SECTION_ID + " : '" + sectionId + additionalKeyValuePairs + "'}, ";
     }
 }
