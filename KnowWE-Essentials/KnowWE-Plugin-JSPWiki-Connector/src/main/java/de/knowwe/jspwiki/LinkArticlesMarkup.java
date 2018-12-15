0f13534e0d4953862ba6698ee36b8f4c4a866a55
volker.belli <volker.belli@denkbares.com>
2018-12-15 23:09:36 +0100

    some rework on LinkArticles, allow to specify multiple filter-annotations, using intersections of articles

--- KnowWE-Essentials/KnowWE-Plugin-JSPWiki-Connector/src/main/java/de/knowwe/jspwiki/LinkArticlesMarkup.java
+++ KnowWE-Essentials/KnowWE-Plugin-JSPWiki-Connector/src/main/java/de/knowwe/jspwiki/LinkArticlesMarkup.java
@@ -1,29 +1,53 @@
 package de.knowwe.jspwiki;
 
+import java.text.ParseException;
 import java.util.Collection;
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import com.denkbares.strings.StringFragment;
+import com.denkbares.strings.Strings;
 import de.knowwe.core.compile.packaging.PackageManager;
 import de.knowwe.core.kdom.Article;
 import de.knowwe.core.kdom.parsing.Section;
+import de.knowwe.core.kdom.parsing.Sections;
+import de.knowwe.core.kdom.rendering.RenderResult;
+import de.knowwe.core.report.Message;
+import de.knowwe.core.report.Messages;
 import de.knowwe.core.utils.KnowWEUtils;
 import de.knowwe.core.utils.Scope;
 import de.knowwe.kdom.defaultMarkup.DefaultMarkup;
+import de.knowwe.kdom.defaultMarkup.DefaultMarkupRenderer;
 import de.knowwe.kdom.defaultMarkup.DefaultMarkupType;
 
 import static com.denkbares.strings.Strings.*;
 
 /**
- * @author Tim Abler
+ * The markup takes a subset of articles and renders the links to the articles as a list. You may specify multiple
+ * "selectors" for the articles, then the subset of articles is listed that matches all the selectors. The selectors are
+ * specified as annotations. If there are multiple definition of the same selector, they are combined by "OR", using the
+ * union-set of articles.
+ * <p>
+ * the selectors are:
+ * <ul>
+ * <li>article: regular expression for the article title</li>
+ * <li>exclude: regular expression to exclude articles by their title</li>
+ * <li>package: package-name, to include all articles that are in a specified package</li>
+ * <li>markup: {@link Scope}-definition of the markup to be searched, only articles are listed that contains the
+ * denoted markup</li>
+ * </ul>
+ *
+ * @author Tim Abler, Volker Belli (denkbares GmbH)
  * @created 10.10.2018
  */
 public class LinkArticlesMarkup extends DefaultMarkupType {
@@ -52,7 +76,7 @@ public class LinkArticlesMarkup extends DefaultMarkupType {
 				.setDocumentation("Specify the name of a markup to show all articles it's used in.");
 		MARKUP.getAnnotation(ANNOTATION_TEMPLATE)
 				.setDocumentation("Set a template how each Link-Label should be adapted, e.g.\n"
-						+ "@template: * link.replace(\"Prefix\", \"Other Prefix\")");
+						+ "@template: * link.replace(\"\u00ABregex\u00BB\", \"\u00ABreplacement\u00BB\")");
 		MARKUP.setTemplate("%%LinkArticles \n" +
 				"@package: \u00ABpackage-name\u00BB\n" +
 				"@markup: \u00ABmarkup-name\u00BB\n" +
@@ -63,165 +87,8 @@ public class LinkArticlesMarkup extends DefaultMarkupType {
 
 	public LinkArticlesMarkup() {
 		super(MARKUP);
-		this.setRenderer((section, user, string) -> {
-			//message displayed if unable to find markups/packages
-			Set<String> errorMessages = new HashSet<>();
-
-			//sections which should be shown
-			Set<String> shownSections = new HashSet<>();
-
-			string.appendHtml("<div>");
-			string.append("\n");
-
-			//annotation template
-			String template = DefaultMarkupType.getAnnotation(section, ANNOTATION_TEMPLATE);
-			TemplateResult templateResult = new TemplateResult(template).invoke();
-			String prefix = templateResult.getPrefix();
-			String suffix = templateResult.getSuffix();
-			String oldString = templateResult.getOldString();
-			String newString = templateResult.getNewString();
-
-			final String prefixFinal = prefix;
-			final String suffixFinal = suffix;
-			final String oldStringFinal = oldString;
-			final String newStringFinal = newString;
-
-			//annotation exclude
-			String[] excludeAnnotations = DefaultMarkupType.getAnnotations(section, ANNOTATION_EXCLUDE);
-			List<String> excludeRegexes = new LinkedList<>();
-			if (excludeAnnotations.length != 0) {
-				for (String exclude : excludeAnnotations) {
-					String[] excludeRegex = splitUnquotedToArray(exclude, ",");
-					for (String anExcludeRegex : excludeRegex) {
-						excludeRegexes.add(unquote(trim(anExcludeRegex)));
-					}
-				}
-			}
-
-			//annotation package
-			handleAnnotationPackage(section, errorMessages, shownSections, excludeRegexes);
-
-			//handle annotation markup
-			handleAnnotationMarkup(section, errorMessages, shownSections);
-
-			//handle annotaion titles
-			handleAnnotationArticle(section, errorMessages, shownSections, excludeRegexes);
-
-			//add messages to site
-			if (!errorMessages.isEmpty()) {
-				string.appendHtml("<span class=\"error\" style=\"white-space: pre-wrap;\">");
-				for (String message : errorMessages) {
-					string.appendHtml(message + "<br>");
-				}
-				string.appendHtml("</span>\n");
-			}
-
-			//add result to site
-			shownSections.stream()
-					.filter(titel -> {
-						for (String regex : excludeRegexes) {
-							if (titel.matches(regex)) {
-								return false;
-							}
-						}
-						return true;
-					})
-					.forEach(titel -> string.append(prefixFinal + "[" + titel
-							.replace(oldStringFinal, newStringFinal) + "|" + titel + "]\n" + suffixFinal));
-
-			string.appendHtml("</div>");
-		});
-	}
-
-	private void handleAnnotationArticle(Section<?> section, Set<String> errorMessages, Set<String> shownSections, List<String> excludeRegexes) {
-		String[] titleAnnotations = DefaultMarkupType.getAnnotations(section, ANNOTATION_ARTICLE);
-		// TODO: is not specified, use all articles?
-		if (titleAnnotations.length > 0) {
-			Collection<Article> articles = KnowWEUtils.getArticleManager(section.getWeb()).getArticles();
-			for (String title : titleAnnotations) {
-				AtomicInteger itemCount = new AtomicInteger(0);
-				articles.stream()
-						.filter(a -> {
-							for (String regex : excludeRegexes) {
-								if (title.matches(regex)) {
-									errorMessages.add("unable to find articles which match the regex: " + title + ", because they're excluded!");
-									return false;
-								}
-							}
-							return true;
-						})
-						.filter(a -> a.getTitle().matches(title))
-						.forEach(a -> {
-							shownSections.add(a.getTitle());
-							itemCount.getAndIncrement();
-						});
-				if (itemCount.get() < 1) {
-					errorMessages.add("unable to find articles which match the regex: " + title);
-				}
-			}
-		}
-	}
-
-	private void handleAnnotationMarkup(Section<?> section, Set<String> errorMessages, Set<String> shownSections) {
-		String[] markupAnnotations = DefaultMarkupType.getAnnotations(section, ANNOTATION_MARKUP);
-		if (markupAnnotations.length > 0) {
-			boolean hasAny = false;
-			for (String markup : markupAnnotations) {
-				Scope scope = Scope.getScope(markup);
-				for (Article article : KnowWEUtils.getArticleManager(section.getWeb()).getArticles()) {
-					for (Section<?> sec : scope.getMatchingSuccessors(article.getRootSection())) {
-						// skip link to the rendered article (do not link itself)
-						if (Objects.equals(section.getArticle(), sec.getArticle())) continue;
-						shownSections.add(sec.getTitle());
-						hasAny = true;
-					}
-				}
-
-				//test if found any Sections for this markup
-				if (!hasAny) {
-					errorMessages.add("unable to find markups: " + markup);
-				}
-			}
-		}
-	}
-
-	private void handleAnnotationPackage(Section<?> section, Set<String> errorMessages, Set<String> shownSections, List<String> excludeRegexes) {
-		String[] mentionedPackages = DefaultMarkupType.getAnnotations(section, "package");
-		if (mentionedPackages.length > 0) {
-
-			for (String mentionedPackage : mentionedPackages) {
-				Collection<Section<?>> packageSections = KnowWEUtils.getPackageManager(section)
-						.getSectionsOfPackage(mentionedPackage);
-
-				AtomicInteger itemCount = new AtomicInteger(0);
-				packageSections.stream()
-						.filter(s -> {
-							assert s.getTitle() != null;
-							for (String regex : excludeRegexes) {
-								if (s.getTitle().matches(regex)) {
-									return false;
-								}
-							}
-							return true;
-						})
-						.filter(s -> {
-							assert section.getTitle() != null;
-							return !section.getTitle().equals(s.getTitle());
-						})
-						.filter(distinctByKey(Section::getTitle))
-
-						.forEach(s -> {
-							assert s.getTitle() != null;
-							if (!section.getArticle().getTitle().equals(s.getTitle())) {
-								shownSections.add(s.getTitle());
-							}
-							itemCount.getAndIncrement();
-						});
-				if (itemCount.get() == 0) {
-					errorMessages.add("unable to find packages: " + mentionedPackage);
-				}
-			}
-		}
+		this.setRenderer((section, user, out) ->
+				new RenderWorker(Sections.cast(section, LinkArticlesMarkup.class)).render(out));
 	}
 
 	private static <T> Predicate<T> distinctByKey(Function<? super T, Object> keyExtractor) {
@@ -229,46 +96,149 @@ public class LinkArticlesMarkup extends DefaultMarkupType {
 		return t -> seen.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
 	}
 
+	private static class RenderWorker {
+
+		private final Section<LinkArticlesMarkup> section;
+		private final Collection<Message> messages = new LinkedHashSet<>();
+
+		private RenderWorker(Section<LinkArticlesMarkup> section) {
+			this.section = section;
+		}
+
+		public void render(RenderResult out) {
+
+			// prepare articles
+			Stream<Article> articles = applyPackage();
+			articles = applyArticle(articles);
+			articles = applyExclude(articles);
+
+			// prepare linked sections
+			List<Section<?>> sections = applyMarkup(articles).collect(Collectors.toList());
+			if (sections.isEmpty()) {
+				messages.add(Messages.warning("No matching article found."));
+			}
+
+			// prepare template
+			String template = DefaultMarkupType.getAnnotation(section, ANNOTATION_TEMPLATE);
+			TemplateResult templateResult = new TemplateResult(template);
+			try {
+				templateResult.compile();
+			}
+			catch (ParseException e) {
+				messages.add(Messages.warning(e.getMessage()));
+			}
+
+			// and finally render messages and links
+			out.appendHtml("<div>\n");
+			DefaultMarkupRenderer.renderMessageBlock(out, messages);
+			sections.forEach(sec -> templateResult.appendLink(out, sec));
+			out.appendHtml("</div>");
+		}
+
+		private Stream<Article> applyPackage() {
+			// as packages are applied first, we do not need to take care of the existing stream
+			String[] packageNames = DefaultMarkupType.getAnnotations(section, PackageManager.PACKAGE_ATTRIBUTE_NAME);
+			if (packageNames.length == 0) {
+				// if package(s) is not specified, use all articles
+				return KnowWEUtils.getArticleManager(section.getWeb()).getArticles().stream();
+			}
+
+			// otherwise collect articles from package manager
+			return Stream.of(packageNames)
+					// map package name(s) to sections
+					.flatMap(this::resolvePackage)
+					// map to disjoint articles
+					.map(Section::getArticle)
+					.filter(distinctByKey(Article::getTitle));
+		}
+
+		private Stream<Section<?>> resolvePackage(String packageName) {
+			PackageManager packageManager = KnowWEUtils.getPackageManager(section);
+			if (!packageManager.hasPackage(packageName)) {
+				messages.add(Messages.error("Package not known: " + packageName));
+			}
+			return packageManager.getSectionsOfPackage(packageName).stream();
+		}
+
+		private Stream<Section<?>> applyMarkup(Stream<Article> source) {
+			// check if there are any markups specified
+			String[] markups = DefaultMarkupType.getAnnotations(section, ANNOTATION_MARKUP);
+			if (markups.length == 0) {
+				// if not, use the plain articles
+				return source.map(Article::getRootSection);
+			}
+
+			// otherwise link the first matched section of each article
+			List<Scope> scopes = Stream.of(markups).map(Scope::getScope).collect(Collectors.toList());
+			return source.<Section<?>>map(article -> findMarkup(article, scopes)).filter(Objects::nonNull);
+		}
+
+		private Section<?> findMarkup(Article article, List<Scope> scopes) {
+			return scopes.stream()
+					.flatMap(scope -> scope.getMatchingSuccessors(article.getRootSection()).stream())
+					.findFirst().orElse(null);
+		}
+
+		private Stream<Article> applyArticle(Stream<Article> source) {
+			// filter exiting article stream if any title regexes are specified
+			List<Pattern> patterns = getPatterns(ANNOTATION_ARTICLE);
+			if (patterns.isEmpty()) return source;
+			return source.filter(article -> patterns.stream().anyMatch(pattern ->
+					pattern.matcher(article.getTitle()).matches()));
+		}
+
+		private Stream<Article> applyExclude(Stream<Article> source) {
+			// filter exiting article stream if any title exclusions are specified
+			List<Pattern> patterns = getPatterns(ANNOTATION_EXCLUDE);
+			if (patterns.isEmpty()) return source;
+			return source.filter(article -> patterns.stream().noneMatch(pattern ->
+					pattern.matcher(article.getTitle()).matches()));
+		}
+
+		private List<Pattern> getPatterns(String annotationName) {
+			String[] annotations = DefaultMarkupType.getAnnotations(section, annotationName);
+			List<Pattern> regexes = new LinkedList<>();
+			for (String annotation : annotations) {
+				for (StringFragment fragment : splitUnquoted(annotation, ",", false)) {
+					String regex = fragment.getContentTrimmed();
+					try {
+						regexes.add(Pattern.compile(unquote(trim(regex))));
+					}
+					catch (PatternSyntaxException e) {
+						messages.add(Messages.error("Invalid regular expression: " + regex));
+					}
+				}
+			}
+			return regexes;
+		}
+	}
+
 	private static class TemplateResult {
+
 		private String template;
 		private String prefix;
 		private String suffix;
 		private String oldString;
 		private String newString;
 
-		TemplateResult(String template) {
+		public TemplateResult(String template) {
 			this.template = template;
 		}
 
-		public String getPrefix() {
-			return prefix;
-		}
-
-		public String getSuffix() {
-			return suffix;
-		}
-
-		String getOldString() {
-			return oldString;
-		}
-
-		String getNewString() {
-			return newString;
-		}
-
-		public TemplateResult invoke() {
+		public void compile() throws ParseException {
 			//annotation template
 			prefix = "*";
 			suffix = "";
 			oldString = "";
 			newString = "";
-			if (template == null) return this;
+			if (Strings.isBlank(template)) return;
+
 			if (template.contains("link")) {
 				if (template.endsWith("\\")) {
 					suffix = "\\\\";
 				}
 				if (template.matches(".* link.*")) {
-					prefix = template.substring(0, template.indexOf("link") - 1);
+					prefix = template.substring(0, template.indexOf(" link"));
 				}
 				else if (template.matches("link.*")) {
 					prefix = template.substring(0, template.indexOf("link"));
@@ -282,11 +252,18 @@ public class LinkArticlesMarkup extends DefaultMarkupType {
 						newString = unquote(temp[1].trim());
 					}
 					else {
-						throw new RuntimeException("The replace methode can only handle two arguments!");
+						throw new ParseException("The replace method can only handle two arguments!", 0);
 					}
 				}
 			}
-			return this;
+		}
+
+		public void appendLink(RenderResult out, Section<?> section) {
+			String title = section.getTitle();
+			if (title == null) return;
+			out.append(prefix).append("[").append(title.replace(oldString, newString))
+					.append("|").append(KnowWEUtils.getWikiLink(section)).append("]")
+					.append(suffix).append("\n");
 		}
 	}
 }
