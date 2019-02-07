c81ef27f1398d49e187433a407620ce51eccc37a
Albrecht Striffler <albrecht.striffler@denkbares.com>
2019-02-07 17:33:43 +0100

    Cleanup.

--- KnowWE-Essentials/KnowWE-core/src/main/java/de/knowwe/core/report/DefaultMessageRenderer.java
+++ KnowWE-Essentials/KnowWE-core/src/main/java/de/knowwe/core/report/DefaultMessageRenderer.java
@@ -1,17 +1,17 @@
 /*
  * Copyright (C) 2010 Chair of Artificial Intelligence and Applied Informatics
  * Computer Science VI, University of Wuerzburg
- * 
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -68,16 +68,11 @@ public class DefaultMessageRenderer implements MessageRenderer {
 			result.append(" title='").appendEntityEncoded(
 					tooltip.replace('\'', '"')).append("'");
 		}
-		String cssClass = "tooltipster " + this.cssClass;
-		if (cssClass != null) {
-			result.append(" class='").append(cssClass).append("'");
-		}
+		result.append(" class='").append("tooltipster " + this.cssClass).append("'");
 		if (cssStyle != null) {
 			result.append(" style='").append(cssStyle).append("'");
 		}
 
 		result.appendHtml(">");
-
 	}
-
 }
