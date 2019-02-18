effb95e823ee21072d2badd8c06c79ab12f84bc4
Albrecht Striffler <albrecht.striffler@denkbares.com>
2019-02-18 20:01:54 +0100

    Fixed test case.

--- KnowWE-Plugins/KnowWE-Plugin-Rdf2GoSemanticCore/src/test/java/de.knowwe/rdf2go/Rdf2GoCoreTest.java
+++ KnowWE-Plugins/KnowWE-Plugin-Rdf2GoSemanticCore/src/test/java/de.knowwe/rdf2go/Rdf2GoCoreTest.java
@@ -4,14 +4,18 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 
 import org.junit.Test;
+
+import com.denkbares.strings.Strings;
+import com.denkbares.utils.Streams;
 
 import static org.junit.Assert.*;
 
 /**
- * @author Your Name (optionally your company)
+ * Testing correct unquoting
+ *
+ * @author Felix Herrmann (Olyro GmbH)
  * @created 2/18/19
  */
 public class Rdf2GoCoreTest {
@@ -19,16 +23,16 @@ public class Rdf2GoCoreTest {
 	public void checkTurtleLiteralUnquote() throws IOException {
 		Map<String, String> tests = new HashMap<>();
 		try (InputStream in = Rdf2GoCoreTest.class.getResourceAsStream("TurtleLiteralUnquoteTest.txt")) {
-			final String data = new Scanner(in).useDelimiter("\\A").next().trim();
+			String data = Streams.readStream(in);
 
 			for (String test : data.split("\\s*---\\s*")) {
 				String[] testLines = test.split("\\s*--\\s*");
-				tests.put(testLines[0], testLines[1]);
+				tests.put(Strings.trim(testLines[0]), Strings.trim(testLines[1]));
 			}
-		}
 
-		for (Map.Entry<String, String> entry : tests.entrySet()) {
-			assertEquals(Rdf2GoCore.unquoteTurtleLiteral(entry.getKey()), entry.getValue());
+			for (Map.Entry<String, String> entry : tests.entrySet()) {
+				assertEquals(entry.getValue(), Rdf2GoCore.unquoteTurtleLiteral(entry.getKey()));
+			}
 		}
 	}
 }
