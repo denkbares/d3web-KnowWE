b10e0af040234b1d5f70116d92a32d63c6c4228a
Albrecht Striffler <albrecht.striffler@denkbares.com>
2019-02-21 02:25:43 +0100

    Conditions are now created lazy the first time they are requested, no need for compile scripts. This solves various issues with compile priority.

--- KnowWE-Plugins/KnowWE-Plugin-d3web-basic/src/main/java/de/d3web/we/kdom/condition/D3webCondition.java
+++ KnowWE-Plugins/KnowWE-Plugin-d3web-basic/src/main/java/de/d3web/we/kdom/condition/D3webCondition.java
@@ -1,17 +1,17 @@
 /*
  * Copyright (C) 2009 Chair of Artificial Intelligence and Applied Informatics
  * Computer Science VI, University of Wuerzburg
- * 
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -30,22 +30,23 @@ import de.knowwe.core.report.Messages;
 import de.knowwe.core.utils.KnowWEUtils;
 
 /**
- * 
+ * @param <T>
  * @author Jochen
  * @created 26.07.2010
- * @param <T>
  */
 public abstract class D3webCondition<T extends D3webCondition<T>>
 		extends AbstractType {
 
 	private static final String COND_STORE_KEY = "cond-store-key";
 
-	public D3webCondition() {
-		this.addCompileScript(new CondCreateHandler());
-	}
-
-	public final Condition getCondition(PackageCompiler compiler, Section<? extends D3webCondition<?>> s) {
-		return (Condition) KnowWEUtils.getStoredObject(compiler, s, COND_STORE_KEY);
+	public final Condition getCondition(PackageCompiler compiler, Section<? extends D3webCondition<?>> section) {
+		Condition condition = (Condition) KnowWEUtils.getStoredObject(compiler, section, COND_STORE_KEY);
+		if (condition == null) {
+			//noinspection unchecked
+			condition = createCondition((D3webCompiler) compiler, (Section<T>) section);
+			KnowWEUtils.storeObject(compiler, section, COND_STORE_KEY, condition);
+		}
+		return condition;
 	}
 
 	public static Condition findCondition(D3webCompiler compiler, Section<?> parent) {
@@ -56,34 +57,14 @@ public abstract class D3webCondition<T extends D3webCondition<T>>
 		return condition;
 	}
 
-	private void storeCondition(D3webCompiler compiler, Condition condition, Section<? extends D3webCondition<?>> section) {
-		KnowWEUtils.storeObject(compiler, section, COND_STORE_KEY, condition);
-	}
-
 	/**
 	 * Creates the condition for the requested section in the specified article.
-	 * 
-	 * @created 02.10.2010
+	 *
 	 * @param compiler to create the condition for
-	 * @param section the section of this condition
+	 * @param section  the section of this condition
 	 * @return the newly created condition
+	 * @created 02.10.2010
 	 */
 	protected abstract Condition createCondition(D3webCompiler compiler, Section<T> section);
-
-	private class CondCreateHandler implements D3webCompileScript<T> {
-
-		@Override
-		public void destroy(D3webCompiler compiler, Section<T> section) {
-			storeCondition(compiler, null, section);
-			Messages.clearMessages(compiler, section, getClass());
-		}
-
-		@Override
-		public void compile(D3webCompiler compiler, Section<T> section) {
-			Condition condition = createCondition(compiler, section);
-			storeCondition(compiler, condition, section);
-		}
-
-	}
 
 }
