ae32006edb433d36d9e46cdb94e7d89a61ce6ecb
Albrecht Striffler <albrecht.striffler@denkbares.com>
2019-03-01 11:38:14 +0100

    Table preview renderer now also renders section outside the table of the same markup (annotations, other stuff above/below table). Cleaned up.

--- KnowWE-Plugins/KnowWE-Plugin-Table/src/main/java/de/knowwe/kdom/table/TablePreviewRenderer.java
+++ KnowWE-Plugins/KnowWE-Plugin-Table/src/main/java/de/knowwe/kdom/table/TablePreviewRenderer.java
@@ -4,19 +4,27 @@ import java.util.Collection;
 
 import de.knowwe.core.kdom.Type;
 import de.knowwe.core.kdom.parsing.Section;
-import de.knowwe.core.kdom.rendering.DelegateRenderer;
+import de.knowwe.core.kdom.parsing.Sections;
 import de.knowwe.core.kdom.rendering.RenderResult;
-import de.knowwe.core.kdom.rendering.Renderer;
 import de.knowwe.core.preview.AbstractPreviewRenderer;
 import de.knowwe.core.user.UserContext;
+import de.knowwe.kdom.defaultMarkup.AnnotationType;
+import de.knowwe.kdom.defaultMarkup.ContentType;
 
 import static de.knowwe.core.kdom.parsing.Sections.$;
 
 /**
+ * Render previews of default markups containing a table. Can be used in "Show Info" tool (CompositeEdit).
+ * <p>
+ * The flag <tt>showNonTableSections</tt> allows to specify whether sections outside the table will be shown only if
+ * they contain relevant sections or always
+ *
  * @author Veronika Sehne (denkbares GmbH)
  * @created 20.11.17
  */
 public class TablePreviewRenderer extends AbstractPreviewRenderer {
+
+	private final boolean showNonTableSections = true;
 
 	protected String getHeader() {
 		return "";
@@ -32,6 +40,31 @@ public class TablePreviewRenderer extends AbstractPreviewRenderer {
 
 	@Override
 	public void render(Section<?> section, Collection<Section<?>> relevantSubSections, UserContext user, RenderResult result) {
+
+		Sections<ContentType> contentType = $(section).successor(ContentType.class);
+
+		for (Section<Type> child : contentType.children()) {
+
+			if (child.get() instanceof Table) {
+				renderTable(section, child, relevantSubSections, user, result);
+			}
+			else {
+				if (showNonTableSections ||
+						$(relevantSubSections).ancestor(child.get().getClass()).anyMatch(s -> s == child)) {
+					result.append(child, user);
+				}
+			}
+		}
+		for (Section<AnnotationType> annotationTypeSection : $(section).successor(AnnotationType.class)) {
+
+			if (showNonTableSections ||
+					$(relevantSubSections).ancestor(AnnotationType.class).anyMatch(s -> s == annotationTypeSection)) {
+				result.append(annotationTypeSection, user).append("\n");
+			}
+		}
+	}
+
+	private void renderTable(Section<?> section, Section<?> table, Collection<Section<?>> relevantSubSections, UserContext user, RenderResult result) {
 		boolean sortable = TableUtils.sortOption(section);
 
 		result.appendHtml(getOpeningTag(section));
@@ -46,35 +79,22 @@ public class TablePreviewRenderer extends AbstractPreviewRenderer {
 				+ "' class='wikitable knowwetable' border='1'><tbody>");
 		result.appendHtml(getHeader());
 
-		outerLoop:
-		for (Section<? extends Type> subsection : section.getChildren()) {
-			for (Section<? extends Type> table : subsection.getChildren()) {
-				for (Section<? extends Type> row : table.getChildren()) {
-					if (row.equals(table.getChildren().get(0))) {
-						Renderer renderer = DelegateRenderer.getRenderer(row, user);
-						if (renderer instanceof TableLineRenderer) {
-							renderer.render(row, user, result);
-						}
-					}
-					else {
-						for (Section<?> relevantSubSection : relevantSubSections) {
-							Section<? extends Type> firstMatch = $(row).successor(relevantSubSection.get()
-									.getClass())
-									.filter(s -> s.getID().equals(relevantSubSection.getID()))
-									.getFirst();
-							if (firstMatch != null) {
-								Renderer renderer = DelegateRenderer.getRenderer(row, user);
-								renderer.render(row, user, result);
-								break outerLoop;
-							}
-						}
-					}
-				}
+		boolean foundRow = false;
+		for (Section<TableLine> row : $(section).successor(TableLine.class)) {
+			if (row.equals(table.getChildren().get(0))) {
+				result.append(row, user);
 			}
+			else if ($(relevantSubSections).ancestor(row.get().getClass()).anyMatch(s -> s == row)) {
+				result.append(row, user);
+				foundRow = true;
+				break;
+			}
+		}
+		if (!foundRow) {
+			result.appendHtmlTag("tr").appendHtmlElement("td", "...").appendHtmlTag("/tr");
 		}
 		result.appendHtml("</tbody></table>");
 		result.appendHtml("</div>");
-
 		result.appendHtml(getClosingTag());
 	}
 }
