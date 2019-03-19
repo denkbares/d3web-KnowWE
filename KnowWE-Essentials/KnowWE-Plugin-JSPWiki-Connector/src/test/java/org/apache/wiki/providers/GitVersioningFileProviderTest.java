9b48a24c770da98a0a954e3cf58dece51a34ec17
Josua Nürnberger <jnuernberger@email.de>
2019-03-19 16:02:25 +0100

    Corrected test to empty list not null

--- KnowWE-Essentials/KnowWE-Plugin-JSPWiki-Connector/src/test/java/org/apache/wiki/providers/GitVersioningFileProviderTest.java
+++ KnowWE-Essentials/KnowWE-Plugin-JSPWiki-Connector/src/test/java/org/apache/wiki/providers/GitVersioningFileProviderTest.java
@@ -47,7 +47,8 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 
 /**
@@ -228,7 +229,7 @@ public class GitVersioningFileProviderTest {
 
 		fileProvider.deletePage("test");
 		allChangedSince = fileProvider.getVersionHistory("test");
-		assertNull("deleted pages have no version log anymore", allChangedSince);
+		assertTrue("deleted pages have no version log anymore", allChangedSince.isEmpty());
 	}
 
 	@NotNull WikiEngine getWikiEngineMock(String author) throws NoSuchPrincipalException {
