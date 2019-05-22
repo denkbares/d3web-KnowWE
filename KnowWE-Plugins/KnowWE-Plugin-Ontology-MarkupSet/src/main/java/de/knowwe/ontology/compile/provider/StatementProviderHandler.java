49b373bc9dd836e4df7df561dd45b99349cab31c
Sven Hesselbach <sven.hesselbach@servicemate.de>
2019-05-22 18:46:03 +0200

    pimped turtle markup: rdf won't be compiled when "compile" is set to false

--- KnowWE-Plugins/KnowWE-Plugin-Ontology-MarkupSet/src/main/java/de/knowwe/ontology/compile/provider/StatementProviderHandler.java
+++ KnowWE-Plugins/KnowWE-Plugin-Ontology-MarkupSet/src/main/java/de/knowwe/ontology/compile/provider/StatementProviderHandler.java
@@ -12,14 +12,24 @@ import de.knowwe.core.kdom.Type;
 import de.knowwe.core.kdom.parsing.Section;
 import de.knowwe.core.kdom.parsing.Sections;
 import de.knowwe.core.report.Message;
+import de.knowwe.kdom.defaultMarkup.DefaultMarkupType;
 import de.knowwe.ontology.compile.OntologyCompiler;
 import de.knowwe.ontology.compile.OntologyHandler;
+import de.knowwe.ontology.turtle.TurtleMarkup;
+
+import static de.knowwe.core.kdom.parsing.Sections.$;
 
 public class StatementProviderHandler<Z extends Type> extends OntologyHandler<Z> {
 
 	@Override
 	public Collection<Message> create(OntologyCompiler compiler, Section<Z> section) {
 		List<Message> messages = new ArrayList<>();
+
+		String compile = $(section).ancestor(TurtleMarkup.class)
+				.mapFirst(s -> DefaultMarkupType.getAnnotation(s, TurtleMarkup.COMPILE));
+		if ("false".equals(compile)) {
+			return messages;
+		}
 
 		for (Section<StatementProvider> statementSection : Sections.successors(section, StatementProvider.class)) {
 			//noinspection unchecked
--- KnowWE-Plugins/KnowWE-Plugin-Ontology-MarkupSet/src/main/java/de/knowwe/ontology/turtle/TurtleMarkup.java
+++ KnowWE-Plugins/KnowWE-Plugin-Ontology-MarkupSet/src/main/java/de/knowwe/ontology/turtle/TurtleMarkup.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (C) 2013 denkbares GmbH
- * 
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -46,6 +46,7 @@ public class TurtleMarkup extends DefaultMarkupType {
 			new QuoteSet('[', ']'), new QuoteSet('<', '>') };
 
 	private static final DefaultMarkup MARKUP;
+	public static final String COMPILE = "compile";
 
 	static {
 		MARKUP = new DefaultMarkup("Turtle");
@@ -53,6 +54,7 @@ public class TurtleMarkup extends DefaultMarkupType {
 		MARKUP.ignoreAnnotation("prefix");
 		MARKUP.addContentType(new TurtlePrefixType());
 		MARKUP.addContentType(new TurtleContent());
+		MARKUP.addAnnotation(COMPILE, false, "true", "false");
 	}
 
 	public TurtleMarkup() {
