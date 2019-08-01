052988bf0ff162c6cc85ebd288cf9d5214737610
Albrecht Striffler <albrecht.striffler@denkbares.com>
2019-08-01 11:07:36 +0200

    Cleanup and javadoc.

--- KnowWE-Essentials/KnowWE-core/src/main/java/de/knowwe/core/kdom/sectionFinder/SectionFinder.java
+++ KnowWE-Essentials/KnowWE-core/src/main/java/de/knowwe/core/kdom/sectionFinder/SectionFinder.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (C) 2010 University Wuerzburg, Computer Science VI
- * 
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -24,28 +24,27 @@ import de.knowwe.core.kdom.Type;
 import de.knowwe.core.kdom.parsing.Section;
 
 /**
- * 
+ * Interface that can be implemented and added to a {@link Type}. It will be used to find {@link Section}s with that {@link
+ * Type} in the text of the parent {@link Section}.
+ *
  * @author Jochen
  * @created 21.07.2010
  */
+@FunctionalInterface
 public interface SectionFinder {
 
 	/**
-	 * 
-	 * Allocates text parts for the type owning this sectionfinder. The
-	 * resulting SectionFinderResult list contains indices of substrings of the
-	 * passed text. These specified substrings will be allocated to this type.
-	 * Method will be called multiple times with various article fragments
-	 * depending on previous allocations of preceding types. If no interesting
-	 * section is found in a passed fragment, return 'null' or an empty list;
-	 * 
-	 * @param text Text fragment of the wiki article source
-	 * @param father the section that should be further divided in subsection
-	 * @param type the type that this sections finder is called for; from the result sections of this type will be created
-	 * @return List of SectionFinderResults with informations about what part of
-	 *         the next belongs to the ObjectType calling the SectionFinder
+	 * Allocates text parts for the type owning this SectionFinder. The resulting SectionFinderResult list contains
+	 * indices of substrings of the passed text. These specified substrings will be allocated to this type. Method will
+	 * be called multiple times with various article fragments depending on previous allocations of preceding types. If
+	 * no interesting section is found in a passed fragment, return 'null' or an empty list;
+	 *
+	 * @param text   Text fragment of the wiki article source
+	 * @param parent the section that should be further divided in subsection
+	 * @param type   the type that this sections finder is called for; from the result sections of this type will be
+	 *               created
+	 * @return List of SectionFinderResults with informations about what part of the next belongs to the ObjectType
+	 * calling the SectionFinder
 	 */
-	List<SectionFinderResult> lookForSections(
-			String text, Section<?> father, Type type);
-
+	List<SectionFinderResult> lookForSections(String text, Section<?> parent, Type type);
 }
