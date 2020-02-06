3d3b5a1a2a92123f406472f12f3d39d2e7baf59f
Albrecht Striffler <albrecht.striffler@denkbares.com>
2020-02-06 15:51:40 +0100

    Avoid NPE

--- KnowWE-Plugins/KnowWE-Plugin-RightPanel/src/main/java/de/knowwe/rightpanel/watches/AddToWatchesToolProvider.java
+++ KnowWE-Plugins/KnowWE-Plugin-RightPanel/src/main/java/de/knowwe/rightpanel/watches/AddToWatchesToolProvider.java
@@ -21,6 +21,8 @@ package de.knowwe.rightpanel.watches;
 
 import com.denkbares.strings.Identifier;
 import com.denkbares.strings.Strings;
+import de.knowwe.core.compile.Compilers;
+import de.knowwe.core.compile.terminology.TermCompiler;
 import de.knowwe.core.kdom.objects.Term;
 import de.knowwe.core.kdom.parsing.Section;
 import de.knowwe.core.kdom.parsing.Sections;
@@ -47,8 +49,15 @@ public class AddToWatchesToolProvider implements ToolProvider {
 
 	@Override
 	public boolean hasTools(Section<?> section, UserContext userContext) {
-		return (section.get() instanceof Term
-				&& ((Term) section.get()).getTermIdentifier(Sections.cast(section, Term.class)) != null);
+		if (section.get() instanceof Term) {
+			final Section<Term> termSection = Sections.cast(section, Term.class);
+			return termSection.get().getTermIdentifier(getTermCompiler(section), termSection) != null;
+		}
+		return false;
+	}
+
+	public TermCompiler getTermCompiler(Section<?> section) {
+		return Compilers.getCompilersWithCompileScript(section, TermCompiler.class).stream().findAny().orElse(null);
 	}
 
 	protected Tool getAddToWatchesTool(Section<? extends Term> section) {
@@ -60,8 +69,7 @@ public class AddToWatchesToolProvider implements ToolProvider {
 	}
 
 	protected String createAddToWatchesAction(Section<? extends Term> section) {
-		Identifier termIdentifier = section.get()
-				.getTermIdentifier(Sections.cast(section, Term.class));
+		Identifier termIdentifier = section.get().getTermIdentifier(getTermCompiler(section), section);
 		return "KNOWWE.core.plugin.rightPanel.watches.addToWatches('" + Strings.encodeHtml(termIdentifier.toExternalForm()) + "')";
 	}
 }
