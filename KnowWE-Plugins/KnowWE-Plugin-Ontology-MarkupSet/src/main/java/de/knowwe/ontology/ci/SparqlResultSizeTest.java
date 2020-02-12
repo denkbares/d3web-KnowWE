1bb00dd0c69f4009e9c67ec1a10b41ec4922da0b
Stefan Olbrecht <stefan.olbrecht@servicemate.de>
2020-02-12 16:59:19 +0100

    Refactor code

--- KnowWE-Plugins/KnowWE-Plugin-Ontology-MarkupSet/src/main/java/de/knowwe/ontology/ci/SparqlResultSizeTest.java
+++ KnowWE-Plugins/KnowWE-Plugin-Ontology-MarkupSet/src/main/java/de/knowwe/ontology/ci/SparqlResultSizeTest.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (C) 2014 denkbares GmbH
- * 
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -33,12 +33,11 @@ import de.knowwe.rdf2go.utils.Rdf2GoUtils;
  * This test allows to verify the size of the result of a specific sparql query
  * (referenced by name). In that way one for instance can ensure the an ontology
  * contains at least/at most/exactly x instances of some class.
- * 
+ *
  * @author Jochen Reutelshöfer
  * @created 08.01.2014
  */
-public class SparqlResultSizeTest extends SparqlTests<SparqlQuerySection>{
-
+public class SparqlResultSizeTest extends SparqlTests<SparqlQuerySection> {
 
 	public static final String GREATER_THAN = ">";
 	public static final String SMALLER_THAN = "<";
@@ -53,8 +52,7 @@ public class SparqlResultSizeTest extends SparqlTests<SparqlQuerySection>{
 		this.addParameter("comparator", Mode.Mandatory,
 				"how to compare the result size against the expected size", comparators);
 		this.addParameter("expected size", de.d3web.testing.TestParameter.Type.Number,
-				Mode.Mandatory,
-				"expected size to compare with");
+				Mode.Mandatory, "expected size to compare with");
 		this.addParameter("warning", Mode.Optional, "show warning instead of failure if this test fails", WARNING);
 	}
 
@@ -74,10 +72,9 @@ public class SparqlResultSizeTest extends SparqlTests<SparqlQuerySection>{
 		double number = Double.parseDouble(args[1]);
 
 		Message.Type messageTypeTestFailed = Message.Type.FAILURE;
-		if (args.length > 2 && args[2] != null && WARNING.equalsIgnoreCase(args[2])){
+		if (args.length > 2 && args[2] != null && WARNING.equalsIgnoreCase(args[2])) {
 			messageTypeTestFailed = Message.Type.WARNING;
 		}
-
 
 		Rdf2GoCore core = Rdf2GoUtils.getRdf2GoCoreForDefaultMarkupSubSection(query.getSection());
 
@@ -94,7 +91,7 @@ public class SparqlResultSizeTest extends SparqlTests<SparqlQuerySection>{
 		int count = 0;
 		while (iterator.hasNext()) {
 			BindingSet binding = iterator.next();
-			if(binding.size() == 0) {
+			if (binding.size() == 0) {
 				// invalid empty row - not actually a result
 				continue;
 			}
@@ -134,6 +131,5 @@ public class SparqlResultSizeTest extends SparqlTests<SparqlQuerySection>{
 
 		return new Message(Message.Type.ERROR,
 				"Unknown error in test (invalid comparator?)");
-
 	}
 }
