9d4e59d7e6b76003d01388cef06f8864ad696057
Albrecht Striffler <albrecht.striffler@denkbares.com>
2020-04-15 11:27:01 +0200

    Javadoc

--- KnowWE-Essentials/KnowWE-core/src/main/java/de/knowwe/core/compile/CompilationLocal.java
+++ KnowWE-Essentials/KnowWE-core/src/main/java/de/knowwe/core/compile/CompilationLocal.java
@@ -103,7 +103,8 @@ public final class CompilationLocal<E> {
 	/**
 	 * Get the object provided by the supplier either freshly generated, or if still valid, from the cache. The object
 	 * stays valid until a new compilation is started by the given compilation manager. As soon as the object provided
-	 * as the cacheKey is no longer used elsewhere and garbage collected, the cached object is also cleaned up.
+	 * as the cacheKey is no longer used/referenced elsewhere and garbage collected, the cached object is also cleaned
+	 * up.
 	 *
 	 * @param cacheKey        the object by which the supplier/result is cached
 	 * @param compilerManager the CompilerManager the cache refers to
