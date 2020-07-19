f6135ecd62cfb5ce2f9513fd10d13008e3e58859
Friedrich Fell <friedrich.fell@servicemate.de>
2020-07-19 22:57:48 +0200

    fixed attachment handling

--- KnowWE-Plugins/KnowWE-Plugin-DropZone/src/main/resources/webapp/KnowWEExtension/scripts/KnowWE-Plugin-DropZone.js
+++ KnowWE-Plugins/KnowWE-Plugin-DropZone/src/main/resources/webapp/KnowWEExtension/scripts/KnowWE-Plugin-DropZone.js
@@ -1,20 +1,5 @@
 /*
- * Copyright (C) 2018 denkbares GmbH, Germany
- *
- * This is free software; you can redistribute it and/or modify it under the
- * terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 3 of the License, or (at your option) any
- * later version.
- *
- * This software is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this software; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
- * site: http://www.fsf.org.
+ * Copyright (C) 2020 denkbares GmbH. All rights reserved.
  */
 
 KNOWWE = typeof KNOWWE === "undefined" ? {} : KNOWWE;
@@ -22,210 +7,223 @@ KNOWWE.core = KNOWWE.core || {};
 KNOWWE.core.plugin = KNOWWE.core.plugin || {};
 
 (function init() {
-	window.addEvent('domready', _KL.setup);
-	if (window.location.search !== "?page=Create%20new%20EDB%20entry") if (KNOWWE.helper.loadCheck(['Wiki.jsp'])) {
-		window.addEvent('domready', function () {
-			KNOWWE.core.plugin.dropZone.initAttachToExisting();
-		});
-	}
+  window.addEvent('domready', _KL.setup);
+  if (window.location.search !== "?page=Create%20new%20EDB%20entry") if (KNOWWE.helper.loadCheck(['Wiki.jsp'])) {
+    window.addEvent('domready', function() {
+      KNOWWE.core.plugin.dropZone.initAttachToExisting();
+    });
+  }
 }());
 
 
 /**
  * Namespace: KNOWWE.core.plugin.dropZone for debugging d3web expressions in KnowWE
  */
-KNOWWE.core.plugin.dropZone = function () {
+KNOWWE.core.plugin.dropZone = function() {
 
-	function handleDragOver(event) {
-		if (!isEventWithFiles(event)) return;
-		event.stopPropagation();
-		event.preventDefault();
-		setDragOverStyle(event.target);
-		const dropIndicator = jq$(event.target).closest('.dropzone').find(".drop-indicator").first();
-		if (dropIndicator.length === 0) return;
-		dropIndicator[0].addEventListener('dragleave', handleDragLeave);
-		event.dataTransfer.dropEffect = 'copy';
-	}
+  function manualUpload(event) {
+    // const uploadData = prepareUploadData(event);
+    // ajaxData(uploadData, event);
+  }
+
+  function handleDragOver(event) {
+    if (!isEventWithFiles(event)) return;
+    event.stopPropagation();
+    event.preventDefault();
+    setDragOverStyle(event.target);
+    const dropIndicator = jq$(event.target).closest('.dropzone').find(".drop-indicator").first();
+    if (dropIndicator.length === 0) return;
+    dropIndicator[0].addEventListener('dragleave', handleDragLeave);
+    event.dataTransfer.dropEffect = 'copy';
+  }
 
 
-	function handleDragLeave(event) {
-		resetStyle(event.target);
-	}
+  function handleDragLeave(event) {
+    resetStyle(event.target);
+  }
 
-	function prepareUploadData(event, pageName) {
-		if (!isEventWithFiles(event)) return;
-		event.stopPropagation();
-		event.preventDefault();
-		let form = jq$(event.target).closest('.dropzone').find('.drop-indicator').first();
-		const ajaxData = new FormData();
-		if (pageName == null) pageName = KNOWWE.helper.getPagename();
-		ajaxData.append('page', pageName);
-		ajaxData.append('nextpage', 'Upload.jsp?page=' + pageName);
-		ajaxData.append('action', 'upload');
+  function prepareUploadData(event, pageName) {
+    if (!isEventWithFiles(event)) return;
+    event.stopPropagation();
+    event.preventDefault();
+    let form = jq$(event.target).closest('.dropzone').find('.drop-indicator').first();
+    const ajaxData = new FormData();
+    if (pageName == null) pageName = KNOWWE.helper.getPagename();
+    ajaxData.append('page', pageName);
+    ajaxData.append('nextpage', 'Upload.jsp?page=' + pageName);
+    ajaxData.append('action', 'upload');
 
-		let files = [];
-		if (typeof event.dataTransfer !== "undefined") {
-			files = event.dataTransfer.files; // Dropped files
-		}
-		const input = form.find('input[type="file"]').first();
-		if (files.length === 0) {
-			if (typeof input.context.files !== "undefined" && input.context.files.length > 0) {
-				files.append(input.context.files); // Manually chosen files w/ file chooser
-			} else {
-				setClass(event.target, "uploading", "Not a valid attachment...");
-				setTimeout(function () {
-					resetStyle(event.target, "Drop attachment(s) here");
-				}, 1000);
-				return null;
-			}
-		}
+    let files = [];
+    if (typeof event.dataTransfer !== "undefined") {
+      files = event.dataTransfer.files; // Dropped files
+    }
+    const input = form.find('input[type="file"]').first();
+    if (files.length === 0) {
+      if (typeof input.context.files !== "undefined" && input.context.files.length > 0) {
+        files.append(input.context.files); // Manually chosen files w/ file chooser
+      } else {
+        setClass(event.target, "uploading", "Not a valid attachment...");
+        setTimeout(function() {
+          resetStyle(event.target, "Drop attachment(s) here");
+        }, 1000);
+        return null;
+      }
+    }
 
-		setUploadingStyle(event.target);
-		Array.prototype.forEach.call(files, function (file) {
-			ajaxData.append(input.attr('name'), file);
-		});
+    setUploadingStyle(event.target);
+    Array.prototype.forEach.call(files, function(file) {
+      ajaxData.append(input.attr('name'), file);
+    });
 
-		return {
-			form: form,
-			ajaxData: ajaxData
-		}
-	}
+    return {
+      form: form,
+      ajaxData: ajaxData
+    };
+  }
 
-	function isEventWithFiles(event) {
-		var temp = (event.originalEvent || event).dataTransfer;
-		return temp && (temp = temp.types) && (temp.indexOf('Files') !== -1);
-	}
+  function isEventWithFiles(event) {
+    var temp = (event.originalEvent || event).dataTransfer;
+    return temp && (temp = temp.types) && (temp.indexOf('Files') !== -1);
+  }
 
-	function ajaxData(uploadData, event) {
-		jq$.ajax({
-			url: uploadData.form.attr('action'),
-			type: uploadData.form.attr('method'),
-			data: uploadData.ajaxData,
-			cache: false,
-			contentType: false,
-			processData: false,
-			success: function () {
-				setUploadedStyle(event.target);
-				if (!event.reload) {
-					setTimeout(function () {
-						resetStyle(event.target, "Drop attachment(s) here")
-					}, 1000)
-					if (event.callback) event.callback();
-				} else {
-					setTimeout(function () {
-						window.location.reload();
-					}, 1000);
-				}
-			},
-			error: function (data) {
-				KNOWWE.notification.error(data.responseText);
-				resetStyle(event.target, "Drop attachment(s) here");
-			}
-		});
-	}
+  function ajaxData(uploadData, event) {
+    uploadFile(uploadData.ajaxData, uploadData.form, function() {
+      setUploadedStyle(event.target);
+      if (!event.reload) {
+        setTimeout(function() {
+          resetStyle(event.target, "Drop attachment(s) here");
+        }, 1000);
+        if (event.callback) event.callback();
+      } else {
+        setTimeout(function() {
+          window.location.reload();
+        }, 1000);
+      }
+    }, function(data) {
+      KNOWWE.notification.error(data.responseText);
+      resetStyle(event.target, "Drop attachment(s) here");
+    });
+  }
 
-	function handleDropToExisting(event) {
-		if (!isEventWithFiles(event)) return;
-		event.reload = true;
-		const uploadData = prepareUploadData(event)
-		ajaxData(uploadData, event);
-	}
+  function uploadFile(data, form, successCallback, errorCallback) {
+    jq$.ajax({
+      url: form.attr("action"),
+      type: form.attr("method"),
+      data: data,
+      cache: false,
+      contentType: false,
+      processData: false,
+      success: successCallback,
+      error: errorCallback
+    });
+  }
 
-	function setUploadingStyle(element) {
-		setClass(element, "uploading", "Upload in progress...");
-	}
+  function handleDropToExisting(event) {
+    if (!isEventWithFiles(event)) return;
+    event.reload = true;
+    const uploadData = prepareUploadData(event);
+    ajaxData(uploadData, event);
+  }
 
-	function setDragOverStyle(element) {
-		setClass(element, "dragover", null);
-	}
+  function setUploadingStyle(element) {
+    setClass(element, "uploading", "Upload in progress...");
+  }
 
-	function setUploadedStyle(element) {
-		setClass(element, "uploaded", "Upload complete 🎉");
-	}
+  function setDragOverStyle(element) {
+    setClass(element, "dragover", null);
+  }
 
-	function resetStyle(element, title) {
-		setClass(element, "", title);
-	}
+  function setUploadedStyle(element) {
+    setClass(element, "uploaded", "Upload complete 🎉");
+  }
 
-	function setClass(element, className, title) {
-		const dropZone = jq$(element).closest('.dropzone');
-		const boxInput = dropZone.find(".box-input").first();
-		boxInput.attr('class', 'box-input');
-		boxInput.addClass(className);
-		const dropIndicator = dropZone.find('.drop-indicator').first();
-		dropIndicator.attr('class', 'drop-indicator'); // Remove all other classes
-		dropIndicator.addClass(className);
-		const dropTextWrapper = dropIndicator.find("label").first();
-		if (title !== null) dropTextWrapper.html(title);
-		dropTextWrapper.removeClass()
-		dropTextWrapper.addClass(className);
-	}
+  function resetStyle(element, title) {
+    setClass(element, "", title);
+  }
 
-	function attachDropZoneToElement(element, actionUrl, multiple, title, mode) {
-		if (element == null || typeof element === "undefined" || element.length === 0) return;
-		const form =
-			'<form class="drop-indicator" method="post" action=' + actionUrl + ' enctype="multipart/form-data">' +
-			'  <div class="box-input">' +
-			'    <input style="display: none" class="box__file" type="file" name="files" id="file" ' +
-			(multiple ? 'data-multiple-caption="{count} files selected" multiple' : '') + ' />' +
-			'    <label for="file"><span class="box__dragndrop"/>' + title + '</span></label>' +
-			'  </div>' +
-			'</form>';
+  function setClass(element, className, title) {
+    const dropZone = jq$(element).closest('.dropzone');
+    const boxInput = dropZone.find(".box-input").first();
+    boxInput.attr('class', 'box-input');
+    boxInput.addClass(className);
+    const dropIndicator = dropZone.find('.drop-indicator').first();
+    dropIndicator.attr('class', 'drop-indicator'); // Remove all other classes
+    dropIndicator.addClass(className);
+    const dropTextWrapper = dropIndicator.find("label").first();
+    if (title !== null) dropTextWrapper.html(title);
+    dropTextWrapper.removeClass();
+    dropTextWrapper.addClass(className);
+  }
 
-		if (mode === "full-height") {
-			element.addClass("dropzone full-height");
-			element.prepend(form);
-		} else if (mode === "append") {
-			element.append('<div class="dropzone">' + form + '</div>');
-		} else if (mode === "replace") {
-			element.addClass("dropzone replace");
-			element.prepend(form);
-		}
-	}
+  function attachDropZoneToElement(element, actionUrl, multiple, title, mode) {
+    if (element == null || typeof element === "undefined" || element.length === 0) return;
+    const form =
+      '<form class="drop-indicator" method="post" action=' + actionUrl + ' enctype="multipart/form-data">' +
+      '  <div class="box-input">' +
+      '    <input style="display: none" class="box__file" type="file" name="files" id="file" ' +
+      (multiple ? 'data-multiple-caption="{count} files selected" multiple' : '') + ' />' +
+      '    <label for="file"><span class="box__dragndrop"/>' + title + '</span></label>' +
+      '  </div>' +
+      '</form>';
 
-	return {
+    if (mode === "full-height") {
+      element.addClass("dropzone full-height");
+      element.prepend(form);
+    } else if (mode === "append") {
+      element.append('<div class="dropzone">' + form + '</div>');
+    } else if (mode === "replace") {
+      element.addClass("dropzone replace");
+      element.prepend(form);
+    }
+  }
 
-		prepareUploadData: function (event, name) {
-			return prepareUploadData(event, name);
-		},
+  return {
 
-		uploadData: function (data, event) {
-			ajaxData(data, event)
-		},
+    prepareUploadData: function(event, name) {
+      return prepareUploadData(event, name);
+    },
 
-		setDropZoneStyleUploading: function (element) {
-			setUploadingStyle(element);
-		},
+    uploadData: function(data, event) {
+      ajaxData(data, event);
+    },
 
-		setDropZoneStyleUploaded: function (element) {
-			setUploadedStyle(element)
-		},
+    uploadFile: function(data, form, successCallback, errorCallback) {
+      uploadFile(data, form, successCallback, errorCallback);
+    },
 
-		resetDropZoneStyle: function (element, title) {
-			resetStyle(element, title);
-		},
+    setDropZoneStyleUploading: function(element) {
+      setUploadingStyle(element);
+    },
 
-		initAttachToExisting: function () {
-			KNOWWE.core.plugin.dropZone.addDropZoneTo('div.page', "Drop attachment(s)", handleDropToExisting)
-		},
+    setDropZoneStyleUploaded: function(element) {
+      setUploadedStyle(element);
+    },
 
-		addDropZoneTo: function (elementSelector, title, dropHandlerCallback, actionUrl, mode, multiple) {
-			if (!actionUrl) actionUrl = 'attach';
-			if (!mode) mode = "full-height";
-			if (typeof multiple === "undefined") multiple = true;
-			const canWrite = jq$('#knowWEInfoCanWrite').attr('value');
-			if (!KNOWWE.core.util.isHaddockTemplate() || typeof canWrite === 'undefined' || canWrite !== 'true') return;
+    resetDropZoneStyle: function(element, title) {
+      resetStyle(element, title);
+    },
 
-			const elements = jq$(elementSelector);
-			attachDropZoneToElement(elements, actionUrl, multiple, title, mode);
+    initAttachToExisting: function() {
+      KNOWWE.core.plugin.dropZone.addDropZoneTo('div.page', "Drop attachment(s)", handleDropToExisting)
+    },
 
-			elements.each(function () {
-				this.addEventListener('dragover', handleDragOver);
-				this.addEventListener('drop', dropHandlerCallback);
-			});
-			elements.find('input').first().on('change', dropHandlerCallback);
-		}
-	}
+    addDropZoneTo: function(elementSelector, title, dropHandlerCallback, actionUrl, mode, multiple) {
+      if (!actionUrl) actionUrl = 'attach';
+      if (!mode) mode = "full-height";
+      if (typeof multiple === "undefined") multiple = true;
+      const canWrite = jq$('#knowWEInfoCanWrite').attr('value');
+      if (!KNOWWE.core.util.isHaddockTemplate() || typeof canWrite === 'undefined' || canWrite !== 'true') return;
+
+      const elements = jq$(elementSelector);
+      attachDropZoneToElement(elements, actionUrl, multiple, title, mode);
+
+      elements.each(function() {
+        this.addEventListener('dragover', handleDragOver);
+        this.addEventListener('drop', dropHandlerCallback);
+      });
+      elements.find('input').first().on('change', dropHandlerCallback);
+    }
+  };
 
 }
 ();
