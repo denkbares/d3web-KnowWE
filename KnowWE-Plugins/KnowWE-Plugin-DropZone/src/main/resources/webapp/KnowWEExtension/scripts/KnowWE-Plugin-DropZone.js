625a60989e04f1552a17ae27cd35a298f1b49606
Friedrich Fell <friedrich.fell@servicemate.de>
2020-08-05 13:30:35 +0200

    readded open source copyright

--- KnowWE-Plugins/KnowWE-Plugin-DropZone/src/main/resources/webapp/KnowWEExtension/scripts/KnowWE-Plugin-DropZone.js
+++ KnowWE-Plugins/KnowWE-Plugin-DropZone/src/main/resources/webapp/KnowWEExtension/scripts/KnowWE-Plugin-DropZone.js
@@ -1,5 +1,20 @@
 /*
- * Copyright (C) 2020 denkbares GmbH. All rights reserved.
+ * Copyright (C) 2020 denkbares GmbH, Germany
+ *
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
  */
 
 KNOWWE = typeof KNOWWE === "undefined" ? {} : KNOWWE;
