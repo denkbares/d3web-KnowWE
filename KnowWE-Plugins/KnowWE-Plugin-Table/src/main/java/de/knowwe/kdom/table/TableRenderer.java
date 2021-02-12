9187180b11b3e3ef4f5d57e7b98a90b448387215
Albrecht Striffler <albrecht.striffler@denkbares.com>
2021-02-13 00:33:36 +0100

    TableRenderer now has sticky headers

--- KnowWE-Plugins/KnowWE-Plugin-Table/src/main/java/de/knowwe/kdom/table/TableRenderer.java
+++ KnowWE-Plugins/KnowWE-Plugin-Table/src/main/java/de/knowwe/kdom/table/TableRenderer.java
@@ -1,17 +1,17 @@
 /*
  * Copyright (C) 2009 Chair of Artificial Intelligence and Applied Informatics
  * Computer Science VI, University of Wuerzburg
- * 
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -20,17 +20,22 @@
 
 package de.knowwe.kdom.table;
 
+import java.util.List;
+
+import org.jetbrains.annotations.NotNull;
+
 import de.knowwe.core.kdom.parsing.Section;
-import de.knowwe.core.kdom.rendering.DelegateRenderer;
 import de.knowwe.core.kdom.rendering.RenderResult;
 import de.knowwe.core.kdom.rendering.Renderer;
 import de.knowwe.core.user.UserContext;
+
+import static de.knowwe.core.kdom.parsing.Sections.$;
 
 /**
  * This is a renderer for the Table. It wraps the <code>Table</code> tag into an
  * own DIV and delegates the rendering of each <code>TableCellContent</code> to
  * its own renderer.
- * 
+ *
  * @author smark
  */
 public class TableRenderer implements Renderer {
@@ -49,18 +54,21 @@ public class TableRenderer implements Renderer {
 			string.appendHtml("<div style='overflow:auto;white-space:normal;'>");
 		}
 		string.appendHtml("<table style='border:1px solid #999999;' id='table_" + sec.getID()
-				+ "' class='wikitable knowwetable' border='1'><tbody>");
-		string.appendHtml(getHeader());
-		DelegateRenderer.getInstance().render(sec, user, string);
+				+ "' class='wikitable knowwetable sticky-header' border='1'>");
+		@NotNull List<Section<TableLine>> lines = $(sec).successor(TableLine.class).asList();
+		if (!lines.isEmpty()) {
+			string.appendHtml("<thead>");
+			string.append(lines.get(0), user);
+			string.appendHtml("</thead>");
+		}
+		string.appendHtml("<tbody>");
+		for (Section<TableLine> line : lines.subList(1, lines.size())) {
+			string.append(line, user);
+		}
 		string.appendHtml("</tbody></table>");
 		string.appendHtml("</div>");
 
 		string.appendHtml(getClosingTag());
-
-	}
-
-	protected String getHeader() {
-		return "";
 	}
 
 	protected String getOpeningTag(Section<?> sec) {
@@ -70,5 +78,4 @@ public class TableRenderer implements Renderer {
 	protected String getClosingTag() {
 		return "</div>";
 	}
-
 }
