52e4d68a4e07c79f724e12660b1d359d31b8c615
Albrecht Striffler <albrecht.striffler@denkbares.com>
2021-05-07 18:29:23 +0200

    Refactoring to allow reuse

--- KnowWE-Essentials/KnowWE-Plugin-Core/src/main/java/de/knowwe/core/tools/CopyToClipboardTool.java
+++ KnowWE-Essentials/KnowWE-Plugin-Core/src/main/java/de/knowwe/core/tools/CopyToClipboardTool.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 denkbares GmbH, Germany
+ * Copyright (C) 2021 denkbares GmbH, Germany
  *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
@@ -15,30 +15,25 @@
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
  * site: http://www.fsf.org.
- *
  */
 
 package de.knowwe.core.tools;
 
-import de.knowwe.core.kdom.parsing.Section;
-import de.knowwe.core.user.UserContext;
+import com.denkbares.strings.Strings;
+import de.knowwe.tools.DefaultTool;
 import de.knowwe.tools.Tool;
-import de.knowwe.tools.ToolProvider;
+import de.knowwe.util.Icon;
 
 /**
- * @author Veronika Sehne (denkbares GmbH)
- * @created 2019-10-28
+ * Simple tool allowing to copy a given text into the clipboard of the use
+ *
+ * @author Albrecht Striffler (denkbares GmbH)
+ * @created 07.05.21
  */
-public class CopyToClipboardToolProvider implements ToolProvider {
-	@Override
-	public Tool[] getTools(Section<?> section, UserContext userContext) {
-		return new Tool[] {
-				new CopyToClipboardTool("Copy term to clipboard", "Copies this term to your clipboard.", section.getText())
-		};
-	}
+public class CopyToClipboardTool extends DefaultTool {
 
-	@Override
-	public boolean hasTools(Section<?> section, UserContext userContext) {
-		return true;
+	public CopyToClipboardTool(String title, String description, String text) {
+		super(Icon.COPY_TO_CLIPBOARD, title, description,
+				"jq$(this).copyToClipboard('" + Strings.encodeHtml(text) + "');", Tool.CATEGORY_EDIT);
 	}
 }
