c71a9fbe2683a0b7feef631824514ab0550f7534
Friedrich Fell <friedrich.fell@servicemate.de>
2021-05-28 11:10:35 +0200

    mask double backslash

--- KnowWE-Essentials/KnowWE-core/src/main/java/de/knowwe/core/kdom/rendering/RenderResult.java
+++ KnowWE-Essentials/KnowWE-core/src/main/java/de/knowwe/core/kdom/rendering/RenderResult.java
@@ -1,21 +1,5 @@
 /*
- * Copyright (C) 2010 Chair of Artificial Intelligence and Applied Informatics
- * Computer Science VI, University of Wuerzburg
- *
- * This is free software; you can redistribute it and/or modify it under the
- * terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 3 of the License, or (at your option) any
- * later version.
- *
- * This software is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this software; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
- * site: http://www.fsf.org.
+ * Copyright (C) 2021 denkbares GmbH. All rights reserved.
  */
 package de.knowwe.core.kdom.rendering;
 
@@ -40,7 +24,7 @@ public class RenderResult {
 
 	private static final String storeKey = RenderResult.class.getName();
 	private static final String[] HTML = new String[] {
-			"[{", "}]", "\"", "'", ">", "<", "[", "]" };
+			"[{", "}]", "\\\\", "\"", "'", ">", "<", "[", "]" };
 
 	private final String maskKey;
 	private final String[] maskedHtml;
