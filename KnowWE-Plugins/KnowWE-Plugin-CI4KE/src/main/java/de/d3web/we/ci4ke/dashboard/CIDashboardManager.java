76b770538ebd92c7551606a254902985fecdd350
Sven Hesselbach <sven.hesselbach@servicemate.de>
2021-07-12 13:23:14 +0200

    [KON-5834] add QSReportProvider to generate reports from all CIDashoards

--- KnowWE-Plugins/KnowWE-Plugin-CI4KE/src/main/java/de/d3web/we/ci4ke/dashboard/CIDashboardManager.java
+++ KnowWE-Plugins/KnowWE-Plugin-CI4KE/src/main/java/de/d3web/we/ci4ke/dashboard/CIDashboardManager.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (C) 2013 denkbares GmbH
- * 
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -20,6 +20,7 @@ package de.d3web.we.ci4ke.dashboard;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -34,7 +35,7 @@ import de.knowwe.core.kdom.parsing.Section;
 
 /**
  * Manages and provides {@link CIDashboard}s
- * 
+ *
  * @author Albrecht Striffler (denkbares GmbH)
  * @created 31.12.2013
  */
@@ -49,6 +50,10 @@ public class CIDashboardManager {
 			dashboardsForManagers.put(manager, dashboards);
 		}
 		return dashboards;
+	}
+
+	public static Set<CIDashboard> getAllDashboards(ArticleManager manager) {
+		return Collections.unmodifiableSet(CIDashboardManager.getDashboardsMap(manager).valueSet());
 	}
 
 	public static synchronized CIDashboard generateAndRegisterDashboard(Section<CIDashboardType> section, List<TestSpecification<?>> tests) {
