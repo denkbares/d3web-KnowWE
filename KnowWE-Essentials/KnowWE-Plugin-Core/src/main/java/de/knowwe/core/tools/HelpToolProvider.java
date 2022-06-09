688d65c4e35fb185328c511e5aa2b2d9673d5df8
Albrecht Striffler <albrecht.striffler@denkbares.com>
2022-06-09 14:14:15 +0200

    Use the system or jspwiki property to specify link prefix for help docs

--- KnowWE-Essentials/KnowWE-Plugin-Core/src/main/java/de/knowwe/core/tools/HelpToolProvider.java
+++ KnowWE-Essentials/KnowWE-Plugin-Core/src/main/java/de/knowwe/core/tools/HelpToolProvider.java
@@ -18,6 +18,10 @@
  */
 package de.knowwe.core.tools;
 
+import java.util.ArrayList;
+
+import org.jetbrains.annotations.NotNull;
+
 import de.knowwe.core.ArticleManager;
 import de.knowwe.core.Environment;
 import de.knowwe.core.kdom.Article;
@@ -27,7 +31,6 @@ import de.knowwe.core.utils.KnowWEUtils;
 import de.knowwe.tools.DefaultTool;
 import de.knowwe.tools.Tool;
 import de.knowwe.tools.ToolProvider;
-import de.knowwe.tools.ToolUtils;
 import de.knowwe.util.Icon;
 
 /**
@@ -39,32 +42,52 @@ public class HelpToolProvider implements ToolProvider {
 	public static final String HELP_TOOL_WIKI_PROPERTY = "knowwe.helpTool.externalWiki";
 
 	private String getExternalHelpWikiLink(UserContext context, Section<?> section) {
-		String wikiProperty = Environment.getInstance().getWikiConnector().getWikiProperty(HELP_TOOL_WIKI_PROPERTY);
+		String wikiProperty = System.getProperty(HELP_TOOL_WIKI_PROPERTY, Environment.getInstance()
+				.getWikiConnector()
+				.getWikiProperty(HELP_TOOL_WIKI_PROPERTY));
 		if (wikiProperty == null) return null;
-		return wikiProperty + section.get().getName();
+		return wikiProperty + "Doc " + section.get().getName();
 	}
 
 	@Override
 	public boolean hasTools(Section<?> section, UserContext userContext) {
-		return getDocArticle(section, userContext) != null;
+		return getDocArticle(section, userContext) != null || getExternalHelpWikiLink(userContext, section) != null;
 	}
 
 	@Override
 	public Tool[] getTools(Section<?> section, UserContext userContext) {
 
+		ArrayList<Tool> tools = new ArrayList<>();
+		Tool local = getLocalTool(section, userContext);
+		if (local != null) tools.add(local);
+		Tool external = getExternalTool(section, userContext);
+		if (external != null) tools.add(external);
+
+		return tools.toArray(new Tool[0]);
+	}
+
+	private Tool getExternalTool(Section<?> section, UserContext userContext) {
+		String externalHelpWikiLink = getExternalHelpWikiLink(userContext, section);
+		if (externalHelpWikiLink == null) return null;
+		return toolWithLink(section, externalHelpWikiLink);
+	}
+
+	private Tool getLocalTool(Section<?> section, UserContext userContext) {
 		Article article = getDocArticle(section, userContext);
-		if (article == null) return ToolUtils.emptyToolArray();
-
+		if (article == null) return null;
 		String link = KnowWEUtils.getURLLink(article);
+		return toolWithLink(section, link);
+	}
 
-		String js = "window.location.href = '" + link + "';";
-		Tool help = new DefaultTool(
+	@NotNull
+	private DefaultTool toolWithLink(Section<?> section, String link) {
+		return new DefaultTool(
 				Icon.HELP,
 				"Help: " + section.get().getName(),
 				"Open help page for this markup.",
-				js,
+				link,
+				Tool.ActionType.HREF,
 				Tool.CATEGORY_HELP);
-		return new Tool[] { help };
 	}
 
 	private Article getDocArticle(Section<?> section, UserContext userContext) {
