05a73aace9b57ed887ce846d176f23e23d7f8484
Albrecht Striffler <albrecht.striffler@denkbares.com>
2023-05-12 10:08:54 +0200

    Make CheckTypeHandler incremental

--- KnowWE-Plugins/KnowWE-Plugin-d3web-MarkupSet/src/main/java/de/d3web/we/kdom/action/QASetIndicationAction.java
+++ KnowWE-Plugins/KnowWE-Plugin-d3web-MarkupSet/src/main/java/de/d3web/we/kdom/action/QASetIndicationAction.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (C) 2010 University Wuerzburg, Computer Science VI
- * 
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -41,6 +41,7 @@ import de.knowwe.core.kdom.parsing.Section;
 import de.knowwe.core.kdom.parsing.Sections;
 import de.knowwe.core.kdom.sectionFinder.AllTextFinderTrimmed;
 import de.knowwe.core.report.CompilerMessage;
+import de.knowwe.core.report.Messages;
 import de.knowwe.kdom.AnonymousType;
 
 /**
@@ -57,7 +58,6 @@ public class QASetIndicationAction extends D3webRuleAction<QASetIndicationAction
 		qasetType.addCompileScript(new CheckTypeHandler());
 
 		this.addChildType(qasetType);
-
 	}
 
 	@Override
@@ -93,17 +93,30 @@ public class QASetIndicationAction extends D3webRuleAction<QASetIndicationAction
 					Section<? extends Term> simpleDef = (Section<? extends Term>) termDefinitionSection;
 					Class<?> objectClazz = simpleDef.get().getTermObjectClass(compiler, simpleDef);
 					if (Question.class.isAssignableFrom(objectClazz) || QContainer.class.isAssignableFrom(objectClazz)) {
-						throw new CompilerMessage();
+						Messages.clearMessages(compiler, s, this.getClass());
+						return;
 					}
-					throw CompilerMessage.error(Strings.trimQuotes(termIdentifier.toPrettyPrint()) + "is defined as: "
-							+ objectClazz.getName()
-							+ " - expected was Question or Questionnaire");
+					Messages.storeMessage(compiler, s, this.getClass(),
+							Messages.error(Strings.trimQuotes(termIdentifier.toPrettyPrint())
+										   + "is defined as: " + objectClazz.getName()
+										   + " - expected was Question or Questionnaire"));
+					return;
 				}
 			}
-			throw CompilerMessage.error("Could not find '"
-					+ Strings.trimQuotes(termIdentifier.toPrettyPrint())
-					+ "' - expected was Question or Questionnaire");
+			Messages.storeMessage(compiler, s, this.getClass(),
+					Messages.error("Could not find '"
+								   + Strings.trimQuotes(termIdentifier.toPrettyPrint())
+								   + "' - expected was Question or Questionnaire"));
+		}
+
+		@Override
+		public void destroy(D3webCompiler compiler, Section<AnonymousType> section) {
+			Messages.clearMessages(compiler, section, this.getClass());
+		}
+
+		@Override
+		public boolean isIncrementalCompilationSupported(Section<AnonymousType> section) {
+			return true;
 		}
 	}
-
 }
