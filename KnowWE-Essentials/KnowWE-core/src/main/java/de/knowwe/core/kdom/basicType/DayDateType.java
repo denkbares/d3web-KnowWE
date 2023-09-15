12cb1b6baf078f80b321a0989aefc65cc68cc6e6
Veronika Oschmann <veronika.oschmann@denkbares.com>
2023-09-15 10:17:26 +0200

    ILMERED-508: Add milestones to progress markup and list them in the data sheet

--- KnowWE-Essentials/KnowWE-core/src/main/java/de/knowwe/core/kdom/basicType/DayDateType.java
+++ KnowWE-Essentials/KnowWE-core/src/main/java/de/knowwe/core/kdom/basicType/DayDateType.java
@@ -1,18 +1,18 @@
 package de.knowwe.core.kdom.basicType;
 
-import de.knowwe.core.compile.DefaultGlobalCompiler;
-import de.knowwe.core.kdom.AbstractType;
-import de.knowwe.core.kdom.parsing.Section;
-import de.knowwe.core.kdom.sectionFinder.AllTextFinderTrimmed;
-import de.knowwe.core.report.Message;
-import de.knowwe.core.report.Messages;
-
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.regex.Pattern;
+
+import de.knowwe.core.compile.DefaultGlobalCompiler;
+import de.knowwe.core.kdom.AbstractType;
+import de.knowwe.core.kdom.parsing.Section;
+import de.knowwe.core.kdom.sectionFinder.RegexSectionFinder;
+import de.knowwe.core.report.Message;
+import de.knowwe.core.report.Messages;
 
 /**
  * Date type for common date formats without time:
@@ -24,39 +24,39 @@ import java.util.regex.Pattern;
  */
 public class DayDateType extends AbstractType {
 
-    private static final String DATE = "(\\d{4}(-\\d{2}){2}|(\\d{2}\\.){2}\\d{4})";
-    private static final Pattern DATE_PATTERN = Pattern.compile(DATE);
+	private static final String DATE = "(\\d{4}(-\\d{2}){2}|(\\d{2}\\.){2}\\d{4})";
+	private static final Pattern DATE_PATTERN = Pattern.compile(DATE);
 
-    public DayDateType() {
-        setSectionFinder(new AllTextFinderTrimmed());
-        addCompileScript(new DateSubtreeHandler());
-    }
+	public DayDateType() {
+		setSectionFinder(new RegexSectionFinder(DATE_PATTERN));
+		addCompileScript(new DateSubtreeHandler());
+	}
 
-    public static boolean isValid(String sectionText) {
-        return DATE_PATTERN.matcher(sectionText).matches();
-    }
+	public static boolean isValid(String sectionText) {
+		return DATE_PATTERN.matcher(sectionText).matches();
+	}
 
-    public static LocalDate getDate(Section<DayDateType> sec) {
-        return LocalDate.parse(sec.getText());
-    }
+	public static LocalDate getDate(Section<DayDateType> sec) {
+		return LocalDate.parse(sec.getText());
+	}
 
-    public static String getISOLocalDate(Section<DayDateType> sec) {
-        return getDate(sec).format(DateTimeFormatter.ISO_LOCAL_DATE);
-    }
+	public static String getISOLocalDate(Section<DayDateType> sec) {
+		return getDate(sec).format(DateTimeFormatter.ISO_LOCAL_DATE);
+	}
 
+	static class DateSubtreeHandler extends DefaultGlobalCompiler.DefaultGlobalHandler<DayDateType> {
 
-    static class DateSubtreeHandler extends DefaultGlobalCompiler.DefaultGlobalHandler<DayDateType> {
+		@Override
+		public Collection<Message> create(DefaultGlobalCompiler compiler, Section<DayDateType> section) {
+			if (DayDateType.isValid(section.getText())) {
+				return Collections.emptyList();
+			}
+			else {
+				LinkedList<Message> list = new LinkedList<>();
+				list.add(Messages.syntaxError("Invalid date: '" + section.getText() + "'"));
 
-        @Override
-        public Collection<Message> create(DefaultGlobalCompiler compiler, Section<DayDateType> section) {
-            if (DayDateType.isValid(section.getText())) {
-                return Collections.emptyList();
-            } else {
-                LinkedList<Message> list = new LinkedList<>();
-                list.add(Messages.syntaxError("Invalid date: '" + section.getText() + "'"));
-
-                return list;
-            }
-        }
-    }
+				return list;
+			}
+		}
+	}
 }
