93c8bc3f8a5a1672b09c69e96836b336a6bf208b
Stefan Olbrecht <stefan.olbrecht@servicemate.de>
2024-02-22 16:11:32 +0100

    Refactor edit action logic

--- KnowWE-Plugins/KnowWE-Plugin-InstantEdit/src/main/java/de/knowwe/instantedit/actions/CheckCanEditPageAction.java
+++ KnowWE-Plugins/KnowWE-Plugin-InstantEdit/src/main/java/de/knowwe/instantedit/actions/CheckCanEditPageAction.java
@@ -2,30 +2,31 @@ package de.knowwe.instantedit.actions;
 
 import java.io.IOException;
 
+import org.json.JSONObject;
+
 import de.knowwe.core.Environment;
 import de.knowwe.core.action.AbstractAction;
 import de.knowwe.core.action.UserActionContext;
 
 public class CheckCanEditPageAction extends AbstractAction {
 
+	private static final String CAN_EDIT = "canedit";
+
 	@Override
 	public void execute(UserActionContext context) throws IOException {
-
 		String topic = context.getTitle();
-
-		String result = "{\"canedit\":false}";
+		JSONObject response = new JSONObject();
+		response.put(CAN_EDIT, false); // default
 
 		if (Environment.getInstance().getArticle(context.getWeb(), topic) != null) {
-			if (Environment.getInstance().getWikiConnector().userCanEditArticle(
-					topic, context.getRequest())) {
-				result = "{\"canedit\":true}";
+			if (Environment.getInstance().getWikiConnector().userCanEditArticle(topic, context.getRequest())) {
+				response.put(CAN_EDIT, true);
 			}
 		}
 
 		if (context.getWriter() != null) {
 			context.setContentType(HTML);
-			context.getWriter().write(result);
+			response.write(context.getWriter());
 		}
 	}
-
 }
