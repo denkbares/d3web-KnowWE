d76cfcea93a0225ce501f43f4d67d8eb4b905429
Albrecht Striffler <albrecht.striffler@denkbares.com>
2024-06-10 13:18:34 +0200

    Fix header

--- KnowWE-Essentials/KnowWE-Resources/src/main/webapp/KnowWEExtension/scripts/jquery-treeTable.js
+++ KnowWE-Essentials/KnowWE-Resources/src/main/webapp/KnowWEExtension/scripts/jquery-treeTable.js
@@ -1,20 +1,9 @@
 /*
- * Copyright (C) 2014 denkbares GmbH, Germany
+ * jQuery treeTable Plugin 3.0.0
+ * http://ludo.cubicphuse.nl/jquery-treetable
  *
- * This is free software; you can redistribute it and/or modify it under the
- * terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 3 of the License, or (at your option) any
- * later version.
- *
- * This software is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this software; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
- * site: http://www.fsf.org.
+ * Copyright 2013, Ludo van den Boom
+ * Dual licensed under the MIT or GPL Version 2 licenses.
  */
 (function() {
 	var jq$, Node, Tree, methods;
