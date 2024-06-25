2803288cf4cbc0e3a2105f7838e6ca8fd9732d74
jochenreutelshoefer <jochen.reutelshoefer@denkbares.com>
2024-06-25 13:37:33 +0200

    added an Action to replace a page content

---
+++ KnowWE-Essentials/KnowWE-Plugin-Core/src/main/java/de/knowwe/core/action/ReplacePageAction.java
@@ -0,0 +1,48 @@
+package de.knowwe.core.action;
+
+import java.io.IOException;
+
+import com.denkbares.strings.Strings;
+import de.knowwe.core.Attributes;
+import de.knowwe.core.Environment;
+
+public class ReplacePageAction extends AbstractAction {
+
+	private String perform(UserActionContext context) throws IOException {
+		String name = context.getTitle();
+		String newText = context.getParameter(Attributes.TEXT);
+
+		// Check for user access
+		if (!Environment.getInstance().getWikiConnector().userCanEditArticle(name,
+				context.getRequest())) {
+			return "perm";
+		}
+
+		newText = Strings.decodeURL(newText);
+
+		// Remove any extra whitespace that might have gotten appended by
+		// JSPWiki
+		newText = newText.replaceAll("\\s*$", "");
+
+
+		Environment.getInstance()
+				.getWikiConnector()
+				.writeArticleToWikiPersistence(name, newText, context, "DDOS");
+
+		return "success";
+	}
+
+	@Override
+	public void execute(UserActionContext context) throws IOException {
+		String result = perform(context);
+		if (result != null && context.getWriter() != null) {
+			if (result.equals("perm")) {
+				context.sendError(403, "You do not have the permission to edit this page.");
+			}
+			else {
+				context.setContentType(HTML);
+				context.getWriter().write(result);
+			}
+		}
+	}
+}
