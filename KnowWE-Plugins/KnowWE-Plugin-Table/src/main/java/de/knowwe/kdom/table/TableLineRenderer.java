b834752146ef1855b68cfbad327dcbec49e1b52f
Konstantin Herud <konstantin.herud@denkbares.com>
2024-09-13 14:55:23 +0200

    add table line renderer method for line end

--- KnowWE-Plugins/KnowWE-Plugin-Table/src/main/java/de/knowwe/kdom/table/TableLineRenderer.java
+++ KnowWE-Plugins/KnowWE-Plugin-Table/src/main/java/de/knowwe/kdom/table/TableLineRenderer.java
@@ -1,17 +1,17 @@
 /*
  * Copyright (C) 2009 Chair of Artificial Intelligence and Applied Informatics
  * Computer Science VI, University of Wuerzburg
- * 
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -30,7 +30,7 @@ import de.knowwe.core.user.UserContext;
  * This is a renderer for the TableLine. It wraps the <code>TableLine</code>
  * into the according HTML element and delegates the rendering of each
  * <code>TableCell</code> to its own renderer.
- * 
+ *
  * @author smark
  */
 public class TableLineRenderer implements Renderer {
@@ -40,8 +40,7 @@ public class TableLineRenderer implements Renderer {
 
 		renderLineBeginning(sec, user, string);
 		DelegateRenderer.getInstance().render(sec, user, string);
-
-		string.appendHtml("</tr>\n");
+		renderLineEnding(sec, user, string);
 
 	}
 
@@ -59,9 +58,14 @@ public class TableLineRenderer implements Renderer {
 		string.appendHtml(">");
 	}
 
+	@SuppressWarnings("unused") // suppressed since unused parameters might be used in child classes
+	protected void renderLineEnding(Section<?> sec, UserContext user, RenderResult string) {
+		string.appendHtml("</tr>\n");
+	}
+
 	/**
 	 * Returns the CSS-class for the given table line.
-	 * 
+	 *
 	 * @created 16.03.2011
 	 * @return an empty string, if no classes should be assigned to the table
 	 *         line, a string of CSS classes otherwise
