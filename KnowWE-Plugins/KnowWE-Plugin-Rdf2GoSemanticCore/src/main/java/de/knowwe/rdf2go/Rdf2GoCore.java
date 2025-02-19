645328e0bf9f169a44956134a1c06e7f9dc3732c
Albrecht Striffler <albrecht.striffler@denkbares.com>
2025-02-19 14:45:40 +0100

    Don't optimize for max supported parallel connections, may cause deadlocks...

--- KnowWE-Plugins/KnowWE-Plugin-Rdf2GoSemanticCore/src/main/java/de/knowwe/rdf2go/Rdf2GoCore.java
+++ KnowWE-Plugins/KnowWE-Plugin-Rdf2GoSemanticCore/src/main/java/de/knowwe/rdf2go/Rdf2GoCore.java
@@ -280,8 +280,9 @@ public class Rdf2GoCore implements SPARQLEndpoint {
 	}
 
 	private int getMaxSparqlThreadCount(RepositoryConfig reasoning) {
-		final int defaultThreadCount = Math.min(Runtime.getRuntime()
-				.availableProcessors() - 1, reasoning.getNumberOfSupportedParallelConnections());
+		final int defaultThreadCount = Runtime.getRuntime().availableProcessors() - 1;
+//		Math.min(Runtime.getRuntime()
+//				.availableProcessors() - 1, reasoning.getNumberOfSupportedParallelConnections());
 		final String threadCount = System.getProperty(SEMANTICCORE_SPARQL_THREADS_COUNT, String.valueOf(defaultThreadCount));
 		try {
 			return Integer.parseInt(threadCount);
@@ -602,9 +603,9 @@ public class Rdf2GoCore implements SPARQLEndpoint {
 				}
 				else {
 					LOGGER.info("Removed " + removeSize + " statements from and added "
-							+ insertSize
-							+ " statements to " + Rdf2GoCore.class.getSimpleName() + " " + getName() + " in "
-							+ connectionStopwatch.getDisplay() + ".");
+								+ insertSize
+								+ " statements to " + Rdf2GoCore.class.getSimpleName() + " " + getName() + " in "
+								+ connectionStopwatch.getDisplay() + ".");
 				}
 
 				LOGGER.info("Current number of statements in " + Rdf2GoCore.class.getSimpleName() + " " + getName() + ": " + this.statementCache.size() + " cached, " + this.uncachedStatementsCounter + " uncached.");
@@ -1394,8 +1395,8 @@ public class Rdf2GoCore implements SPARQLEndpoint {
 							: preparedSelect.getQueryString()
 							: query;
 					LOGGER.warn("Slow compile time SPARQL query detected. Query finished after "
-							+ stopwatch.getDisplay()
-							+ ": " + Rdf2GoUtils.getReadableQuery(usedQuery, type) + "...");
+								+ stopwatch.getDisplay()
+								+ ": " + Rdf2GoUtils.getReadableQuery(usedQuery, type) + "...");
 				}
 				return result.get();
 			}
@@ -1421,7 +1422,7 @@ public class Rdf2GoCore implements SPARQLEndpoint {
 					}
 				}
 				if (sparqlTask == null
-						|| (sparqlTask.isCancelled() && sparqlTask.getTimeOutMillis() != options.timeoutMillis)) {
+					|| (sparqlTask.isCancelled() && sparqlTask.getTimeOutMillis() != options.timeoutMillis)) {
 					SparqlCallable callable = newSparqlCallable(query, type, options.timeoutMillis, true, preparedAsk, preparedSelect, bindings);
 					sparqlTask = new SparqlTask(callable, options.priority);
 					this.sparqlCache.put(query, sparqlTask);
@@ -1718,7 +1719,7 @@ public class Rdf2GoCore implements SPARQLEndpoint {
 					this.semanticCore.release();
 					if (this.semanticCore.isAllocated()) {
 						LOGGER.warn("Semantic core " + this.semanticCore.getRepositoryId()
-								+ " is still allocated and cannot be shut down, this may be an memory leak.");
+									+ " is still allocated and cannot be shut down, this may be an memory leak.");
 					}
 				}
 				finally {
