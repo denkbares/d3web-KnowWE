3f840f5fb65321c6bd6c2f4169cdbc83a25dff43
Albrecht Striffler <albrecht.striffler@denkbares.com>
2026-02-03 17:16:18 +0100

    Support concurrent access during initialization

--- KnowWE-Essentials/KnowWE-core/src/main/java/de/knowwe/util/CredentialProviders.java
+++ KnowWE-Essentials/KnowWE-core/src/main/java/de/knowwe/util/CredentialProviders.java
@@ -1,6 +1,9 @@
 package de.knowwe.util;
 
 import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import com.denkbares.collections.PriorityList;
 import de.knowwe.plugin.Plugins;
@@ -10,37 +13,35 @@ import de.knowwe.plugin.Plugins;
  * @created 2018-10-12
  */
 public class CredentialProviders {
-	private static PriorityList<Double, CredentialProvider> providers = Plugins.getCredentialProviders();
+	// Einmalig beim Klassenladen initialisierte, unveränderliche, flache Provider-Liste
+	private static final List<CredentialProvider> providers;
 
-	public static String get(String key, CredentialProvider.Credential credential) {
-		final Iterator<PriorityList.Group<Double, CredentialProvider>> iterator = providers.groupIterator();
-
-		while (iterator.hasNext()) {
-			final PriorityList.Group<Double, CredentialProvider> group = iterator.next();
-
-			for (CredentialProvider provider : group) {
-				final String cred = provider.get(key, credential);
-
-				if (cred != null) return cred;
+	static {
+		PriorityList<Double, CredentialProvider> pl = Plugins.getCredentialProviders();
+		List<CredentialProvider> flat = new ArrayList<>();
+		Iterator<PriorityList.Group<Double, CredentialProvider>> it = pl.groupIterator();
+		while (it.hasNext()) {
+			PriorityList.Group<Double, CredentialProvider> group = it.next();
+			for (CredentialProvider p : group) {
+				flat.add(p);
 			}
 		}
+		providers = Collections.unmodifiableList(flat);
+	}
 
+	public static String get(String key, CredentialProvider.Credential credential) {
+		for (CredentialProvider provider : providers) {
+			final String cred = provider.get(key, credential);
+			if (cred != null) return cred;
+		}
 		return null;
 	}
 
 	public static String match(String key, CredentialProvider.Credential credential) {
-		final Iterator<PriorityList.Group<Double, CredentialProvider>> iterator = providers.groupIterator();
-
-		while (iterator.hasNext()) {
-			final PriorityList.Group<Double, CredentialProvider> group = iterator.next();
-
-			for (CredentialProvider provider : group) {
-				final String cred = provider.match(key, credential);
-
-				if (cred != null) return cred;
-			}
+		for (CredentialProvider provider : providers) {
+			final String cred = provider.match(key, credential);
+			if (cred != null) return cred;
 		}
-
 		return null;
 	}
 }
